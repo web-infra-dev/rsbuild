60bed963fddfe0a1ac5f75ae4d81361c018496a1
chenjiahan <chenjiahan.jait@bytedance.com>
2023-10-12 10:52:25 +0800

    chore: extract common modern config to scripts

--- packages/monorepo-utils/modern.config.ts
+++ packages/monorepo-utils/modern.config.ts
@@ -1,10 +1,3 @@
-import { defineConfig, moduleTools } from '@modern-js/module-tools';
+import baseConfig from '../../scripts/modern.base.config';
 
-export default defineConfig({
-  plugins: [moduleTools()],
-  buildConfig: {
-    buildType: 'bundleless',
-    format: 'cjs',
-    target: 'es2019',
-  },
-});
+export default baseConfig;
