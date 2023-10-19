90cafcf49f1731654d8eca7c02ae7ec2b2a0687a
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-10-19 20:13:03 +0800

    test: plugin

--- packages/plugin-node-polyfill/tests/index.test.ts
+++ packages/plugin-node-polyfill/tests/index.test.ts
@@ -1,5 +1,6 @@
 import { expect, describe, it } from 'vitest';
-import { createStubBuilder } from '@rsbuild/webpack/stub';
+import { createStubBuilder } from '@rsbuild/vitest-helper';
+import { webpackProvider } from '@rsbuild/webpack';
 import { pluginNodePolyfill } from '../src';
 
 describe('plugins/node-polyfill', () => {
@@ -7,8 +8,19 @@ describe('plugins/node-polyfill', () => {
     const builder = await createStubBuilder({
       plugins: [pluginNodePolyfill()],
     });
-    const config = await builder.unwrapWebpackConfig();
+    const configs = await builder.initConfigs();
 
-    expect(config).toMatchSnapshot();
+    expect(configs[0]).toMatchSnapshot();
+  });
+
+  it('should add node-polyfill config when use webpack', async () => {
+    const builder = await createStubBuilder({
+      plugins: [pluginNodePolyfill()],
+      provider: webpackProvider,
+      builderConfig: {},
+    });
+    const configs = await builder.initConfigs();
+
+    expect(configs[0]).toMatchSnapshot();
   });
 });
