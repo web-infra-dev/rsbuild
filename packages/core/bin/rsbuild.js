8f8b1ba5d397efbc00b840a13e2f95891018bcb7
neverland <chenjiahan.jait@bytedance.com>
2023-10-20 17:42:22 +0800

    fix(cli): output a blank line to keep the greet log nice (#147)

--- packages/core/bin/rsbuild.js
+++ packages/core/bin/rsbuild.js
@@ -3,6 +3,14 @@ const { logger } = require('rslog');
 
 async function main() {
   const { version } = require('../package.json');
+
+  // If not called through a package manager,
+  // output a blank line to keep the greet log nice.
+  const { npm_execpath } = process.env;
+  if (!npm_execpath || npm_execpath.includes('npx-cli.js')) {
+    console.log();
+  }
+
   logger.greet(`  ${`Rsbuild v${version}`}\n`);
 
   try {
