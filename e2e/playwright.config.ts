32ac34e243bc2e8d10a9e4084117d9ab74ce93e7
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-10-20 16:21:36 +0800

    fix: test case

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from '@playwright/test';
 
-// TODO: fix these cases
-const ignoreFolder = ['stylus', 'stylus-rem'];
-
 export default defineConfig({
   // exclude webpack / rspack self-feature test cases when run rspack / webpack test
   testIgnore: [
     ...(process.env.PROVIDE_TYPE === 'webpack'
       ? ['**/cases/**/**.rspack.test.ts']
       : ['**/cases/**/**.webpack.test.ts', '**/cases/**/**.swc.test.ts']),
-    ...ignoreFolder.map((item) => `**/cases/${item}/**/*`),
   ],
 });
