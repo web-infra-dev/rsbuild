4919baac75f0bcefcc8f31e38dac60e8415d8f11
neverland <chenjiahan.jait@bytedance.com>
2023-10-20 22:01:17 +0800

    feat(plugin-vue): split vue-jsx as standalone plugin (#154)

--- packages/plugin-vue/tests/index.test.ts
+++ packages/plugin-vue/tests/index.test.ts
@@ -1,14 +1,11 @@
 import { expect, describe, it } from 'vitest';
 import { createStubRsbuild } from '@rsbuild/test-helper';
-import { webpackProvider } from '@rsbuild/webpack';
-import { pluginBabel } from '@rsbuild/webpack/plugin-babel';
 import { pluginDefine } from '@rsbuild/core/plugins/define';
 import { pluginVue } from '../src';
 
 describe('plugins/vue', () => {
   it('should add vue-loader and VueLoaderPlugin correctly', async () => {
     const rsbuild = await createStubRsbuild({
-      provider: webpackProvider,
       rsbuildConfig: {},
       plugins: [pluginVue()],
     });
@@ -19,7 +16,6 @@ describe('plugins/vue', () => {
 
   it('should allow to configure vueLoader options', async () => {
     const rsbuild = await createStubRsbuild({
-      provider: webpackProvider,
       rsbuildConfig: {},
       plugins: [
         pluginVue({
@@ -34,39 +30,9 @@ describe('plugins/vue', () => {
     expect(config).toMatchSnapshot();
   });
 
-  it('should apply jsx babel plugin correctly', async () => {
-    const rsbuild = await createStubRsbuild({
-      provider: webpackProvider,
-      rsbuildConfig: {},
-      plugins: [pluginVue(), pluginBabel()],
-    });
-    const config = await rsbuild.unwrapConfig();
-
-    expect(config).toMatchSnapshot();
-  });
-
-  it('should allow to configure jsx babel plugin options', async () => {
-    const rsbuild = await createStubRsbuild({
-      provider: webpackProvider,
-      rsbuildConfig: {},
-      plugins: [
-        pluginVue({
-          vueJsxOptions: {
-            transformOn: false,
-          },
-        }),
-        pluginBabel(),
-      ],
-    });
-    const config = await rsbuild.unwrapConfig();
-
-    expect(config).toMatchSnapshot();
-  });
-
   it('should define feature flags correctly', async () => {
     const rsbuild = await createStubRsbuild({
       plugins: [pluginVue(), pluginDefine()],
-      provider: webpackProvider,
       rsbuildConfig: {},
     });
     const config = await rsbuild.unwrapConfig();
