25e598456430c701ca23908ace42f7709caef777
Belinda Cao <caohuilin@bytedance.com>
2023-10-20 15:56:03 +0800

    feat(workflow): add update modern and rspress version scripts (#143)

---
+++ scripts/update-packages/src/modern.ts
@@ -0,0 +1,71 @@
+/**
+ * In this script, we will find all the Modern.js dependencies and update the version.
+ */
+import path from 'path';
+import fs from 'fs-extra';
+import { getPackages } from '@manypkg/get-packages';
+import { getPackageVersion } from '@modern-js/generator-utils';
+
+async function run() {
+  const modernVersion = process.env.MODERN_VERSION || 'latest';
+  const cwd = process.cwd();
+  const pkgPath = path.join(cwd, 'package.json');
+  const pkgObj = fs.readJSONSync(pkgPath, 'utf-8');
+
+  pkgObj.devDependencies = await updateModernVersion(
+    pkgObj.devDependencies,
+    modernVersion,
+  );
+
+  fs.writeJSONSync(pkgPath, pkgObj, { spaces: 2 });
+
+  const packages = await getPackages(cwd);
+  for (const pkg of packages.packages) {
+    const { packageJson, dir } = pkg;
+    const { dependencies, devDependencies, peerDependencies } = packageJson;
+    packageJson.dependencies = await updateModernVersion(
+      dependencies,
+      modernVersion,
+    );
+    packageJson.devDependencies = await updateModernVersion(
+      devDependencies,
+      modernVersion,
+    );
+    packageJson.peerDependencies = await updateModernVersion(
+      peerDependencies,
+      modernVersion,
+    );
+    fs.writeJSONSync(path.join(dir, 'package.json'), packageJson, {
+      spaces: 2,
+    });
+  }
+}
+
+const versionMap = new Map();
+
+const updateModernVersion = async (
+  dependencies?: Record<string, string>,
+  modernVersion?: string,
+) => {
+  if (!dependencies) {
+    return dependencies;
+  }
+  for (const dep of Object.keys(dependencies)) {
+    if (dep.startsWith('@modern-js') && dep !== '@modern-js/swc-plugins') {
+      if (versionMap.get(dep)) {
+        dependencies[dep] = `^${versionMap.get(dep)}`;
+      } else {
+        const version = await getPackageVersion(`${dep}@${modernVersion}`);
+        dependencies[dep] = `^${version}`;
+        versionMap.set(dep, version);
+      }
+    }
+  }
+  return dependencies;
+};
+
+run().catch((e) => {
+  console.error(e);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
---
+++ scripts/update-packages/src/rspress.ts
@@ -0,0 +1,43 @@
+/**
+ * In this script, we will find all the rspress dependencies and update the version.
+ */
+import path from 'path';
+import fs from 'fs-extra';
+import { getPackageVersion } from '@modern-js/generator-utils';
+
+const versionMap = new Map();
+
+const updateRspressVersion = async (dependencies?: Record<string, string>) => {
+  if (!dependencies) {
+    return dependencies;
+  }
+  for (const dep of Object.keys(dependencies)) {
+    // Update rspress version
+    if (dep.startsWith('@rspress') || dep.startsWith('rspress')) {
+      if (versionMap.get(dep)) {
+        dependencies[dep] = `^${versionMap.get(dep)}`;
+      } else {
+        const version = await getPackageVersion(`${dep}@latest`);
+        dependencies[dep] = `^${version}`;
+        versionMap.set(dep, version);
+      }
+    }
+  }
+  return dependencies;
+};
+
+async function run() {
+  const cwd = process.cwd();
+  const repoDir = path.join(cwd, '../../');
+  const documentDir = path.join(repoDir, 'packages/document');
+  const pkgPath = path.join(documentDir, 'package.json');
+  const pkgObj = fs.readJSONSync(pkgPath);
+  pkgObj.devDependencies = await updateRspressVersion(pkgObj.devDependencies);
+  fs.writeJSONSync(pkgPath, pkgObj, { spaces: 2 });
+}
+
+run().catch((e) => {
+  console.error(e);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
