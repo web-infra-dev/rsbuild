c4b35c198aafc5374d99141b5555058567978553
neverland <chenjiahan.jait@bytedance.com>
2023-10-21 19:40:26 +0800

    test: remove ui libs, improve testing stability (#159)

--- e2e/cases/import-antd-v4/index.test.ts
+++ e2e/cases/import-antd-v4/index.test.ts
@@ -4,15 +4,13 @@ import { build } from '@scripts/shared';
 
 test.setTimeout(120000);
 
-test('should import antd v4 correctly', async () => {
-  const rsbuild = await build(
-    {
-      cwd: __dirname,
-      entry: { index: path.resolve(__dirname, './src/index.ts') },
-    },
-    undefined,
-    false,
-  );
+// Skipped
+// we should find a better way to test it without installing antd
+test.skip('should import antd v4 correctly', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    entry: { index: path.resolve(__dirname, './src/index.ts') },
+  });
   const files = await rsbuild.unwrapOutputJSON();
   expect(
     Object.keys(files).find((file) => file.includes('lib-antd')),
