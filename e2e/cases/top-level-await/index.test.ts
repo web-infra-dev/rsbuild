a2e1177125fe1b48c2bf5e4813b2ff9dca524dc6
neverland <chenjiahan.jait@bytedance.com>
2023-10-24 16:34:13 +0800

    chore(deps): bump Rspack v0.3.8 (#189)

--- e2e/cases/top-level-await/index.test.ts
+++ e2e/cases/top-level-await/index.test.ts
@@ -1,9 +1,8 @@
 import * as path from 'path';
-import { expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { build, getHrefByEntryName } from '@scripts/shared';
-import { webpackOnlyTest } from '../../scripts/helper';
 
-webpackOnlyTest('should run top level await correctly', async ({ page }) => {
+test('should run top level await correctly', async ({ page }) => {
   const rsbuild = await build({
     cwd: __dirname,
     entry: {
