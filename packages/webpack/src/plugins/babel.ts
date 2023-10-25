6eb683a7188018ae404d98d3e540fa6dcfa9d64c
gaoyuan <gaoyuan.1226@bytedance.com>
2023-10-25 18:29:54 +0800

    chore: apply babel in modifyBundlerChain (#204)

--- packages/webpack/src/plugins/babel.ts
+++ packages/webpack/src/plugins/babel.ts
@@ -32,7 +32,7 @@ export const getUseBuiltIns = (config: NormalizedConfig) => {
 export const pluginBabel = (): RsbuildPlugin => ({
   name: 'plugin-babel',
   setup(api) {
-    api.modifyWebpackChain(
+    api.modifyBundlerChain(
       async (
         chain,
         {
