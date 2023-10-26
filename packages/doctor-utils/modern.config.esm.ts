53d17006444c1f2bd3fb431604f3262cd3edc453
chenjiahan <chenjiahan.jait@bytedance.com>
2023-10-26 20:51:05 +0800

    chore: fix

--- packages/doctor-utils/modern.config.esm.ts
+++ packages/doctor-utils/modern.config.esm.ts
@@ -1,4 +1,4 @@
-import { defineConfig, moduleTools } from '@modern-js/module-tools';
+import { defineConfig } from '@modern-js/module-tools';
 import baseConfig from '../../scripts/modern.base.config';
 
 export default defineConfig({
