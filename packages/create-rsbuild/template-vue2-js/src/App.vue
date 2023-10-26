ada7535d580ad50f7b1274f02d60912f0f7196d2
neverland <chenjiahan.jait@bytedance.com>
2023-10-26 20:01:56 +0800

    feat(create-rsbuild): add Vue 2 templates (#223)

---
+++ packages/create-rsbuild/template-vue2-js/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Rsbuild + Vue</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+  data() {
+    return { count: 0 };
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  padding: 2em;
+}
+</style>
---
+++ packages/create-rsbuild/template-vue2-ts/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Rsbuild + Vue</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+  data() {
+    return { count: 0 };
+  },
+});
+</script>
+
+<style scoped>
+.card {
+  padding: 2em;
+}
+</style>
