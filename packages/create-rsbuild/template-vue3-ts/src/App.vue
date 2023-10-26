431edd428294065e2745b65e5a93d683ae8ea509
neverland <chenjiahan.jait@bytedance.com>
2023-10-26 19:37:34 +0800

    feat(create-rspack): add Vue 3 templates (#222)

---
+++ packages/create-rsbuild/template-vue3-ts/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: {
+    type: String,
+  },
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>Rsbuild + Vue</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 2em;
+}
+</style>
