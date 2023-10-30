021b9d5f23fb7542667a15d5944389758ae3ac2a
zjk <jingkai.zhao@foxmail.com>
2023-10-30 16:33:45 +0800

    chore: format example files

--- examples/svelte/src/App.svelte
+++ examples/svelte/src/App.svelte
@@ -1,32 +1,38 @@
 <script>
-	import Footer from './Footer.svelte';
-	export let name;
+  import Footer from './Footer.svelte';
+  export let name;
   export let count = 0;
 </script>
 
 <main>
-	<h1>Hello {name}! </h1>
-  <div><button on:click={() => { count += 1 }}>{count}</button></div>
-	<Footer />
+  <h1>Hello {name}!</h1>
+  <div>
+    <button
+      on:click={() => {
+        count += 1;
+      }}>{count}</button
+    >
+  </div>
+  <Footer />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
   h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 </style>
--- examples/svelte/src/Footer.svelte
+++ examples/svelte/src/Footer.svelte
@@ -1,5 +1,8 @@
 <main>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
 <style>
