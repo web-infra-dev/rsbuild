1d1a715398365e15dd93f8482aa8e62ee7896776
neverland <chenjiahan.jait@bytedance.com>
2023-10-30 10:10:51 +0800

    chore(doctor): simplify doctor build configs (#253)

--- packages/doctor-utils/modern.config.ts
+++ packages/doctor-utils/modern.config.ts
@@ -1,15 +1,3 @@
-import { defineConfig } from '@modern-js/module-tools';
-import baseConfig from '../../scripts/modern.base.config';
+import { configWithEsm } from '../../scripts/modern.base.config';
 
-export default defineConfig({
-  ...baseConfig,
-  buildConfig: {
-    buildType: 'bundleless',
-    format: 'cjs',
-    target: 'es2019',
-    outDir: './dist/cjs',
-    dts: {
-      distPath: '../type',
-    },
-  },
-});
+export default configWithEsm;
