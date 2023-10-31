50c271181381b8ed23829aaf5971f8b46788e098
neverland <chenjiahan.jait@bytedance.com>
2023-10-31 16:51:19 +0800

    feat(create-rsbuild): always use the latest version (#287)

---
+++ .changeset/shaggy-crabs-pay.md
@@ -0,0 +1,5 @@
+---
+'create-rsbuild': patch
+---
+
+feat(create-rsbuild): always use the latest version
--- packages/create-rsbuild/src/index.ts
+++ packages/create-rsbuild/src/index.ts
@@ -22,6 +22,8 @@ async function main() {
 
   const cwd = process.cwd();
   const packageRoot = path.resolve(__dirname, '..');
+  const packageJsonPath = path.join(packageRoot, 'package.json');
+  const { version } = require(packageJsonPath);
 
   let targetDir = (await text({
     message: 'Input target folder',
@@ -66,8 +68,8 @@ async function main() {
   const commonFolder = path.join(packageRoot, `template-common`);
   const distFolder = path.join(cwd, targetDir);
 
-  copyFolder(commonFolder, distFolder);
-  copyFolder(srcFolder, distFolder);
+  copyFolder(commonFolder, distFolder, version);
+  copyFolder(srcFolder, distFolder, version);
 
   const nextSteps = [`cd ${targetDir}`, 'npm i', 'npm run dev'];
 
@@ -76,7 +78,7 @@ async function main() {
   outro('Done.');
 }
 
-function copyFolder(src: string, dist: string) {
+function copyFolder(src: string, dist: string, version: string) {
   const renameFiles: Record<string, string> = {
     gitignore: '.gitignore',
   };
@@ -98,11 +100,21 @@ function copyFolder(src: string, dist: string) {
     const stat = fs.statSync(srcFile);
 
     if (stat.isDirectory()) {
-      copyFolder(srcFile, distFile);
+      copyFolder(srcFile, distFile, version);
     } else {
       fs.copyFileSync(srcFile, distFile);
+
+      if (file === 'package.json') {
+        replaceVersion(distFile, version);
+      }
     }
   }
 }
+
+const replaceVersion = (pkgJsonPath: string, version: string) => {
+  let content = fs.readFileSync(pkgJsonPath, 'utf-8');
+  content = content.replace(/workspace:\*/g, `^${version}`);
+  fs.writeFileSync(pkgJsonPath, content);
+};
 
 main();
