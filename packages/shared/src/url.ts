3a0976ac23b4c827328c32170a00d08b11bec7ac
Ranger <32590310+southorange0929@users.noreply.github.com>
2023-11-03 17:10:09 +0800

    fix(shared): get ipv4 interfaces should be uniq (#341)

--- packages/shared/src/url.ts
+++ packages/shared/src/url.ts
@@ -25,19 +25,22 @@ export type AddressUrl = { label: string; url: string };
 
 const getIpv4Interfaces = () => {
   const interfaces = os.networkInterfaces();
-  const ipv4Interfaces: os.NetworkInterfaceInfo[] = [];
+  const ipv4Interfaces: Map<string, os.NetworkInterfaceInfo> = new Map();
 
   Object.keys(interfaces).forEach((key) => {
     interfaces[key]!.forEach((detail) => {
       // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
       const familyV4Value = typeof detail.family === 'string' ? 'IPv4' : 4;
 
-      if (detail.family === familyV4Value) {
-        ipv4Interfaces.push(detail);
+      if (
+        detail.family === familyV4Value &&
+        !ipv4Interfaces.has(detail.address)
+      ) {
+        ipv4Interfaces.set(detail.address, detail);
       }
     });
   });
-  return ipv4Interfaces;
+  return Array.from(ipv4Interfaces.values());
 };
 
 export const getAddressUrls = (
