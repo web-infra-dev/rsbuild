d96b7b07d07bb7ec42b4c0a5725d96965abb9cd3
neverland <chenjiahan.jait@bytedance.com>
2023-11-11 11:05:33 +0800

    chore: add uni-builder example (#457)

---
+++ examples/uni-builder/dev.mjs
@@ -0,0 +1,12 @@
+import { createUniBuilder } from '@rsbuild/uni-builder';
+
+async function main() {
+  const builder = await createUniBuilder({
+    bundlerType: process.env.WEBPACK ? 'webpack' : 'rspack',
+    config: {},
+  });
+
+  builder.startDevServer();
+}
+
+main();
