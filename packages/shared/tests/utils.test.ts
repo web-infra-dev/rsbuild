995dc8949fa237e2a3d49afdc59dd06db2aa896a
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-12 16:17:15 +0800

    chore: add tests

--- packages/shared/tests/utils.test.ts
+++ packages/shared/tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { awaitableGetter } from '../src';
+import { awaitableGetter, camelCase } from '../src';
 
 describe('awaitableGetter', () => {
   it('should work', async () => {
@@ -7,5 +7,27 @@ describe('awaitableGetter', () => {
     const wrapped = awaitableGetter(promises);
     expect(wrapped.promises).toStrictEqual(promises);
     expect(await wrapped).toStrictEqual(arr);
+  });
+});
+
+describe('camelCase', () => {
+  test('should convert snake_case to camelCase', () => {
+    expect(camelCase('hello_world')).toBe('helloWorld');
+  });
+
+  test('should convert kebab-case to camelCase', () => {
+    expect(camelCase('foo-bar-baz')).toBe('fooBarBaz');
+  });
+
+  test('should handle already camelCase input', () => {
+    expect(camelCase('alreadyCamelCase')).toBe('alreadyCamelCase');
+  });
+
+  test('should handle empty input', () => {
+    expect(camelCase('')).toBe('');
+  });
+
+  test('should handle single-word input', () => {
+    expect(camelCase('single')).toBe('single');
   });
 });
