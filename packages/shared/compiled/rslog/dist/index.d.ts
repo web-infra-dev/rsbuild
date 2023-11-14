6fc0ddda7b46727975e9899e0142cff6476796c3
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-14 21:23:12 +0800

    fix: include dist

---
+++ packages/shared/compiled/rslog/dist/index.d.ts
@@ -0,0 +1,64 @@
+type ColorFn = (input: string | number | null | undefined) => string;
+
+declare let LOG_TYPES: {
+    error: {
+        label: string;
+        level: "error";
+        color: ColorFn;
+    };
+    warn: {
+        label: string;
+        level: "warn";
+        color: ColorFn;
+    };
+    info: {
+        label: string;
+        level: "info";
+        color: ColorFn;
+    };
+    start: {
+        label: string;
+        level: "info";
+        color: ColorFn;
+    };
+    ready: {
+        label: string;
+        level: "info";
+        color: ColorFn;
+    };
+    success: {
+        label: string;
+        level: "info";
+        color: ColorFn;
+    };
+    log: {
+        level: "log";
+    };
+    debug: {
+        label: string;
+        level: "verbose";
+        color: ColorFn;
+    };
+};
+
+type LogLevel = 'error' | 'warn' | 'info' | 'log' | 'verbose';
+type LogMessage = number | string | unknown | Error | null;
+interface LogType {
+    label?: string;
+    level: LogLevel;
+    color?: ColorFn;
+}
+type LogFunction = (message?: LogMessage, ...args: any[]) => void;
+interface Options {
+    level?: LogLevel;
+}
+type LogMethods = keyof typeof LOG_TYPES;
+type Logger = Record<LogMethods, LogFunction> & {
+    greet: (message: string) => void;
+};
+
+declare let createLogger: (options?: Options) => Logger;
+
+declare let logger: Logger;
+
+export { LogFunction, LogLevel, LogMessage, LogType, Logger, Options, createLogger, logger };
