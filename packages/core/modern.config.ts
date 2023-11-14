6df008563664e5fa47885a2382a0182009f204f9
gaoyuan <gaoyuan.1226@bytedance.com>
2023-11-14 10:50:22 +0800

    chore: compile hmr-client as es5 (#501)

--- packages/core/modern.config.ts
+++ packages/core/modern.config.ts
@@ -1,3 +1,28 @@
-import baseConfig from '../../scripts/modern.base.config';
+import { baseBuildConfig } from '../../scripts/modern.base.config';
 
-export default baseConfig;
+export default {
+  ...baseBuildConfig,
+  buildConfig: [
+    {
+      ...baseBuildConfig.buildConfig,
+      input: ['src', '!src/client'],
+    },
+    {
+      buildType: 'bundle',
+      format: 'esm',
+      target: 'es5',
+      dts: false,
+      input: {
+        hmr: 'src/client/hmr/index.ts',
+      },
+      outDir: './dist/client',
+      // fix `module.hot` not works (avoid inject module as local variable)
+      esbuildOptions: (options) => {
+        options.format = undefined;
+        return options;
+      },
+      // bundle shared deps when used in client
+      autoExternal: false,
+    },
+  ],
+};
