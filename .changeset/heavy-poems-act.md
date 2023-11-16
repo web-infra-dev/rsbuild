53f8b52ef26134ac34519c64d742964337695840
neverland <chenjiahan.jait@bytedance.com>
2023-11-16 11:06:05 +0800

    fix: should register config.plugins when using JavaScript API (#541)

---
+++ .changeset/heavy-poems-act.md
@@ -0,0 +1,6 @@
+---
+'@rsbuild/shared': patch
+'@rsbuild/core': patch
+---
+
+fix: should register config.plugins when using JavaScript API
---
+++ e2e/cases/javascript-api/plugin/index.test.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { expect } from '@playwright/test';
+import { rspackOnlyTest, globContentJSON } from '../../../scripts/helper';
+import { createRsbuild } from '@rsbuild/core';
+import { pluginVue } from '@rsbuild/plugin-vue';
+
+rspackOnlyTest(
+  'should register plugins correctly when using JavaScript API',
+  async () => {
+    const rsbuild = await createRsbuild({
+      cwd: __dirname,
+      rsbuildConfig: {
+        plugins: [pluginVue()],
+      },
+    });
+
+    await rsbuild.build();
+
+    const outputs = await globContentJSON(path.join(__dirname, 'dist'));
+    const outputFiles = Object.keys(outputs);
+
+    expect(
+      outputFiles.find((item) => item.includes('index.html')),
+    ).toBeTruthy();
+    expect(
+      outputFiles.find((item) => item.includes('static/js/index.')),
+    ).toBeTruthy();
+  },
+);
