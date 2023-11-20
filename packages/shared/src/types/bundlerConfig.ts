3ea18233701adb7e2f5908f0fce3d96ea6e044b6
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-20 14:58:48 +0800

    types: fix some type issue caused by BundlerChain

--- packages/shared/src/types/bundlerConfig.ts
+++ packages/shared/src/types/bundlerConfig.ts
@@ -13,64 +13,8 @@ export interface BundlerPluginInstance {
   }) => void;
 }
 
-// excludeAny: any extends boolean/string/xxx ? A : B  => A | B;
-type ExtendsExcludeAny<T, E> = T extends any
-  ? T extends E
-    ? true
-    : false
-  : false;
-
-/**
- * use `fn: (...args) => BundlerChain` instead of `fn: (...args) => WebpackChain`
- */
-type ModifyReturnThis<F extends (...args: any) => any, R = ReturnType<F>> = (
-  ...values: Parameters<F>
-) => ExtendsExcludeAny<R, WebpackChain> extends true
-  ? BundlerChain
-  : ExtendsExcludeAny<R, Record<string, any>> extends true
-  ? PickAndModifyThis<R>
-  : R;
-
-/**
- * use `{ a: () => BundlerChain; b: { c: () => BundlerChain }}` instead of `{ a: () => WebpackChain; b: { c: () => WebpackChain }}`
- */
-type PickAndModifyThis<T, K extends keyof T = keyof T> = {
-  [P in K]: T[P] extends (...args: any) => any
-    ? ModifyReturnThis<T[P]>
-    : ExtendsExcludeAny<T[P], Record<string, any>> extends true
-    ? PickAndModifyThis<T[P]>
-    : T[P];
-};
-
-export interface BundlerChain
-  extends PickAndModifyThis<
-    WebpackChain,
-    | 'devtool'
-    | 'target'
-    | 'name'
-    | 'merge'
-    | 'cache'
-    | 'plugin'
-    | 'plugins'
-    | 'entryPoints'
-    | 'mode'
-    | 'module'
-    | 'context'
-    | 'externals'
-    | 'externalsType'
-    | 'externalsPresets'
-    | 'entry'
-    | 'get'
-    | 'output'
-    | 'resolve'
-    | 'optimization'
-    | 'experiments'
-    | 'profile'
-    | 'ignoreWarnings'
-    | 'infrastructureLogging'
-  > {
+export interface BundlerChain extends Omit<WebpackChain, 'toConfig'> {
   toConfig: () => Configuration;
 }
 
-export type WebpackChainRule = WebpackChain.Rule;
-export type BundlerChainRule = PickAndModifyThis<WebpackChain.Rule>;
+export type BundlerChainRule = WebpackChain.Rule;
