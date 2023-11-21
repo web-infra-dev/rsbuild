7b525a998df1a80a97370b3e6876faefe8755c8a
easy1090 <752355956@qq.com>
2023-11-21 16:51:57 +0800

    fix: rsbuild doctor add e2e tests
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests
    
    fix: rsbuild doctor add e2e tests

--- e2e/cases/doctor-rspack/tests/test-utils.ts
+++ e2e/cases/doctor-rspack/tests/test-utils.ts
@@ -1,9 +1,9 @@
 import { DoctorRspackPluginOptions } from '@rsbuild/doctor-core/types';
+import { RsbuildDoctorRspackPlugin } from '@rsbuild/doctor-rspack-plugin';
 import { Linter } from '@rsbuild/doctor-types';
 import { File } from '@rsbuild/doctor-utils/build';
 import { tmpdir } from 'os';
 import path from 'path';
-import { RsbuildDoctorRspackPlugin } from '../src';
 
 export function createDoctorPlugin<T extends Linter.ExtendRuleData[]>(
   options: DoctorRspackPluginOptions<T>,
@@ -20,10 +20,14 @@ export function createDoctorPlugin<T extends Linter.ExtendRuleData[]>(
     tmpdir(),
     `./${Date.now()}/rsbuild_doctor_rspack_plugin_test`,
   );
-  plugin.sdk.setOutputDir(outdir);
 
   plugin.sdk.hooks.afterSaveManifest.tapPromise('REMOVE_TMP_DIR', async () => {
-    await File.fse.remove(plugin.sdk.outputDir);
+    plugin.sdk.setOutputDir(outdir);
+    try {
+      await File.fse.remove(plugin.sdk.outputDir);
+    } catch (e) {
+      console.error(e);
+    }
   });
 
   return plugin;
--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,11 +4,16 @@ export default defineConfig({
   // exclude webpack / rspack self-feature test cases when run rspack / webpack test
   testIgnore: [
     ...(process.env.PROVIDE_TYPE === 'webpack'
-      ? ['**/cases/**/**.rspack.test.ts', '/uni-builder/**/*']
+      ? [
+          '**/cases/**/**.rspack.test.ts',
+          '/uni-builder/**/*',
+          '**/cases/doctor-*/**/*',
+        ]
       : [
           '**/cases/**/**.webpack.test.ts',
           '**/cases/**/**.swc.test.ts',
           '/uni-builder/**/*',
+          '**/cases/doctor-*/**/*',
         ]),
   ],
 });
