e0608b56437a91bd9bfd287c83c549aec7dae1c1
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-21 15:57:22 +0800

    feat(cli): set 300ms debounce to avoid restart frequently

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -180,3 +180,20 @@ export function createCacheGroups(
 
   return experienceCacheGroup;
 }
+
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId !== null) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
+}
