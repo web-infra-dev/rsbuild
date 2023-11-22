7fc96d783066bf85de367e561dee11b511c45bbe
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-22 11:29:51 +0800

    chore: remove demo

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,9 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  server: {
-    proxy: {
-      '/api': 'https://cnodejs.org',
-    },
-  },
 });
