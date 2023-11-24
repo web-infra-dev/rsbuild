e09043a8933dda69ad2c72f983773923ae1e3502
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-24 20:26:50 +0800

    chore: add Vue examples

---
+++ examples/vue2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <h1>Rsbuild with Vue</h1>
+    <p>Start building amazing things with Rsbuild.</p>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  min-height: 100vh;
+  line-height: 1.1;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.content h1 {
+  font-size: 3.6rem;
+  font-weight: 700;
+}
+
+.content p {
+  font-size: 1.2rem;
+  font-weight: 400;
+  opacity: 0.5;
+}
+</style>
