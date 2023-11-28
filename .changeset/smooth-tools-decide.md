6f3175a7a5f0a6a57d4ccc077831f23af4ba42d9
gaoyuan <gaoyuan.1226@bytedance.com>
2023-11-28 16:44:22 +0800

    fix: rspack ecosystem-ci failed (#744)

---
+++ .changeset/smooth-tools-decide.md
@@ -0,0 +1,5 @@
+---
+'@rsbuild/core': patch
+---
+
+fix: bump rspack and update snapshot to fix rspack ecosystem-ci failed
--- e2e/cases/stylus/index.test.ts
+++ e2e/cases/stylus/index.test.ts
@@ -16,7 +16,7 @@ test('should compile stylus correctly', async () => {
 
   if (rsbuild.providerType === 'rspack') {
     expect(content).toEqual(
-      'body{color:#f00;font:14px Arial,sans-serif}.title-class-_7352f{font-size:14px}',
+      'body{color:#f00;font:14px Arial,sans-serif}.title-class-_6c2f8{font-size:14px}',
     );
   } else {
     expect(content).toEqual(
