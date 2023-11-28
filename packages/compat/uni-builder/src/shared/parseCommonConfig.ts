4c9c6ed6694c3ac5e4a6fd3677ee799f0df9e023
gaoyuan <gaoyuan.1226@bytedance.com>
2023-11-28 13:41:17 +0800

    feat(uni-builder): support checkSyntax (#739)

--- packages/compat/uni-builder/src/shared/parseCommonConfig.ts
+++ packages/compat/uni-builder/src/shared/parseCommonConfig.ts
@@ -30,6 +30,7 @@ import { pluginMainFields } from './plugins/mainFields';
 import { pluginExtensionPrefix } from './plugins/extensionPrefix';
 import { pluginSplitChunks } from './plugins/splitChunk';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginCheckSyntax } from '@rsbuild/plugin-check-syntax';
 
 const GLOBAL_CSS_REGEX = /\.global\.\w+$/;
 
@@ -214,6 +215,16 @@ export async function parseCommonConfig<B = 'rspack' | 'webpack'>(
     pluginSplitChunks(),
     pluginGlobalVars(uniBuilderConfig.source?.globalVars),
   ];
+
+  const checkSyntaxOptions = uniBuilderConfig.security?.checkSyntax;
+
+  if (checkSyntaxOptions) {
+    rsbuildPlugins.push(
+      pluginCheckSyntax(
+        typeof checkSyntaxOptions === 'boolean' ? {} : checkSyntaxOptions,
+      ),
+    );
+  }
 
   if (!uniBuilderConfig.output?.disableTsChecker) {
     rsbuildPlugins.push(
