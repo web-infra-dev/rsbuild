fb26310c00a1db19c03e1384a259cdd0b03f5c41
chenjiahan <chenjiahan.jait@bytedance.com>
2023-12-02 17:33:52 +0800

    test(e2e): check validity of mjs artifact

---
+++ e2e/cases/mjs-artifact/test.mjs
@@ -0,0 +1,43 @@
+import { pluginAssetsRetry } from '@rsbuild/plugin-assets-retry';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginCheckSyntax } from '@rsbuild/plugin-check-syntax';
+import { pluginCssMinimizer } from '@rsbuild/plugin-css-minimizer';
+import { pluginImageCompress } from '@rsbuild/plugin-image-compress';
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
+import { pluginPug } from '@rsbuild/plugin-pug';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginRem } from '@rsbuild/plugin-rem';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginSolid } from '@rsbuild/plugin-solid';
+import { pluginSvelte } from '@rsbuild/plugin-svelte';
+import { pluginStylus } from '@rsbuild/plugin-stylus';
+import { pluginSourceBuild } from '@rsbuild/plugin-source-build';
+import { pluginStyledComponents } from '@rsbuild/plugin-styled-components';
+import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+import { pluginVue } from '@rsbuild/plugin-vue';
+import { pluginVue2 } from '@rsbuild/plugin-vue2';
+import { pluginVueJsx } from '@rsbuild/plugin-vue-jsx';
+import { pluginVue2Jsx } from '@rsbuild/plugin-vue2-jsx';
+
+export default {
+  pluginAssetsRetry,
+  pluginBabel,
+  pluginCheckSyntax,
+  pluginCssMinimizer,
+  pluginImageCompress,
+  pluginNodePolyfill,
+  pluginPug,
+  pluginReact,
+  pluginRem,
+  pluginSvgr,
+  pluginSolid,
+  pluginStylus,
+  pluginSvelte,
+  pluginSourceBuild,
+  pluginStyledComponents,
+  pluginTypeCheck,
+  pluginVue,
+  pluginVue2,
+  pluginVueJsx,
+  pluginVue2Jsx,
+};
