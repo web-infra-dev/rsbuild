03a0ee2153b17c3c05c7dea2f6fa4727e136be40
neverland <chenjiahan.jait@bytedance.com>
2023-12-03 17:22:09 +0800

    test(e2e): add basic case for tailwindcss (#816)

---
+++ e2e/cases/css/tailwindcss/index.test.ts
@@ -0,0 +1,22 @@
+import { build } from '@scripts/shared';
+import { expect, test } from '@playwright/test';
+
+test('should generate tailwindcss utilities correctly', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    rsbuildConfig: {
+      html: {
+        template: './src/index.html',
+      },
+    },
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+  const indexCssFile = Object.keys(files).find(
+    (file) => file.includes('index.') && file.endsWith('.css'),
+  )!;
+
+  expect(files[indexCssFile]).toEqual(
+    '.text-3xl{font-size:1.875rem;line-height:2.25rem}.font-bold{font-weight:700}.underline{text-decoration-line:underline}',
+  );
+});
