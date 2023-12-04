c2661ac8c6d43a7c80110768f2448b570fb7f9e2
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-12-04 19:16:08 +0800

    fix: extends rsbuild default config

--- packages/compat/uni-builder/src/shared/defaults.ts
+++ packages/compat/uni-builder/src/shared/defaults.ts
@@ -1,39 +1,41 @@
 import { UniBuilderRspackConfig } from '../types';
+import {
+  getDefaultOutputConfig as getRsbuildDefaultOutputConfig,
+  getDefaultPerformanceConfig as getRsbuildDefaultPerformanceConfig,
+  getDefaultSourceConfig as getRsbuildDefaultSourceConfig,
+  getDefaultHtmlConfig as getRsbuildDefaultHtmlConfig,
+  getDefaultSecurityConfig as getRsbuildDefaultSecurityConfig,
+  getDefaultDevConfig as getRsbuildDefaultDevConfig,
+  getDefaultServerConfig as getRsbuildDefaultServerConfig,
+} from '@rsbuild/shared';
 
-export const DEFAULT_PORT = 8080;
-export const DEFAULT_DATA_URL_SIZE = 10000;
-export const DEFAULT_MOUNT_ID = 'root';
-export const DEFAULT_ASSET_PREFIX = '/';
-export const DEFAULT_DEV_HOST = '0.0.0.0';
+const rsbuildDefaultServerConfig = getRsbuildDefaultServerConfig();
 
 export const getDefaultDevConfig = () => ({
-  hmr: true,
+  ...getRsbuildDefaultDevConfig(),
+  port: rsbuildDefaultServerConfig.port,
+  host: rsbuildDefaultServerConfig.host,
   https: false,
-  port: DEFAULT_PORT,
-  assetPrefix: DEFAULT_ASSET_PREFIX,
-  startUrl: false,
   progressBar: true,
-  host: DEFAULT_DEV_HOST,
 });
 
 export const getDefaultSourceConfig = (): UniBuilderRspackConfig['source'] => ({
-  alias: {},
-  aliasStrategy: 'prefer-tsconfig',
-  preEntry: [],
+  ...getRsbuildDefaultSourceConfig(),
   globalVars: {},
-  define: {},
 });
 
+const rsbuildDefaultHtmlConfig = getRsbuildDefaultHtmlConfig();
+
 export const getDefaultHtmlConfig = (): UniBuilderRspackConfig['html'] => ({
-  inject: 'head',
-  mountId: DEFAULT_MOUNT_ID,
-  crossorigin: false,
+  inject: rsbuildDefaultHtmlConfig.inject,
+  mountId: rsbuildDefaultHtmlConfig.mountId,
+  crossorigin: rsbuildDefaultHtmlConfig.crossorigin,
+  scriptLoading: rsbuildDefaultHtmlConfig.scriptLoading,
   disableHtmlFolder: false,
-  scriptLoading: 'defer',
 });
 
 export const getDefaultSecurityConfig = () => ({
-  nonce: '',
+  ...getRsbuildDefaultSecurityConfig(),
   checkSyntax: false,
 });
 
@@ -45,73 +47,27 @@ export const getDefaultExperimentsConfig = () => ({
   sourceBuild: false,
 });
 
-export const getDefaultPerformanceConfig =
-  (): UniBuilderRspackConfig['performance'] => ({
-    profile: false,
-    buildCache: true,
-    printFileSize: true,
-    removeConsole: false,
-    transformLodash: true,
-    removeMomentLocale: false,
-    chunkSplit: {
-      strategy: 'split-by-experience',
-    },
-  });
+export const getDefaultPerformanceConfig = getRsbuildDefaultPerformanceConfig;
 
-export const ROOT_DIST_DIR = 'dist';
 export const HTML_DIST_DIR = 'html';
-export const SERVER_DIST_DIR = 'bundles';
-export const SERVER_WORKER_DIST_DIR = 'worker';
-export const JS_DIST_DIR = 'static/js';
-export const CSS_DIST_DIR = 'static/css';
-export const SVG_DIST_DIR = 'static/svg';
-export const FONT_DIST_DIR = 'static/font';
-export const WASM_DIST_DIR = 'static/wasm';
-export const IMAGE_DIST_DIR = 'static/image';
-export const MEDIA_DIST_DIR = 'static/media';
+
+const rsbuildDefaultOutputConfig = getRsbuildDefaultOutputConfig();
 
 export const getDefaultOutputConfig = (): UniBuilderRspackConfig['output'] => ({
+  ...rsbuildDefaultOutputConfig,
   distPath: {
-    root: ROOT_DIST_DIR,
-    js: JS_DIST_DIR,
-    css: CSS_DIST_DIR,
-    svg: SVG_DIST_DIR,
-    font: FONT_DIST_DIR,
+    ...rsbuildDefaultOutputConfig.distPath,
     html: HTML_DIST_DIR,
-    wasm: WASM_DIST_DIR,
-    image: IMAGE_DIST_DIR,
-    media: MEDIA_DIST_DIR,
-    server: SERVER_DIST_DIR,
-    worker: SERVER_WORKER_DIST_DIR,
   },
-  assetPrefix: DEFAULT_ASSET_PREFIX,
-  filename: {},
-  charset: 'ascii',
   polyfill: 'entry',
-  dataUriLimit: {
-    svg: DEFAULT_DATA_URL_SIZE,
-    font: DEFAULT_DATA_URL_SIZE,
-    image: DEFAULT_DATA_URL_SIZE,
-    media: DEFAULT_DATA_URL_SIZE,
-  },
-  legalComments: 'linked',
-  cleanDistPath: true,
   svgDefaultExport: 'url',
   disableSvgr: false,
-  disableCssExtract: false,
-  disableMinimize: false,
-  disableSourceMap: {
-    js: false,
-    css: undefined,
-  },
   disableTsChecker: false,
-  disableFilenameHash: false,
   disableCssModuleExtension: false,
   disableInlineRuntimeChunk: false,
   enableAssetFallback: false,
   enableAssetManifest: false,
   enableLatestDecorators: false,
-  enableCssModuleTSDeclaration: false,
   enableInlineScripts: false,
   enableInlineStyles: false,
   cssModules: {
