5b5172734cd61959b255057822161ff967df2d8e
chenjiahan <chenjiahan.jait@bytedance.com>
2023-12-06 23:14:20 +0800

    fix

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,9 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
 });
