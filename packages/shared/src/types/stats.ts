ba98f28b6bb4147f1bbed6f27df6bfe8aeb5e705
gaoyuan <gaoyuan.1226@bytedance.com>
2023-12-11 15:28:35 +0800

    types: export stats type from rspack (#914)

--- packages/shared/src/types/stats.ts
+++ packages/shared/src/types/stats.ts
@@ -1,18 +1,11 @@
-interface StatsOptionsObj {
-  all?: boolean;
-  preset?: 'normal' | 'none' | 'verbose' | 'errors-only' | 'errors-warnings';
-  assets?: boolean;
-  chunks?: boolean;
-  modules?: boolean;
-  entrypoints?: boolean;
-  warnings?: boolean;
-  warningsCount?: boolean;
-  errors?: boolean;
-  errorsCount?: boolean;
-  colors?: boolean;
-  logging?: boolean | 'none' | 'error' | 'warn' | 'info' | 'log' | 'verbose';
-  loggingTrace?: boolean;
+import type {
+  StatsOptions as RspackStatsOptions,
+  StatsCompilation,
+} from '@rspack/core';
 
+export type { StatsError, StatsAsset } from '@rspack/core';
+
+type StatsOptions = RspackStatsOptions & {
   /** Rspack not support below opts */
   cachedAssets?: boolean;
   groupAssetsByInfo?: boolean;
@@ -20,60 +13,7 @@ interface StatsOptionsObj {
   groupAssetsByChunk?: boolean;
   groupAssetsByExtension?: boolean;
   groupAssetsByEmitStatus?: boolean;
-}
-
-/** webpack not support boolean or string */
-type StatsOptions = StatsOptionsObj;
-
-interface StatsAssetInfo {
-  development?: boolean;
-}
-
-export interface StatsAsset {
-  type: string;
-  name: string;
-  size: number;
-  chunks?: Array<string | undefined | null>;
-  chunkNames?: Array<string | number>;
-  info: StatsAssetInfo;
-}
-
-export interface StatsError {
-  message: string;
-  formatted?: string;
-}
-
-interface StatsModule {
-  type?: string;
-  moduleType?: string;
-  identifier?: string;
-  name?: string;
-  id?: string;
-  chunks?: Array<string | undefined | null>;
-  size?: number;
-}
-
-interface StatsCompilation {
-  assets?: Array<StatsAsset>;
-  hash?: string;
-  modules?: Array<StatsModule>;
-  chunks?: Array<StatsChunk>;
-  // entrypoints?: Array<StatsEntrypoint>;
-  errors?: Array<StatsError>;
-  errorsCount?: number;
-  warnings?: Array<StatsError>;
-  warningsCount?: number;
-}
-
-interface StatsChunk {
-  type?: string;
-  files?: Array<string>;
-  id?: string | number;
-  entry: boolean;
-  initial: boolean;
-  names?: Array<string>;
-  size: number;
-}
+};
 
 export declare class Stats {
   constructor(statsJson: any);
