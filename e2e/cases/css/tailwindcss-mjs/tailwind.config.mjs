7a0855265b28449f56daab387fee966d7c03b2ed
neverland <chenjiahan.jait@bytedance.com>
2023-12-14 21:55:47 +0800

    test(e2e): add test case for mjs tailwindcss config (#955)

---
+++ e2e/cases/css/tailwindcss-mjs/tailwind.config.mjs
@@ -0,0 +1,10 @@
+import { join } from 'path';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [join(__dirname, './src/**/*.{html,js,ts,jsx,tsx}')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
