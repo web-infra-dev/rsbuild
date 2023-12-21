5a2bd32e4b3bc4b184124bc9a2348ba1bee93e2f
neverland <chenjiahan.jait@bytedance.com>
2023-12-21 22:21:12 +0800

    test(e2e): add test case for split-by-module (#1033)

---
+++ e2e/cases/performance/single-vendor/index.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@playwright/test';
+import { build } from '@scripts/shared';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { basename } from 'path';
+
+test('should generate vendor chunk when chunkSplit is "single-vendor"', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    plugins: [pluginReact()],
+    rsbuildConfig: {
+      output: {
+        disableFilenameHash: true,
+      },
+      performance: {
+        chunkSplit: {
+          strategy: 'single-vendor',
+        },
+      },
+    },
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+
+  const [vendorFile] = Object.entries(files).find(
+    ([name, content]) => name.includes('vendor') && content.includes('React'),
+  )!;
+  expect(vendorFile).toBeTruthy();
+
+  const jsFiles = Object.keys(files)
+    .filter((name) => name.endsWith('.js'))
+    .map((name) => basename(name));
+
+  expect(jsFiles.length).toEqual(2);
+  expect(jsFiles).toContain('index.js');
+  expect(jsFiles).toContain('vendor.js');
+});
---
+++ e2e/cases/performance/split-by-module/index.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test';
+import { build } from '@scripts/shared';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { basename } from 'path';
+
+test('should generate module chunks when chunkSplit is "split-by-module"', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    plugins: [pluginReact()],
+    rsbuildConfig: {
+      output: {
+        disableFilenameHash: true,
+      },
+      performance: {
+        chunkSplit: {
+          strategy: 'split-by-module',
+        },
+      },
+    },
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+
+  const [reactFile] = Object.entries(files).find(
+    ([name, content]) =>
+      name.includes('npm.react@') && content.includes('React'),
+  )!;
+  expect(reactFile).toBeTruthy();
+
+  const jsFiles = Object.keys(files)
+    .filter((name) => name.endsWith('.js'))
+    .map((name) => basename(name));
+
+  expect(jsFiles.length).toEqual(4);
+  expect(jsFiles).toContain('index.js');
+  expect(jsFiles.find((file) => file.includes('npm.react@'))).toBeTruthy();
+  expect(jsFiles.find((file) => file.includes('npm.react-dom@'))).toBeTruthy();
+  expect(jsFiles.find((file) => file.includes('npm.scheduler@'))).toBeTruthy();
+});
