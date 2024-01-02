a29e9e9dbfc6c708fa30013cacbe684069ebf9e1
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-02 19:21:44 +0800

    chore: fix

--- e2e/cases/javascript-api/custom-logger/index.test.ts
+++ e2e/cases/javascript-api/custom-logger/index.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { build } from '@scripts/shared';
 import { rspackOnlyTest, proxyConsole } from '@scripts/helper';
 
