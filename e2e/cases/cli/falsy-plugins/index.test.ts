1a330832dee82ce7121a967aad39fc594aa82dc8
Asuka109 <asuka109@foxmail.com>
2024-01-03 15:41:11 +0800

    chore: add e2e test `falsy-plugins`

---
+++ e2e/cases/cli/falsy-plugins/index.test.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import { execSync } from 'child_process';
+import { expect, test } from '@playwright/test';
+import { globContentJSON } from '../../../scripts/helper';
+
+test('should run build command correctly', async () => {
+  execSync('npx rsbuild build', {
+    cwd: __dirname,
+  });
+
+  const outputs = await globContentJSON(path.join(__dirname, 'dist'));
+  const outputFiles = Object.keys(outputs);
+
+  expect(outputFiles.find((item) => item.includes('index.html'))).toBeTruthy();
+  expect(
+    outputFiles.find((item) => item.includes('static/js/index.')),
+  ).toBeTruthy();
+});
