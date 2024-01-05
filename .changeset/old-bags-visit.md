9b77e48967b00f9e528f045b9a9cef6b3758aa90
neverland <chenjiahan.jait@bytedance.com>
2024-01-05 16:17:44 +0800

    fix(babel-preset): should merge targets array correctly (#1182)

---
+++ .changeset/old-bags-visit.md
@@ -0,0 +1,5 @@
+---
+'@rsbuild/babel-preset': patch
+---
+
+fix(babel-preset): should merge targets array correctly
--- packages/babel-preset/tests/node.test.ts
+++ packages/babel-preset/tests/node.test.ts
@@ -3,3 +3,13 @@ import { getBabelConfigForNode } from '../src/node';
 test('should provide node preset as expected', () => {
   expect(getBabelConfigForNode()).toMatchSnapshot();
 });
+
+test('should allow to override target node version', () => {
+  expect(
+    getBabelConfigForNode({
+      presetEnv: {
+        targets: ['node >= 20'],
+      },
+    }),
+  ).toMatchSnapshot();
+});
