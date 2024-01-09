530448aa9b5fc1afea37d6a9e15da5039c0f2bdb
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-09 10:16:48 +0800

    chore: merge HMR plugin into basic plugin

--- packages/core/src/plugins/basic.ts
+++ packages/core/src/plugins/basic.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { TARGET_ID_MAP, getJsSourceMap } from '@rsbuild/shared';
+import { TARGET_ID_MAP, getJsSourceMap, isUsingHMR } from '@rsbuild/shared';
 import type { RsbuildPlugin } from '../types';
 
 /**
@@ -9,43 +9,53 @@ export const pluginBasic = (): RsbuildPlugin => ({
   name: 'rsbuild:basic',
 
   setup(api) {
-    api.modifyBundlerChain((chain, { env, isProd, target }) => {
-      const config = api.getNormalizedConfig();
+    api.modifyBundlerChain(
+      (chain, { env, isProd, target, bundler, CHAIN_ID }) => {
+        const config = api.getNormalizedConfig();
 
-      chain.name(TARGET_ID_MAP[target]);
+        chain.name(TARGET_ID_MAP[target]);
 
-      chain.devtool(getJsSourceMap(config));
+        chain.devtool(getJsSourceMap(config));
 
-      // The base directory for resolving entry points and loaders from the configuration.
-      chain.context(api.context.rootPath);
+        // The base directory for resolving entry points and loaders from the configuration.
+        chain.context(api.context.rootPath);
 
-      chain.mode(isProd ? 'production' : 'development');
+        chain.mode(isProd ? 'production' : 'development');
 
-      chain.merge({
-        infrastructureLogging: {
-          // Using `error` level to avoid `cache.PackFileCacheStrategy` logs
-          level: 'error',
-        },
-      });
+        chain.merge({
+          infrastructureLogging: {
+            // Using `error` level to avoid `cache.PackFileCacheStrategy` logs
+            level: 'error',
+          },
+        });
 
-      // Disable performance hints, these logs are too complex
-      chain.performance.hints(false);
+        // Disable performance hints, these logs are too complex
+        chain.performance.hints(false);
 
-      // Align with the futureDefaults of webpack 6
-      chain.module.parser.merge({
-        javascript: {
-          exportsPresence: 'error',
-        },
-      });
+        // Align with the futureDefaults of webpack 6
+        chain.module.parser.merge({
+          javascript: {
+            exportsPresence: 'error',
+          },
+        });
 
-      if (env === 'development') {
-        // Set correct path for source map
-        // this helps VS Code break points working correctly in monorepo
-        chain.output.devtoolModuleFilenameTemplate(
-          (info: { absoluteResourcePath: string }) =>
-            path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
-        );
-      }
-    });
+        const usingHMR = isUsingHMR(config, { target, isProd });
+
+        if (usingHMR) {
+          chain
+            .plugin(CHAIN_ID.PLUGIN.HMR)
+            .use(bundler.HotModuleReplacementPlugin);
+        }
+
+        if (env === 'development') {
+          // Set correct path for source map
+          // this helps VS Code break points working correctly in monorepo
+          chain.output.devtoolModuleFilenameTemplate(
+            (info: { absoluteResourcePath: string }) =>
+              path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+          );
+        }
+      },
+    );
   },
 });
