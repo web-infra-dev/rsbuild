946407cf8fb328b30065e5e17e690dce712e9dba
neverland <chenjiahan.jait@bytedance.com>
2024-01-11 16:38:40 +0800

    test(e2e): add basic cases for Preact / React (#1250)

---
+++ e2e/cases/react/basic/index.test.ts
@@ -0,0 +1,40 @@
+import { expect } from '@playwright/test';
+import { rspackOnlyTest } from '@scripts/helper';
+import { dev, build, getHrefByEntryName } from '@scripts/shared';
+
+rspackOnlyTest(
+  'should render basic React component in development correctly',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+    });
+
+    await page.goto(getHrefByEntryName('index', rsbuild.port));
+
+    const button = page.locator('#button');
+    await expect(button).toHaveText('count: 0');
+    button.click();
+    await expect(button).toHaveText('count: 1');
+
+    rsbuild.server.close();
+  },
+);
+
+rspackOnlyTest(
+  'should render basic React component in production correctly',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      runServer: true,
+    });
+
+    await page.goto(getHrefByEntryName('index', rsbuild.port));
+
+    const button = page.locator('#button');
+    await expect(button).toHaveText('count: 0');
+    button.click();
+    await expect(button).toHaveText('count: 1');
+
+    rsbuild.close();
+  },
+);
