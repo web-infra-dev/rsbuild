789a37bdfbef899ab275305b4bec177a720bf7db
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-11 17:41:28 +0800

    feat(plugin-preact): add reactAliasesEnabled option

--- packages/plugin-preact/src/index.ts
+++ packages/plugin-preact/src/index.ts
@@ -1,23 +1,33 @@
 import type { RsbuildPlugin } from '@rsbuild/core';
 import type { SwcReactConfig } from '@rsbuild/shared';
 
-export type PluginPreactOptions = Record<string, never>;
+export type PluginPreactOptions = {
+  /**
+   * Whether to aliases `react`, `react-dom` to `preact/compat`
+   * @default true
+   */
+  reactAliasesEnabled?: boolean;
+};
 
 export const pluginPreact = (
-  _options: PluginPreactOptions = {},
+  options: PluginPreactOptions = {},
 ): RsbuildPlugin => ({
   name: 'rsbuild:preact',
 
   pre: ['rsbuild:swc'],
 
   setup(api) {
+    const { reactAliasesEnabled = true } = options;
+
     api.modifyBundlerChain(async (chain, { CHAIN_ID, isProd }) => {
-      chain.resolve.alias.merge({
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-        'react/jsx-runtime': 'preact/jsx-runtime',
-      });
+      if (reactAliasesEnabled) {
+        chain.resolve.alias.merge({
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+          'react/jsx-runtime': 'preact/jsx-runtime',
+        });
+      }
 
       const reactOptions: SwcReactConfig = {
         development: !isProd,
@@ -25,27 +35,19 @@ export const pluginPreact = (
         importSource: 'preact',
       };
 
-      chain.module
-        .rule(CHAIN_ID.RULE.JS)
-        .use(CHAIN_ID.USE.SWC)
-        .tap((options) => {
-          options.jsc.transform.react = {
-            ...reactOptions,
-          };
-          return options;
-        });
-
-      if (chain.module.rules.has(CHAIN_ID.RULE.JS_DATA_URI)) {
-        chain.module
-          .rule(CHAIN_ID.RULE.JS_DATA_URI)
-          .use(CHAIN_ID.USE.SWC)
-          .tap((options) => {
-            options.jsc.transform.react = {
-              ...reactOptions,
-            };
-            return options;
-          });
-      }
+      [CHAIN_ID.RULE.JS, CHAIN_ID.RULE.JS_DATA_URI].forEach((ruleId) => {
+        if (chain.module.rules.has(ruleId)) {
+          const rule = chain.module.rule(ruleId);
+          if (rule.uses.has(CHAIN_ID.USE.SWC)) {
+            rule.use(CHAIN_ID.USE.SWC).tap((options) => {
+              options.jsc.transform.react = {
+                ...reactOptions,
+              };
+              return options;
+            });
+          }
+        }
+      });
     });
   },
 });
