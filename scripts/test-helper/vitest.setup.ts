03bfa4fc4bdb82916c8b21b2d64b1b4d70288e02
fi3ework <fi3ework@gmail.com>
2024-01-17 02:10:00 +0800

    test: use test suite dirname as cwd

--- scripts/test-helper/vitest.setup.ts
+++ scripts/test-helper/vitest.setup.ts
@@ -1,6 +1,12 @@
 import path from 'path';
-import { expect } from 'vitest';
+import { expect, beforeAll } from 'vitest';
 import { createSnapshotSerializer } from './dist';
+
+beforeAll((suite) => {
+  process.env.REBUILD_TEST_SUITE_CWD = suite.filepath
+    ? path.dirname(suite.filepath)
+    : '';
+});
 
 expect.addSnapshotSerializer(
   createSnapshotSerializer({
