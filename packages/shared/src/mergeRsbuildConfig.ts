94e2a1c37b397d4e574e740d06c7375fc1436f93
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-23 13:54:57 +0800

    chore: improve

--- packages/shared/src/mergeRsbuildConfig.ts
+++ packages/shared/src/mergeRsbuildConfig.ts
@@ -1,62 +1,52 @@
-import { castArray, isFunction, isObject, cloneDeep } from './utils';
+import { castArray, isFunction, isPlainObject } from './utils';
 import type { RsbuildConfig } from './types/config';
+
+const OVERRIDE_PATH = [
+  'performance.removeConsole',
+  'output.inlineScripts',
+  'output.inlineStyles',
+  'output.cssModules.auto',
+  'output.targets',
+  'server.printUrls',
+  'dev.startUrl',
+];
 
 /**
  * When merging configs, some properties prefer `override` over `merge to array`
  */
-const isOverriddenConfigKey = (key: string) =>
-  [
-    'performance.removeConsole',
-    'output.inlineScripts',
-    'output.inlineStyles',
-    'output.cssModules.auto',
-    'output.targets',
-    'server.printUrls',
-    'dev.startUrl',
-  ].includes(key);
-
-function isMergeableObject(value: unknown): value is Record<string, unknown> {
-  if (!isObject(value)) {
-    return false;
-  }
-  const type = Object.prototype.toString.call(value);
-  return type !== '[object RegExp]' && type !== '[object Date]';
-}
-
-const clone = (value: unknown) =>
-  isMergeableObject(value) ? cloneDeep(value) : value;
+const isOverridePath = (key: string) => OVERRIDE_PATH.includes(key);
 
 const merge = (x: unknown, y: unknown, path = '') => {
   // force some keys to override
-  if (isOverriddenConfigKey(path)) {
-    return clone(y ?? x);
+  if (isOverridePath(path)) {
+    return y ?? x;
   }
 
   // ignore undefined property
   if (x === undefined) {
-    return clone(y);
+    return y;
   }
   if (y === undefined) {
-    return clone(x);
+    return x;
   }
 
   const pair = [x, y];
 
   // combine array
   if (pair.some(Array.isArray)) {
-    return [...castArray(clone(x)), ...castArray(clone(y))];
+    return [...castArray(x), ...castArray(y)];
   }
 
   // convert function to chained function
   if (pair.some(isFunction)) {
-    return [clone(x), clone(y)];
+    return pair;
   }
 
-  if (!isMergeableObject(x) || !isMergeableObject(y)) {
+  if (!isPlainObject(x) || !isPlainObject(y)) {
     return y;
   }
 
-  const merged: Record<string, any> = {};
+  const merged: Record<string, unknown> = {};
   const keys = new Set([...Object.keys(x), ...Object.keys(y)]);
 
   keys.forEach((key) => {
@@ -70,12 +60,12 @@ const merge = (x: unknown, y: unknown, path = '') => {
 export const mergeRsbuildConfig = (
   ...configs: RsbuildConfig[]
 ): RsbuildConfig => {
-  if (configs.length < 2) {
-    return configs[0];
-  }
-
   if (configs.length === 2) {
     return merge(configs[0], configs[1]) as RsbuildConfig;
+  }
+
+  if (configs.length < 2) {
+    return configs[0];
   }
 
   return configs.reduce(
