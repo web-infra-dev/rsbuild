b786461d25bc45903603c6f0010e366e4824948b
neverland <chenjiahan.jait@bytedance.com>
2024-01-25 20:40:25 +0800

    docs: fix missing nav icon CSS

--- packages/document/theme/index.tsx
+++ packages/document/theme/index.tsx
@@ -1,5 +1,6 @@
 import Theme from 'rspress/theme';
 import { RsfamilyNavIcon } from 'rsfamily-nav-icon';
+import 'rsfamily-nav-icon/dist/index.css';
 
 const Layout = () => <Theme.Layout beforeNavTitle={<RsfamilyNavIcon />} />;
 
