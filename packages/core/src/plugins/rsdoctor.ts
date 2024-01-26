48c9be39df8c4e66cdaba99d4d64e27339abba20
neverland <chenjiahan.jait@bytedance.com>
2024-01-26 18:11:23 +0800

    chore: upd

--- packages/core/src/plugins/rsdoctor.ts
+++ packages/core/src/plugins/rsdoctor.ts
@@ -1,66 +1,66 @@
 import { color, logger, type BundlerPluginInstance } from '@rsbuild/shared';
 import type { RsbuildPlugin } from '../types';
 
-export function pluginRsdoctor(): RsbuildPlugin {
-  return {
-    name: 'rsbuild:rsdoctor',
+type RsdoctorExports = {
+  RsdoctorRspackPlugin: { new (): BundlerPluginInstance };
+  RsdoctorWebpackPlugin: { new (): BundlerPluginInstance };
+};
 
-    setup(api) {
-      api.onBeforeCreateCompiler(async ({ bundlerConfigs }) => {
-        if (process.env.RSDOCTOR !== 'true') {
-          return;
-        }
+export const pluginRsdoctor = (): RsbuildPlugin => ({
+  name: 'rsbuild:rsdoctor',
 
-        const isRspack = api.context.bundlerType === 'rspack';
-        const packageName = isRspack
-          ? '@rsdoctor/rspack-plugin'
-          : '@rsdoctor/webpack-plugin';
+  setup(api) {
+    api.onBeforeCreateCompiler(async ({ bundlerConfigs }) => {
+      if (process.env.RSDOCTOR !== 'true') {
+        return;
+      }
 
-        let module: {
-          RsdoctorRspackPlugin: { new (): BundlerPluginInstance };
-          RsdoctorWebpackPlugin: { new (): BundlerPluginInstance };
-        };
+      const isRspack = api.context.bundlerType === 'rspack';
+      const packageName = isRspack
+        ? '@rsdoctor/rspack-plugin'
+        : '@rsdoctor/webpack-plugin';
 
-        try {
-          const path = require.resolve(packageName, {
-            paths: [api.context.rootPath],
-          });
-          module = await import(path);
-        } catch (err) {
-          logger.warn(
-            `\`process.env.RSDOCTOR\` enabled, please install ${color.bold(color.yellow(packageName))} package.`,
-          );
-          return;
-        }
+      let module: RsdoctorExports;
 
-        const pluginName = isRspack
-          ? 'RsdoctorRspackPlugin'
-          : 'RsdoctorWebpackPlugin';
-
-        if (!module || !module[pluginName]) {
-          return;
-        }
-
-        let isAutoRegister = false;
-
-        bundlerConfigs.forEach((config) => {
-          const registered = config.plugins?.some(
-            (plugin) => plugin?.constructor?.name === pluginName,
-          );
-
-          if (registered) {
-            return;
-          }
-
-          config.plugins ||= [];
-          config.plugins.push(new module[pluginName]());
-          isAutoRegister = true;
+      try {
+        const path = require.resolve(packageName, {
+          paths: [api.context.rootPath],
         });
+        module = await import(path);
+      } catch (err) {
+        logger.warn(
+          `\`process.env.RSDOCTOR\` enabled, please install ${color.bold(color.yellow(packageName))} package.`,
+        );
+        return;
+      }
 
-        if (isAutoRegister) {
-          logger.info(`${color.bold(color.yellow(packageName))} enabled.`);
+      const pluginName = isRspack
+        ? 'RsdoctorRspackPlugin'
+        : 'RsdoctorWebpackPlugin';
+
+      if (!module || !module[pluginName]) {
+        return;
+      }
+
+      let isAutoRegister = false;
+
+      bundlerConfigs.forEach((config) => {
+        const registered = config.plugins?.some(
+          (plugin) => plugin?.constructor?.name === pluginName,
+        );
+
+        if (registered) {
+          return;
         }
+
+        config.plugins ||= [];
+        config.plugins.push(new module[pluginName]());
+        isAutoRegister = true;
       });
-    },
-  };
-}
+
+      if (isAutoRegister) {
+        logger.info(`${color.bold(color.yellow(packageName))} enabled.`);
+      }
+    });
+  },
+});
