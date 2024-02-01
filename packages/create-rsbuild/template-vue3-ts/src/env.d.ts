521b9022511127af2d94362527b3749ab2c98143
southorange0929 <southorange0929@foxmail.com>
2024-02-01 17:41:12 +0800

    fix: biome lint rule error

--- packages/create-rsbuild/template-vue3-ts/src/env.d.ts
+++ packages/create-rsbuild/template-vue3-ts/src/env.d.ts
@@ -3,6 +3,7 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
+  // biome-ignore lint/complexity/noBannedTypes: reason
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
