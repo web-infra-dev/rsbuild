546eef6ad592c156379ddcbc7313db8d5c7714fe
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-02-19 11:43:36 +0800

    chore: style-loader esModule default true

--- e2e/cases/css/style-loader/src/index.ts
+++ e2e/cases/css/style-loader/src/index.ts
@@ -7,3 +7,11 @@ if (container) {
   const root = createRoot(container);
   root.render(React.createElement(App));
 }
+
+const num = Math.ceil(Math.random() * 100);
+const testEl = document.createElement('div');
+testEl.id = 'test-keep';
+
+testEl.innerHTML = String(num);
+
+document.body.appendChild(testEl);
