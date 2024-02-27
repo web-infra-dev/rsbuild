a14eed3921561da79adec316dd689fce57e2dab5
neverland <chenjiahan.jait@bytedance.com>
2024-02-27 17:45:52 +0800

    chore: add test case

--- e2e/cases/html/cross-origin/index.test.ts
+++ e2e/cases/html/cross-origin/index.test.ts
@@ -34,6 +34,26 @@ test('should not apply crossOrigin when same origin', async () => {
   expect(html).not.toContain('crossorigin');
 });
 
+test('should not apply crossOrigin if assetPrefix is auto', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    rsbuildConfig: {
+      html: {
+        scriptLoading: 'blocking',
+        crossorigin: 'anonymous',
+      },
+      output: {
+        assetPrefix: 'auto',
+      },
+    },
+  });
+  const files = await rsbuild.unwrapOutputJSON();
+  const html =
+    files[Object.keys(files).find((file) => file.endsWith('index.html'))!];
+
+  expect(html).not.toContain('crossorigin');
+});
+
 test('should apply crossOrigin when crossorigin is "anonymous" and not same origin', async () => {
   const rsbuild = await build({
     cwd: __dirname,
