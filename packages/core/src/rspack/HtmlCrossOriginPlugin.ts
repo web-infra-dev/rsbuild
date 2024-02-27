0b3be55cb35a98b1b75d830a4912adc31b2271ef
neverland <chenjiahan.jait@bytedance.com>
2024-02-27 17:38:01 +0800

    chore: update

--- packages/core/src/rspack/HtmlCrossOriginPlugin.ts
+++ packages/core/src/rspack/HtmlCrossOriginPlugin.ts
@@ -1,4 +1,4 @@
-import { getPublicPathFromCompiler, type CrossOrigin } from '@rsbuild/shared';
+import { DEFAULT_ASSET_PREFIX, type CrossOrigin } from '@rsbuild/shared';
 import type { Compiler, RspackPluginInstance } from '@rspack/core';
 import { getHTMLPlugin } from '../provider/htmlPluginUtil';
 
@@ -22,11 +22,16 @@ export class HtmlCrossOriginPlugin implements RspackPluginInstance {
       return;
     }
 
-    // align with webpack crossOriginLoading logic
+    // no need to set crossorigin="anonymous" if asset domain is the same as
+    // the page domain, align with webpack's crossOriginLoading logic.
     // see: https://github.com/webpack/webpack/blob/611bded369b5a9ea695f2669b25a6f88b67d7826/lib/runtime/LoadScriptRuntimeModule.js#L100-L111
     if (this.crossOrigin !== 'use-credentials') {
-      const publicPath = getPublicPathFromCompiler(compiler);
-      if (publicPath === '' || publicPath === '/') {
+      const { publicPath } = compiler.options.output;
+      if (
+        !publicPath ||
+        publicPath === DEFAULT_ASSET_PREFIX ||
+        publicPath === 'auto'
+      ) {
         return;
       }
     }
