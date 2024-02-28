98d3bf4c64a855aeea66cae0bab345a7601cc1db
neverland <chenjiahan.jait@bytedance.com>
2024-02-28 10:27:36 +0800

    chore: fix

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginRem } from '@rsbuild/plugin-rem';
 
 export default defineConfig({
-  plugins: [
-    pluginReact(),
-    pluginRem({
-      inlineRuntime: false,
-    }),
-  ],
-  html: {
-    crossorigin: 'use-credentials',
-  },
+  plugins: [pluginReact()],
 });
