fccace0582cb30fb79434f9fad452fc64e82ebe2
neverland <chenjiahan.jait@bytedance.com>
2024-02-28 14:11:18 +0800

    test(e2e): add test case for plugin-eslint

---
+++ e2e/cases/eslint/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  rules: {
+    'no-undef': 'error',
+  },
+};
