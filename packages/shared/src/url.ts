98721638bddbb00b4f38af2586d501ea79021a3f
gaoyuan <gaoyuan.1226@bytedance.com>
2024-03-07 20:07:06 +0800

    docs: add ipv6 setup guide (#1759)

--- packages/shared/src/url.ts
+++ packages/shared/src/url.ts
@@ -63,7 +63,7 @@ const getIpv4Interfaces = () => {
 };
 
 const isLoopbackHost = (host: string) => {
-  const loopbackHosts = ['localhost', '127.0.0.1', '::1'];
+  const loopbackHosts = ['localhost', '127.0.0.1', '::1', '0000:0000:0000:0000:0000:0000:0000:0001'];
   return loopbackHosts.includes(host);
 };
 
