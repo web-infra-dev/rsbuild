1880d07d9f0b9bb7b5bcef0c56337aca3c7a6e10
neverland <chenjiahan.jait@bytedance.com>
2024-03-08 21:54:50 +0800

    chore: fix

--- packages/core/src/rspack/nodeAddonsLoader.ts
+++ packages/core/src/rspack/nodeAddonsLoader.ts
@@ -2,7 +2,7 @@ import path from 'node:path';
 import type { Rspack } from '@rsbuild/shared';
 
 const getFilename = (resourcePath: string) => {
-  let basename = 'file';
+  let basename = '';
 
   if (resourcePath) {
     const parsed = path.parse(resourcePath);
@@ -11,20 +11,28 @@ const getFilename = (resourcePath: string) => {
     }
   }
 
-  return `${basename}.node`;
+  if (basename) {
+    return `${basename}.node`;
+  }
+
+  return null;
 };
 
 export default function (this: Rspack.LoaderContext, source: string) {
   const name = getFilename(this.resourcePath);
+
+  if (name === null) {
+    throw new Error(`Failed to load Node.js addon: "${this.resourcePath}"`);
+  }
+
   this.emitFile(name, source);
 
   return `
 try {
-  process.dlopen(module, __dirname + require("path").sep + __webpack_public_path__ + ${JSON.stringify(
-    name,
-  )});
+  const path = require("path");
+  process.dlopen(module, path.join(__dirname, "${name}"));
 } catch (error) {
-  throw new Error('node-loader:\\n' + error);
+  throw new Error('Failed to load Node.js addon: "${name}"\\n' + error);
 }
 `;
 }
