eb4ee2d06719e2eb631cc34f08d9cc2337de8b0d
neverland <chenjiahan.jait@bytedance.com>
2024-03-08 22:25:43 +0800

    fix: use raw loader to handle Node addons

--- e2e/cases/node-addons/index.test.ts
+++ e2e/cases/node-addons/index.test.ts
@@ -1,5 +1,7 @@
+import fs from 'node:fs';
 import { expect, test } from '@playwright/test';
 import { build } from '@e2e/helper';
+import { join } from 'node:path';
 
 test('should compile Node addons correctly', async () => {
   const rsbuild = await build({
@@ -9,4 +11,11 @@ test('should compile Node addons correctly', async () => {
   const addonFile = Object.keys(files).find((file) => file.endsWith('a.node'));
 
   expect(addonFile?.includes('server/a.node')).toBeTruthy();
+
+  expect(
+    fs.existsSync(join(__dirname, 'dist', 'server', 'a.node')),
+  ).toBeTruthy();
+
+  const content = await import('./dist/server/index.js');
+  expect(typeof content.default.readLength).toEqual('function');
 });
