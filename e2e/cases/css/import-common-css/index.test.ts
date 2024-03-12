000db5619ee744486fe7ecf1c457b8cabc32437c
neverland <chenjiahan.jait@bytedance.com>
2024-03-12 19:24:23 +0800

    test(e2e): proxy tilde deprecation warning

--- e2e/cases/css/import-common-css/index.test.ts
+++ e2e/cases/css/import-common-css/index.test.ts
@@ -1,7 +1,9 @@
 import { expect, test } from '@playwright/test';
-import { build } from '@e2e/helper';
+import { build, proxyConsole } from '@e2e/helper';
 
 test('should compile common css import correctly', async () => {
+  const { restore, logs } = proxyConsole();
+
   const rsbuild = await build({
     cwd: __dirname,
     rsbuildConfig: {
@@ -16,7 +18,14 @@ test('should compile common css import correctly', async () => {
   const files = await rsbuild.unwrapOutputJSON();
   const cssFiles = Object.keys(files).find((file) => file.endsWith('.css'))!;
 
+  // there will be a deprecation log for `~`.
+  expect(
+    logs.some((log) => log.includes(`a request starts with '~' is deprecated`)),
+  );
+
   expect(files[cssFiles]).toEqual(
     'html{min-height:100%}#a{color:red}#b{color:blue}',
   );
+
+  restore();
 });
