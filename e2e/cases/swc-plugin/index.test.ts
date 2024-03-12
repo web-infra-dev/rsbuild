f865840b9bbbe51563b028647ab316ebe023fba6
neverland <chenjiahan.jait@bytedance.com>
2024-03-12 18:45:33 +0800

    chore: skip

--- e2e/cases/swc-plugin/index.test.ts
+++ e2e/cases/swc-plugin/index.test.ts
@@ -2,37 +2,35 @@ import { expect, test } from '@playwright/test';
 import { dev, build, gotoPage } from '@e2e/helper';
 
 // TODO: https://github.com/web-infra-dev/rspack/issues/5921
-test.fail(
-  'should run swc warm plugin correctly in dev mode',
-  async ({ page }) => {
-    const rsbuild = await dev({
-      cwd: __dirname,
-    });
+test.skip('should run swc warm plugin correctly in dev mode', async ({
+  page,
+}) => {
+  const rsbuild = await dev({
+    cwd: __dirname,
+  });
 
-    const msgPromise = page.waitForEvent('console');
-    await gotoPage(page, rsbuild);
+  const msgPromise = page.waitForEvent('console');
+  await gotoPage(page, rsbuild);
 
-    const msg = await msgPromise;
-    expect(await msg.args()[0].jsonValue()).toEqual('this is error');
+  const msg = await msgPromise;
+  expect(await msg.args()[0].jsonValue()).toEqual('this is error');
 
-    await rsbuild.close();
-  },
-);
+  await rsbuild.close();
+});
 
-test.fail(
-  'should run swc warm plugin correctly in production build',
-  async ({ page }) => {
-    const rsbuild = await build({
-      cwd: __dirname,
-      runServer: true,
-    });
+test.skip('should run swc warm plugin correctly in production build', async ({
+  page,
+}) => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    runServer: true,
+  });
 
-    const msgPromise = page.waitForEvent('console');
-    await gotoPage(page, rsbuild);
+  const msgPromise = page.waitForEvent('console');
+  await gotoPage(page, rsbuild);
 
-    const msg = await msgPromise;
-    expect(await msg.args()[0].jsonValue()).toEqual('this is error');
+  const msg = await msgPromise;
+  expect(await msg.args()[0].jsonValue()).toEqual('this is error');
 
-    await rsbuild.close();
-  },
-);
+  await rsbuild.close();
+});
