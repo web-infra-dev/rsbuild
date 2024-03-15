24080cfe113be4dacab62bb62133015378a8c652
neverland <chenjiahan.jait@bytedance.com>
2024-03-15 11:18:20 +0800

    feat: enable new tree shaking in production

--- packages/core/src/provider/plugins/transition.ts
+++ packages/core/src/provider/plugins/transition.ts
@@ -6,11 +6,22 @@ import type { RsbuildPlugin } from '../../types';
 export const pluginTransition = (): RsbuildPlugin => ({
   name: 'rsbuild:transition',
 
-  setup() {
+  setup(api) {
     process.env.RSPACK_CONFIG_VALIDATE = 'loose-silent';
 
     // improve kill process performance
     // https://github.com/web-infra-dev/rspack/pull/5486
     process.env.WATCHPACK_WATCHER_LIMIT ||= '20';
+
+    api.modifyRspackConfig((config, { isProd }) => {
+      // only enable new tree shaking in production build,
+      // because Rspack still has some problems when using it in dev mode.
+      // such as https://github.com/web-infra-dev/rspack/issues/5887
+      if (isProd) {
+        config.experiments ||= {};
+        config.experiments.rspackFuture ||= {};
+        config.experiments.rspackFuture.newTreeshaking = true;
+      }
+    });
   },
 });
