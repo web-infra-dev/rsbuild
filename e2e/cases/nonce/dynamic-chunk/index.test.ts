595725dc539b14c99926bef64382e6f6cf6aab94
neverland <chenjiahan.jait@bytedance.com>
2024-03-21 11:10:39 +0800

    fix: apply nonce to dynamic chunks (#1906)

---
+++ e2e/cases/nonce/dynamic-chunk/index.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+import { dev, build, gotoPage } from '@e2e/helper';
+
+test('should apply nonce to dynamic chunks in dev build', async ({ page }) => {
+  const rsbuild = await dev({
+    cwd: __dirname,
+  });
+
+  await gotoPage(page, rsbuild);
+  expect(await page.evaluate('window.dynamicChunkNonce')).toEqual(
+    'this-is-nonce',
+  );
+
+  await rsbuild.close();
+});
+
+test('should apply nonce to dynamic chunks in prod build', async ({ page }) => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    runServer: true,
+  });
+
+  await gotoPage(page, rsbuild);
+  expect(await page.evaluate('window.dynamicChunkNonce')).toEqual(
+    'this-is-nonce',
+  );
+
+  await rsbuild.close();
+});
