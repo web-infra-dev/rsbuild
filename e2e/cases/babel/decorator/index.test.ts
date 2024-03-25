6cdf662280ef49422ad3604a5655079c0dff5bba
sunyiteng <sunyiteng@bytedance.com>
2024-03-25 19:55:54 +0800

    test: add test case of user @babel/preset-env

--- e2e/cases/babel/decorator/index.test.ts
+++ e2e/cases/babel/decorator/index.test.ts
@@ -28,7 +28,43 @@ test('should support legacy decorators and source.decorators.version in JavaScri
     rsbuildConfig: {
       source: {
         entry: {
-          index: './src/_js.js',
+          index: './src/js-index.js',
+        },
+      },
+    },
+  });
+
+  await gotoPage(page, rsbuild);
+  expect(await page.evaluate('window.aaa')).toBe('hello');
+  expect(await page.evaluate('window.bbb')).toBe('world');
+  expect(await page.evaluate('window.FooService')).toBeTruthy();
+  await rsbuild.close();
+});
+
+test('should work together with user custom @babel/preset-env config', async ({
+  page,
+}) => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    runServer: true,
+    plugins: [
+      pluginBabel({
+        babelLoaderOptions(_, { addPresets }) {
+          addPresets([
+            [
+              '@babel/preset-env',
+              {
+                include: ['@babel/plugin-transform-class-properties'],
+              },
+            ],
+          ]);
+        },
+      }),
+    ],
+    rsbuildConfig: {
+      source: {
+        entry: {
+          index: './src/js-index.js',
         },
       },
     },
