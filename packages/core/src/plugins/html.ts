d7aa2275b2823c2211d6395a628aac0c45223217
neverland <chenjiahan.jait@bytedance.com>
2024-04-02 17:11:05 +0800

    chore: remove ts-expect-error comment for dependOn (#1979)

--- packages/core/src/plugins/html.ts
+++ packages/core/src/plugins/html.ts
@@ -161,8 +161,11 @@ function getChunks(
   entryValue: string | string[] | EntryDescription,
 ): string[] {
   if (isPlainObject(entryValue)) {
-    // @ts-expect-error Rspack do not support dependOn yet
-    const { dependOn } = entryValue as EntryDescription;
+    const { dependOn } = entryValue as EntryDescription & {
+      // TODO: remove this after bumping Rspack v0.6
+      // https://github.com/web-infra-dev/rspack/pull/6069
+      dependOn?: string | string[] | undefined;
+    };
     if (Array.isArray(dependOn)) {
       return [...dependOn, entryName];
     }
