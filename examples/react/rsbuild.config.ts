f52a133a1d8faf008214d52702775ffa8d842d4a
chenjiahan <chenjiahan.jait@bytedance.com>
2024-04-04 20:38:15 +0800

    types: export more hook fn types

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,22 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  tools: {
-    rspack: {
-      module: {
-        rules: [
-          {
-            issuer: {
-              and: [
-                '<ROOT>/packages/core/tests',
-                {
-                  not: /\\[\\\\\\\\/\\]node_modules\\[\\\\\\\\/\\]/,
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  },
 });
