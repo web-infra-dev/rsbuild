46ca8b78e230eef7fec81c92a9f58fba86c286ab
neverland <chenjiahan.jait@bytedance.com>
2024-04-10 12:16:22 +0800

    test(e2e): fix unstable server.port case (#2053)

---
+++ e2e/cases/server/port/index.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+import { dev, getRandomPort, gotoPage } from '@e2e/helper';
+
+test('should allow to set port via server.port', async ({ page }) => {
+  const errors: string[] = [];
+  page.on('pageerror', (err) => errors.push(err.message));
+
+  const port = await getRandomPort();
+  const rsbuild = await dev({
+    cwd: __dirname,
+    rsbuildConfig: {
+      server: {
+        port,
+      },
+    },
+  });
+
+  await gotoPage(page, rsbuild);
+
+  expect(rsbuild.port).toBe(port);
+
+  const locator = page.locator('#test');
+  await expect(locator).toHaveText('Hello Rsbuild!');
+
+  await rsbuild.close();
+
+  expect(errors).toEqual([]);
+});
