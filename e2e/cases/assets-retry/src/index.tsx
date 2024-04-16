7ca2b69e37555a638453b6e51847264c2482de34
SoonIter <sooniter@gmail.com>
2024-04-16 19:20:21 +0800

    fix(e2e): remove unnecessary code

--- e2e/cases/assets-retry/src/index.tsx
+++ e2e/cases/assets-retry/src/index.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-async function main() {
-  const root = ReactDOM.createRoot(document.getElementById('root')!);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-}
-
-main()
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
