24d8fd4e8df66d46d6eef0ac969735e374f8dbed
SoonIter <sooniter@gmail.com>
2024-04-16 21:37:30 +0800

    label -> tag

--- packages/plugin-assets-retry/src/runtime/index.ts
+++ packages/plugin-assets-retry/src/runtime/index.ts
@@ -103,7 +103,7 @@ function createElement(
     return {
       element: script,
       str:
-        // biome-ignore lint/style/useTemplate: use "</" + "script>" instead of script label to avoid syntax error when inlining in html
+        // biome-ignore lint/style/useTemplate: use "</" + "script>" instead of script tag to avoid syntax error when inlining in html
         `<script src="${attributes.url}" ${crossOriginAttr} ${retryTimesAttr} ${isAsyncAttr}>` +
         '</' +
         'script>',
