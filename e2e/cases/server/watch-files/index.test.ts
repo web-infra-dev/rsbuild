761e9683508fb20711cecc883d6543666a4c82a8
liyin <liyincode@gmail.com>
2024-04-17 19:37:16 +0800

    test(dev-watchFiles): add e2e test

---
+++ e2e/cases/server/watch-files/index.test.ts
@@ -0,0 +1,29 @@
+import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
+import { fse } from '@rsbuild/shared';
+import path from 'node:path';
+
+rspackOnlyTest(
+  'page should reload when file content is changed',
+  async ({ page }) => {
+    const file = path.join(__dirname, '/assets/example.txt');
+    const rsbuild = await dev({
+      cwd: __dirname,
+      rsbuildConfig: {
+        dev: {
+          watchFiles: file,
+        },
+      },
+    });
+    await gotoPage(page, rsbuild);
+
+    await fse.writeFile(file, 'test');
+
+    // check the page is reloaded
+    await page.waitForURL(`${rsbuild.urls[0]}/index.html`);
+
+    // reset file
+    fse.truncateSync(file);
+
+    await rsbuild.close();
+  },
+);
