60e7455cead078ffc4d36e7d4e92f60fc6f6bb68
xc2 <xc2@users.noreply.github.com>
2024-04-19 00:00:56 +0800

    test: add style test to vue2 case folder for testing loaders duplicating

--- e2e/cases/vue2/sfc-style/src/App.vue
+++ e2e/cases/vue2/sfc-style/src/App.vue
@@ -12,3 +12,11 @@ export default {};
   background-color: green;
 }
 </style>
+
+<style lang="less">
+body {
+  // this case is to test if loaders run twice
+  // if the next line is transformed twice, less will throw.
+  padding: -webkit-calc(~'16px + env(safe-area-inset-bottom)');
+}
+</style>
