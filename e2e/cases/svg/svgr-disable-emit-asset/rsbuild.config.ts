855be8c97e4b707dddf67809cc54e979d82209cc
neverland <chenjiahan.jait@bytedance.com>
2024-04-18 19:12:36 +0800

    fix: failed to disable emitAssets when using SVGR

---
+++ e2e/cases/svg/svgr-disable-emit-asset/rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginSvgr({
+      svgrOptions: {
+        exportType: 'default',
+      },
+    }),
+  ],
+  output: {
+    filenameHash: false,
+    emitAssets: () => false,
+  },
+});
