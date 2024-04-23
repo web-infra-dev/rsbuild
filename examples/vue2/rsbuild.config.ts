15a233a6b8a7a65ed4b7c2afdd8ad321ae06bd39
liyin <liyincode@gmail.com>
2024-04-23 15:15:51 +0800

    fix(server-publicDir-watch): remove debug code

--- examples/vue2/rsbuild.config.ts
+++ examples/vue2/rsbuild.config.ts
@@ -3,10 +3,4 @@ import { pluginVue2 } from '@rsbuild/plugin-vue2';
 
 export default defineConfig({
   plugins: [pluginVue2()],
-  server: {
-    publicDir: {
-      name: 'public1',
-      watch: true,
-    },
-  },
 });
