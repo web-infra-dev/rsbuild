8df0d163620410f37dc462a9ee6cdf1f26a309c6
neverland <chenjiahan.jait@bytedance.com>
2024-04-25 19:04:05 +0800

    feat: transform API supports targets filter

---
+++ e2e/cases/plugin-api/plugin-transform-by-targets/index.test.ts
@@ -0,0 +1,23 @@
+import { build } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+
+test('should allow plugin to transform code by targets', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+  const webJs = Object.keys(files).find(
+    (file) =>
+      file.includes('index') &&
+      !file.includes('server') &&
+      file.endsWith('.js'),
+  );
+  const nodeJs = Object.keys(files).find(
+    (file) =>
+      file.includes('index') && file.includes('server') && file.endsWith('.js'),
+  );
+
+  expect(files[webJs!].includes('target is web')).toBeTruthy();
+  expect(files[nodeJs!].includes('target is node')).toBeTruthy();
+});
