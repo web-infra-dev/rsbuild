54f532fd09ad37f03884ac9b610eeba4e31b589c
neverland <chenjiahan.jait@bytedance.com>
2024-05-17 15:47:34 +0800

    feat(plugin-vue): support CSS Modules in SFC (#2374)

---
+++ e2e/cases/vue/sfc-css-modules/index.test.ts
@@ -0,0 +1,24 @@
+import { build, gotoPage, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest(
+  'should build Vue sfc with CSS Modules correctly',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      runServer: true,
+    });
+
+    await gotoPage(page, rsbuild);
+
+    const test1 = page.locator('#test1');
+    const test2 = page.locator('#test2');
+    const test3 = page.locator('#test3');
+
+    await expect(test1).toHaveCSS('color', 'rgb(255, 0, 0)');
+    await expect(test2).toHaveCSS('color', 'rgb(0, 0, 255)');
+    await expect(test3).toHaveCSS('color', 'rgb(0, 128, 0)');
+
+    await rsbuild.close();
+  },
+);
