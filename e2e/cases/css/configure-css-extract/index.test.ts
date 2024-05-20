273fbfd9f68f0bfba7894b8a5ed4715031e23248
neverland <chenjiahan.jait@bytedance.com>
2024-05-20 17:02:28 +0800

    test: add e2e

---
+++ e2e/cases/css/configure-css-extract/index.test.ts
@@ -0,0 +1,13 @@
+import { build } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+
+test('should allow to configure options of CSSExtractPlugin', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+  });
+  const files = await rsbuild.unwrapOutputJSON();
+  const content =
+    files[Object.keys(files).find((file) => file.endsWith('my-css.css'))!];
+
+  expect(content).toEqual('body{color:red}');
+});
