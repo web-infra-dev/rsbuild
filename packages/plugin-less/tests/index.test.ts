d51012331a84f9e8c8c69f95d75d4bbd3afb9225
neverland <chenjiahan.jait@bytedance.com>
2024-05-22 18:49:20 +0800

    test(ut): reuse the matchRules helper

--- packages/plugin-less/tests/index.test.ts
+++ packages/plugin-less/tests/index.test.ts
@@ -1,20 +1,23 @@
-import { createStubRsbuild } from '@scripts/test-helper';
+import { createRsbuild } from '@rsbuild/core';
+import { matchRules } from '@scripts/test-helper';
 import { pluginLess } from '../src';
 
 describe('plugin-less', () => {
   it('should add less-loader', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [pluginLess()],
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        plugins: [pluginLess()],
+      },
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.less')).toMatchSnapshot();
   });
 
   it('should add less-loader and css-loader when injectStyles', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [pluginLess()],
+    const rsbuild = await createRsbuild({
       rsbuildConfig: {
+        plugins: [pluginLess()],
         output: {
           injectStyles: true,
         },
@@ -22,38 +25,42 @@ describe('plugin-less', () => {
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.less')).toMatchSnapshot();
   });
 
   it('should add less-loader with tools.less', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [
-        pluginLess({
-          lessLoaderOptions: {
-            lessOptions: {
-              javascriptEnabled: false,
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        plugins: [
+          pluginLess({
+            lessLoaderOptions: {
+              lessOptions: {
+                javascriptEnabled: false,
+              },
             },
-          },
-        }),
-      ],
+          }),
+        ],
+      },
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.less')).toMatchSnapshot();
   });
 
   it('should add less-loader with excludes', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [
-        pluginLess({
-          lessLoaderOptions(_config, { addExcludes }) {
-            addExcludes(/node_modules/);
-          },
-        }),
-      ],
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        plugins: [
+          pluginLess({
+            lessLoaderOptions(_config, { addExcludes }) {
+              addExcludes(/node_modules/);
+            },
+          }),
+        ],
+      },
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.less')).toMatchSnapshot();
   });
 });
--- packages/plugin-sass/tests/index.test.ts
+++ packages/plugin-sass/tests/index.test.ts
@@ -1,23 +1,23 @@
-import { createStubRsbuild } from '@scripts/test-helper';
+import { createRsbuild } from '@rsbuild/core';
+import { matchRules } from '@scripts/test-helper';
 import { pluginSass } from '../src';
 
 describe('plugin-sass', () => {
   it('should add sass-loader', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [pluginSass()],
+    const rsbuild = await createRsbuild({
       rsbuildConfig: {
-        tools: {},
+        plugins: [pluginSass()],
       },
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.scss')).toMatchSnapshot();
   });
 
   it('should add sass-loader and css-loader when injectStyles', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [pluginSass()],
+    const rsbuild = await createRsbuild({
       rsbuildConfig: {
+        plugins: [pluginSass()],
         output: {
           injectStyles: true,
         },
@@ -25,21 +25,23 @@ describe('plugin-sass', () => {
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.scss')).toMatchSnapshot();
   });
 
   it('should add sass-loader with excludes', async () => {
-    const rsbuild = await createStubRsbuild({
-      plugins: [
-        pluginSass({
-          sassLoaderOptions(_config, { addExcludes }) {
-            addExcludes(/node_modules/);
-          },
-        }),
-      ],
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        plugins: [
+          pluginSass({
+            sassLoaderOptions(_config, { addExcludes }) {
+              addExcludes(/node_modules/);
+            },
+          }),
+        ],
+      },
     });
 
     const bundlerConfigs = await rsbuild.initConfigs();
-    expect(bundlerConfigs[0]).toMatchSnapshot();
+    expect(matchRules(bundlerConfigs[0], 'a.scss')).toMatchSnapshot();
   });
 });
