61d8b2987c1ce874bec49b6d21b4caad1523cbdf
neverland <chenjiahan.jait@bytedance.com>
2024-05-22 14:05:25 +0800

    test: fix e2e

--- e2e/cases/css/lightningcss/rsbuild.config.ts
+++ e2e/cases/css/lightningcss/rsbuild.config.ts
@@ -1,5 +1,6 @@
 import { pluginLess } from '@rsbuild/plugin-less';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default {
-  plugins: [pluginLess()],
+  plugins: [pluginLess(), pluginSass()],
 };
--- e2e/cases/css/style-loader/rsbuild.config.ts
+++ e2e/cases/css/style-loader/rsbuild.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
   output: {
     injectStyles: true,
   },
-  plugins: [pluginReact(), pluginLess()],
+  plugins: [pluginReact(), pluginLess(), pluginSass()],
 });
