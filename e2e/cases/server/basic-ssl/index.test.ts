f20eabf31a69f080a56ee13149458a046579cbf9
chenjiahan <chenjiahan.jait@bytedance.com>
2024-05-23 22:31:45 +0800

    test: fix

--- e2e/cases/server/basic-ssl/index.test.ts
+++ e2e/cases/server/basic-ssl/index.test.ts
@@ -3,22 +3,22 @@ import { expect, test } from '@playwright/test';
 
 const cwd = __dirname;
 
-test('print HTTPS server URLs when use @rsbuild/plugin-basic-ssl', async ({
-  page,
-}) => {
+test('print HTTPS server URLs when use @rsbuild/plugin-basic-ssl', async () => {
   const { logs, restore } = proxyConsole('log');
 
   const rsbuild = await dev({
     cwd,
   });
 
-  await page.goto(`http://localhost:${rsbuild.port}`);
+  await new Promise((resolve) => {
+    rsbuild.instance.onDevCompileDone(resolve);
+  });
 
   const localLog = logs.find(
-    (log) => log.includes('Local:') && log.includes('http2://localhost'),
+    (log) => log.includes('Local:') && log.includes('https://localhost'),
   );
   const networkLog = logs.find(
-    (log) => log.includes('Network:') && log.includes('http2://'),
+    (log) => log.includes('Network:') && log.includes('https://'),
   );
 
   expect(localLog).toBeTruthy();
