75c38fcb3aca371d11fb7fa0f5348bf5125b7b5a
chenjiahan <chenjiahan.jait@bytedance.com>
2024-05-30 22:19:28 +0800

    feat(typed-css-modules): support for named export

--- e2e/cases/css/css-modules-dom/index.test.ts
+++ e2e/cases/css/css-modules-dom/index.test.ts
@@ -1,45 +1,9 @@
-import { join, resolve } from 'node:path';
+import { resolve } from 'node:path';
 import { build, gotoPage } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
-import { fse } from '@rsbuild/shared';
 
 const fixtures = resolve(__dirname);
-
-test('plugin-typed-css-modules', async () => {
-  fse.removeSync(join(fixtures, 'src/App.module.less.d.ts'));
-  fse.removeSync(join(fixtures, 'src/App.module.scss.d.ts'));
-
-  await build({
-    cwd: fixtures,
-    plugins: [pluginReact(), pluginTypedCSSModules()],
-  });
-
-  expect(
-    fse.existsSync(join(fixtures, 'src/App.module.less.d.ts')),
-  ).toBeTruthy();
-
-  expect(
-    fse
-      .readFileSync(join(fixtures, 'src/App.module.less.d.ts'), {
-        encoding: 'utf-8',
-      })
-      .includes('title: string;'),
-  ).toBeTruthy();
-
-  expect(
-    fse.existsSync(join(fixtures, 'src/App.module.scss.d.ts')),
-  ).toBeTruthy();
-
-  expect(
-    fse
-      .readFileSync(join(fixtures, 'src/App.module.scss.d.ts'), {
-        encoding: 'utf-8',
-      })
-      .includes('header: string;'),
-  ).toBeTruthy();
-});
 
 test('injectStyles', async ({ page }) => {
   const rsbuild = await build({
