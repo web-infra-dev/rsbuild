fa1aedf8251511460e0c44717f1be82f46e155da
chenjiahan <chenjiahan.jait@bytedance.com>
2024-06-03 22:23:39 +0800

    feat: update

--- packages/core/src/plugins/sri.ts
+++ packages/core/src/plugins/sri.ts
@@ -5,6 +5,7 @@ import {
   type SriAlgorithm,
   type SriOptions,
   isDev,
+  isHtmlDisabled,
   removeLeadingSlash,
 } from '@rsbuild/shared';
 import type { RsbuildPlugin } from '../types';
@@ -25,7 +26,7 @@ const getAssetName = (url: string, assetPrefix: string) => {
   return removeLeadingSlash(url);
 };
 
-const getAssetContent = (
+const getAsset = (
   url: string,
   assetPrefix: string,
   compilation: Rspack.Compilation,
@@ -41,66 +42,145 @@ const getAssetContent = (
     return null;
   }
 
-  return source.buffer() as Buffer;
+  return {
+    name: assetName,
+    content: source.buffer() as Buffer,
+  };
 };
 
 export const pluginSri = (): RsbuildPlugin => ({
   name: 'rsbuild:sri',
 
   setup(api) {
+    const integrityMap = new Map<
+      // asset name
+      string,
+      {
+        content: Buffer;
+        integrity: string;
+        htmlFiles: string[];
+      }
+    >();
+
+    const getAlgorithm = () => {
+      const config = api.getNormalizedConfig();
+      const { sri } = config.security;
+
+      if (
+        sri === false ||
+        (sri === true && isDev()) ||
+        (typeof sri === 'object' && !sri.enable)
+      ) {
+        return null;
+      }
+
+      const { algorithm = 'sha384' }: SriOptions = sri === true ? {} : sri;
+
+      return algorithm;
+    };
+
     api.modifyHTMLTags({
       // ensure `sri` can be applied to all tags
       order: 'post',
-      handler(tags, { compilation, assetPrefix }) {
-        const config = api.getNormalizedConfig();
-        const { sri } = config.security;
+      handler(tags, { compilation, assetPrefix, filename }) {
+        const algorithm = getAlgorithm();
 
-        if (
-          sri === false ||
-          (sri === true && isDev()) ||
-          (typeof sri === 'object' && !sri.enable)
-        ) {
+        if (!algorithm) {
           return tags;
         }
 
-        const { algorithm = 'sha384' }: SriOptions = sri === true ? {} : sri;
         const allTags = [...tags.headTags, ...tags.bodyTags];
 
         for (const tag of allTags) {
-          if (tag.tag === 'script' && typeof tag.attrs?.src === 'string') {
-            const content = getAssetContent(
-              tag.attrs.src,
-              assetPrefix,
-              compilation,
-            );
+          let url = '';
 
-            if (!content) {
-              continue;
-            }
+          if (!tag.attrs) {
+            continue;
+          }
 
-            tag.attrs.integrity ??= getIntegrity(algorithm, content);
+          if (tag.tag === 'script' && typeof tag.attrs.src === 'string') {
+            url = tag.attrs.src;
           } else if (
             tag.tag === 'link' &&
-            tag.attrs &&
             tag.attrs.rel === 'stylesheet' &&
             typeof tag.attrs.href === 'string'
           ) {
-            const content = getAssetContent(
-              tag.attrs.href,
-              assetPrefix,
-              compilation,
-            );
-
-            if (!content) {
-              continue;
-            }
-
-            tag.attrs.integrity ??= getIntegrity(algorithm, content);
+            url = tag.attrs.href;
           }
+
+          if (!url) {
+            continue;
+          }
+
+          const asset = getAsset(url, assetPrefix, compilation);
+
+          if (!asset) {
+            continue;
+          }
+
+          const cached = integrityMap.get(asset.name);
+          if (cached) {
+            if (!cached.htmlFiles.includes(filename)) {
+              integrityMap.set(asset.name, {
+                ...cached,
+                htmlFiles: [...cached.htmlFiles, filename],
+              });
+            }
+            continue;
+          }
+
+          const integrity = getIntegrity(algorithm, asset.content);
+          tag.attrs.integrity ??= integrity;
+
+          integrityMap.set(asset.name, {
+            content: asset.content,
+            integrity,
+            htmlFiles: [filename],
+          });
         }
 
         return tags;
       },
+    });
+
+    class SriUpdateIntegrityPlugin {
+      apply(compiler: Rspack.Compiler) {
+        compiler.hooks.compilation.tap(
+          'SriUpdateIntegrityPlugin',
+          (compilation) => {
+            compilation.hooks.processAssets.tapPromise(
+              {
+                name: 'SriUpdateIntegrityPlugin',
+                // use to final stage to get the final asset content
+                stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
+              },
+              async (assets) => {
+                const algorithm = getAlgorithm();
+
+                if (!algorithm) {
+                  return;
+                }
+
+                for (const asset of Object.keys(assets)) {
+                  if (integrityMap.get(asset)) {
+                    console.log(asset);
+                  }
+                }
+              },
+            );
+          },
+        );
+      }
+    }
+
+    api.modifyBundlerChain((chain, { target }) => {
+      const config = api.getNormalizedConfig();
+
+      if (isHtmlDisabled(config, target)) {
+        return;
+      }
+
+      chain.plugin('rsbuild-sri').use(SriUpdateIntegrityPlugin);
     });
   },
 });
