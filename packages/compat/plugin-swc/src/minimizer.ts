e5555ef7b8e72fee1d4606ea871b6ee5116ae053
neverland <chenjiahan.jait@bytedance.com>
2024-06-04 19:42:46 +0800

    fix: apply lightningcss minify to the correct stage (#2513)

--- packages/compat/plugin-swc/src/minimizer.ts
+++ packages/compat/plugin-swc/src/minimizer.ts
@@ -35,20 +35,16 @@ export class SwcMinimizerPlugin {
     cssMinify?: boolean | CssMinifyOptions;
     rsbuildConfig: NormalizedConfig;
   }) {
-    const { minify } = options.rsbuildConfig.output;
-    const minifyJs =
-      minify === true || (typeof minify === 'object' && minify.js);
-    const minifyCss =
-      minify === true || (typeof minify === 'object' && minify.css);
-
     this.minifyOptions = {
-      jsMinify: minifyJs
+      jsMinify: options.jsMinify
         ? deepmerge(
             this.getDefaultJsMinifyOptions(options.rsbuildConfig),
             normalize(options.jsMinify, {}),
           )
         : undefined,
-      cssMinify: minifyCss ? normalize(options.cssMinify, {}) : undefined,
+      cssMinify: options.cssMinify
+        ? normalize(options.cssMinify, {})
+        : undefined,
     };
   }
 
