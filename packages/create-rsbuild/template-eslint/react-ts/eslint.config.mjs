d93f91c7b2a3d713ef931537b01a7e10e1bd3110
neverland <chenjiahan.jait@bytedance.com>
2024-06-12 17:32:23 +0800

    feat(create-rsbuild): add eslint-plugin-react-hooks

--- packages/create-rsbuild/template-eslint/react-ts/eslint.config.mjs
+++ packages/create-rsbuild/template-eslint/react-ts/eslint.config.mjs
@@ -1,5 +1,7 @@
-import { fixupConfigRules } from '@eslint/compat';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
 import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactJsx from 'eslint-plugin-react/configs/jsx-runtime.js';
 import react from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
 import ts from 'typescript-eslint';
@@ -8,6 +10,22 @@ export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...ts.configs.recommended,
-  ...fixupConfigRules(react),
+  ...fixupConfigRules([
+    {
+      ...react,
+      settings: {
+        react: { version: 'detect' },
+      },
+    },
+    reactJsx,
+  ]),
+  {
+    plugins: {
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
   { ignores: ['dist/'] },
 ];
