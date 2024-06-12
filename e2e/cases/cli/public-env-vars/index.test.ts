2f41ebd7833ba61af2e597f091d5a46fea38ef77
chenjiahan <chenjiahan.jait@bytedance.com>
2024-06-12 21:51:53 +0800

    test: fix

--- e2e/cases/cli/public-env-vars/index.test.ts
+++ e2e/cases/cli/public-env-vars/index.test.ts
@@ -4,9 +4,13 @@ import { expect, test } from '@playwright/test';
 import { fse } from '@rsbuild/shared';
 
 test('should inject public env vars to client', async () => {
+  const { NODE_ENV } = process.env;
+  process.env.NODE_ENV = 'production';
+
   execSync('npx rsbuild build', {
     cwd: __dirname,
   });
+
   const content = fse.readFileSync(
     path.join(__dirname, 'dist/static/js/index.js'),
     'utf-8',
@@ -14,4 +18,6 @@ test('should inject public env vars to client', async () => {
   expect(content).not.toContain('jack');
   expect(content).toContain('"process.env","rose"');
   expect(content).toContain('"import.meta.env","rose"');
+
+  process.env.NODE_ENV = NODE_ENV;
 });
