4cc44549280eb9db4b6cd1fef7238d3db779b498
gaoyuan <gaoyuan.1226@bytedance.com>
2024-06-18 17:38:46 +0800

    feat: init environment config (#2624)
    Co-authored-by: neverland <chenjiahan.jait@bytedance.com>

---
+++ packages/core/tests/environments.test.ts
@@ -0,0 +1,101 @@
+import { createRsbuild } from '../src';
+
+describe('environment config', () => {
+  it('should support modify environment config by api.modifyRsbuildConfig', async () => {
+    process.env.NODE_ENV = 'development';
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        source: {
+          alias: {
+            '@common': './src/common',
+          },
+        },
+        environments: {
+          web: {
+            source: {
+              entry: {
+                index: './src/index.client.js',
+              },
+            },
+          },
+          ssr: {
+            source: {
+              entry: {
+                index: './src/index.server.js',
+              },
+            },
+          },
+        },
+      },
+    });
+
+    rsbuild.addPlugins([
+      {
+        name: 'test-environment',
+        setup: (api) => {
+          api.modifyRsbuildConfig((config, { mergeRsbuildConfig }) => {
+            return mergeRsbuildConfig(config, {
+              environments: {
+                web: {
+                  source: {
+                    alias: {
+                      '@common1': './src/common1',
+                    },
+                  },
+                },
+                web1: {
+                  source: {
+                    alias: {
+                      '@common1': './src/common1',
+                    },
+                  },
+                },
+              },
+            });
+          });
+        },
+      },
+    ]);
+
+    const {
+      origin: { rsbuildConfig },
+    } = await rsbuild.inspectConfig();
+
+    expect(rsbuildConfig.environments).toMatchSnapshot();
+  });
+
+  it('should print environment config when inspect config', async () => {
+    process.env.NODE_ENV = 'development';
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        source: {
+          alias: {
+            '@common': './src/common',
+          },
+        },
+        environments: {
+          web: {
+            source: {
+              entry: {
+                index: './src/index.client.js',
+              },
+            },
+          },
+          ssr: {
+            source: {
+              entry: {
+                index: './src/index.server.js',
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const {
+      origin: { rsbuildConfig },
+    } = await rsbuild.inspectConfig();
+
+    expect(rsbuildConfig.environments).toMatchSnapshot();
+  });
+});
