d807e7d3e093eff54e381d8c8fa1d0710cb6b2d6
neverland <chenjiahan.jait@bytedance.com>
2024-06-22 10:39:16 +0800

    chore(config): remove allowJs true (#2667)

--- packages/plugin-umd/modern.config.ts
+++ packages/plugin-umd/modern.config.ts
@@ -1,7 +1,3 @@
-import { moduleTools } from '@modern-js/module-tools';
-import { dualBuildConfigs } from '@rsbuild/config/modern.config.ts';
+import { configForDualPackage } from '@rsbuild/config/modern.config.ts';
 
-export default {
-  plugins: [moduleTools()],
-  buildConfig: dualBuildConfigs,
-};
+export default configForDualPackage;
