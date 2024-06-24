003d9282fd82856d9c25f5b9816229ecd48cc25d
neverland <chenjiahan.jait@bytedance.com>
2024-06-24 20:32:22 +0800

    test: fix windows

--- packages/core/tests/helpers.test.ts
+++ packages/core/tests/helpers.test.ts
@@ -1,3 +1,4 @@
+import { sep } from 'node:path';
 import { ensureAssetPrefix, pick, prettyTime } from '../src/helpers';
 import { getCommonParentPath } from '../src/helpers/path';
 import { normalizeUrl } from '../src/server/helper';
@@ -77,29 +78,37 @@ describe('ensureAssetPrefix', () => {
 });
 
 describe('getCommonParentPath', () => {
+  const normalize = (p: string) => p.replaceAll('/', sep);
+
   it('should return the common parent path for given paths', () => {
     const paths = [
-      '/home/user/project/dist',
-      '/home/user/project/dist/sub1',
-      '/home/user/project/dist/sub2',
+      normalize('/home/user/project/dist'),
+      normalize('/home/user/project/dist/sub1'),
+      normalize('/home/user/project/dist/sub2'),
     ];
     const result = getCommonParentPath(paths);
-    expect(result).toBe('/home/user/project/dist');
+    expect(result).toBe(normalize('/home/user/project/dist'));
 
-    const paths2 = ['/home/user/project/dist1', '/home/user/project/dist2'];
+    const paths2 = [
+      normalize('/home/user/project/dist1'),
+      normalize('/home/user/project/dist2'),
+    ];
     const result2 = getCommonParentPath(paths2);
-    expect(result2).toBe('/home/user/project');
+    expect(result2).toBe(normalize('/home/user/project'));
   });
 
   it('should return empty string if there is no common parent path', () => {
-    const paths = ['/home/user/project/dist', '/home2/user/project/dist'];
+    const paths = [
+      normalize('/home/user/project/dist'),
+      normalize('/home2/user/project/dist'),
+    ];
     const result = getCommonParentPath(paths);
     expect(result).toBe('');
   });
 
   it('should handle single path input', () => {
-    const paths = ['/home/user/project/dist1'];
+    const paths = [normalize('/home/user/project/dist1')];
     const result = getCommonParentPath(paths);
-    expect(result).toBe('/home/user/project/dist1');
+    expect(result).toBe(normalize('/home/user/project/dist1'));
   });
 });
