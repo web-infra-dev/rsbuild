c2602574b4f733504957489719150ab7c635f117
gaoyuan <gaoyuan.1226@bytedance.com>
2024-06-24 17:50:19 +0800

    feat: support apply environment bundleAnalyze config (#2682)
    Co-authored-by: neverland <jait.chen@foxmail.com>

--- packages/core/src/plugins/bundleAnalyzer.ts
+++ packages/core/src/plugins/bundleAnalyzer.ts
@@ -1,10 +1,14 @@
 import { isProd } from '../helpers';
-import type { NormalizedConfig, RsbuildConfig, RsbuildPlugin } from '../types';
+import type {
+  NormalizedEnvironmentConfig,
+  RsbuildConfig,
+  RsbuildPlugin,
+} from '../types';
 
 // There are two ways to enable the bundle analyzer:
 // 1. Set environment variable `BUNDLE_ANALYZE`
 // 2. Set performance.bundleAnalyze config
-const isUseAnalyzer = (config: RsbuildConfig | NormalizedConfig) =>
+const isUseAnalyzer = (config: RsbuildConfig | NormalizedEnvironmentConfig) =>
   process.env.BUNDLE_ANALYZE || config.performance?.bundleAnalyze;
 
 export function pluginBundleAnalyzer(): RsbuildPlugin {
@@ -12,20 +16,33 @@ export function pluginBundleAnalyzer(): RsbuildPlugin {
     name: 'rsbuild:bundle-analyzer',
 
     setup(api) {
-      api.modifyRsbuildConfig((config) => {
-        if (isProd() || !isUseAnalyzer(config)) {
-          return;
-        }
+      api.modifyRsbuildConfig({
+        order: 'post',
+        handler: (config) => {
+          if (isProd()) {
+            return;
+          }
 
-        // webpack-bundle-analyze needs to read assets from disk
-        config.dev ||= {};
-        config.dev.writeToDisk = true;
+          const useAnalyzer =
+            isUseAnalyzer(config) ||
+            Object.values(config.environments || []).some((config) =>
+              isUseAnalyzer(config),
+            );
 
-        return config;
+          if (!useAnalyzer) {
+            return;
+          }
+
+          // webpack-bundle-analyze needs to read assets from disk
+          config.dev ||= {};
+          config.dev.writeToDisk = true;
+
+          return config;
+        },
       });
 
       api.modifyBundlerChain(async (chain, { CHAIN_ID, environment }) => {
-        const config = api.getNormalizedConfig();
+        const config = api.getNormalizedConfig({ environment });
 
         if (!isUseAnalyzer(config)) {
           return;
