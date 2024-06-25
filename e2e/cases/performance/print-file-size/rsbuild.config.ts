b89524c79a55bd545d7cfdda530bbdb7f2b8df32
neverland <chenjiahan.jait@bytedance.com>
2024-06-25 18:45:09 +0800

    fix: e2e

--- e2e/cases/performance/print-file-size/rsbuild.config.ts
+++ e2e/cases/performance/print-file-size/rsbuild.config.ts
@@ -2,25 +2,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default {
   plugins: [pluginReact()],
-  output: {
-    filenameHash: false,
-  },
-  performance: {
-    printFileSize: true,
-  },
-  environments: {
-    web: {
-      output: {
-        target: 'web',
-      },
-    },
-    node: {
-      output: {
-        target: 'node',
-        distPath: {
-          root: 'dist/server',
-        },
-      },
-    },
-  },
 };
