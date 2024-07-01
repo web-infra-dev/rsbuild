c2e65e2b4bcb0fb6cb177a363e6d33dc9084815c
neverland <chenjiahan.jait@bytedance.com>
2024-07-01 17:00:00 +0800

    chore: move mdx plugin to separate repo

--- examples/mdx/rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginMdx } from '@rsbuild/plugin-mdx';
-import { pluginReact } from '@rsbuild/plugin-react';
-
-export default defineConfig({
-  plugins: [pluginReact(), pluginMdx()],
-});
