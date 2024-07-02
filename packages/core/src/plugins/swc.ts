1f8b5be1568642f9dddd538128a10699c24bcad1
SoonIter <sooniter@gmail.com>
2024-07-02 08:49:50 +0800

    fix: error message in decorator

--- packages/core/src/plugins/swc.ts
+++ packages/core/src/plugins/swc.ts
@@ -241,6 +241,6 @@ export function applySwcDecoratorConfig(
       swcConfig.jsc.transform.decoratorVersion = '2022-03';
       break;
     default:
-      throw new Error('Unknown decorators version: ${version}');
+      throw new Error(`Unknown decorators version: ${version}`);
   }
 }
