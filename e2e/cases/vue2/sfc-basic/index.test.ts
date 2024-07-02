63a9229f1f1c7f5e64569e7283265a175679122b
xc2 <xc2@users.noreply.github.com>
2024-07-02 12:57:42 +0800

    test(e2e): add vue2 sfc template polyfill case

--- e2e/cases/vue2/sfc-basic/index.test.ts
+++ e2e/cases/vue2/sfc-basic/index.test.ts
@@ -11,9 +11,11 @@ rspackOnlyTest('should build basic Vue sfc correctly', async ({ page }) => {
 
   const button1 = page.locator('#button1');
   const button2 = page.locator('#button2');
+  const list1 = page.locator('.list1');
 
   await expect(button1).toHaveText('A: 0');
   await expect(button2).toHaveText('B: 0');
+  await expect(list1).toHaveCount(3);
 
   await rsbuild.close();
 });
