80d816d45782ead907dbd4eb7f67ead69021fbbc
neverland <chenjiahan.jait@bytedance.com>
2024-07-04 21:52:12 +0800

    feat(deps): bump Rspack 1.0.0-alpha.1 (#2803)

--- packages/core/src/plugins/rspackProfile.ts
+++ packages/core/src/plugins/rspackProfile.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import inspector from 'node:inspector';
 import path from 'node:path';
-import { rspack } from '@rspack/core';
+import rspack from '@rspack/core';
 import { logger } from '../logger';
 import type { RsbuildPlugin } from '../types';
 
@@ -64,7 +64,7 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
       }
 
       if (enableProfileTrace) {
-        rspack.experimental_registerGlobalTrace(
+        rspack.experiments.globalTrace.register(
           'trace',
           'chrome',
           traceFilePath,
@@ -101,7 +101,7 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
 
     api.onExit(() => {
       if (enableProfileTrace) {
-        rspack.experimental_cleanupGlobalTrace();
+        rspack.experiments.globalTrace.cleanup();
       }
       const profileDir = path.join(api.context.distPath, profileDirName);
 
