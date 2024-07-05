49763a5069bc36ed3a8b6038a4bbc298813bba07
SoonIter <sooniter@gmail.com>
2024-07-05 17:56:03 +0800

    chore: update

--- e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
+++ e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
@@ -1,15 +1,10 @@
 import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 // https://github.com/web-infra-dev/rspack/issues/6633
 rspackOnlyTest(
-  'should render pages correctly when using lazy compilation',
+  'should render pages correctly when using lazy compilation and add new initial chunk',
   async ({ page }) => {
-    // TODO fix this case in Windows
-    if (process.platform === 'win32') {
-      test.skip();
-    }
-
     const rsbuild = await dev({
       cwd: __dirname,
     });
