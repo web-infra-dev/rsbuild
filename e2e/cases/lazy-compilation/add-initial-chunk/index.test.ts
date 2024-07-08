943d15014e4236a685583d5bdb0bad180fcedf3e
Soon <sooniter@gmail.com>
2024-07-08 13:38:09 +0800

    test(e2e): skip test of hmr in windows (#2832)

--- e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
+++ e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
@@ -1,10 +1,14 @@
 import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
-import { expect } from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 // https://github.com/web-infra-dev/rspack/issues/6633
 rspackOnlyTest(
   'should render pages correctly when using lazy compilation and add new initial chunk',
   async ({ page }) => {
+    // TODO fix this case in Windows
+    if (process.platform === 'win32') {
+      test.skip();
+    }
     const rsbuild = await dev({
       cwd: __dirname,
     });
