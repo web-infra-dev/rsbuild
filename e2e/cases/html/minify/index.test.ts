db896341e49dd08eabbfd641c048fcf3af5cb375
neverland <chenjiahan.jait@bytedance.com>
2024-07-08 14:22:49 +0800

    test: fix webpack

--- e2e/cases/html/minify/index.test.ts
+++ e2e/cases/html/minify/index.test.ts
@@ -1,36 +1,39 @@
-import { build, gotoPage } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { build, gotoPage, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
 
 const fixtures = __dirname;
 
-test('should minify template success when inlineScripts & inlineStyles', async ({
-  page,
-}) => {
-  const rsbuild = await build({
-    cwd: fixtures,
-    runServer: true,
-    rsbuildConfig: {
-      html: {
-        template: './static/index.html',
-        // avoid Minified React error #200;
-        inject: 'body',
+rspackOnlyTest(
+  'should minify template success when inlineScripts & inlineStyles',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: fixtures,
+      runServer: true,
+      rsbuildConfig: {
+        html: {
+          template: './static/index.html',
+          // avoid Minified React error #200;
+          inject: 'body',
+        },
+        output: {
+          inlineScripts: true,
+          inlineStyles: true,
+        },
       },
-      output: {
-        inlineScripts: true,
-        inlineStyles: true,
-      },
-    },
-  });
+    });
 
-  await gotoPage(page, rsbuild);
+    await gotoPage(page, rsbuild);
 
-  const files = await rsbuild.unwrapOutputJSON();
+    const files = await rsbuild.unwrapOutputJSON();
 
-  const content =
-    files[Object.keys(files).find((file) => file.endsWith('.html'))!];
+    const content =
+      files[Object.keys(files).find((file) => file.endsWith('.html'))!];
 
-  expect(content.includes('html,body{margin:0;padding:0}')).toBeTruthy();
-  expect(content.includes('let n=document.createElement("div")')).toBeTruthy();
+    expect(content.includes('html,body{margin:0;padding:0}')).toBeTruthy();
+    expect(
+      content.includes('let n=document.createElement("div")'),
+    ).toBeTruthy();
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
