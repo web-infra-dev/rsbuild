e6b0379344476b2c104179ec9f3b6ff5da14323b
gaoyuan <gaoyuan.1226@bytedance.com>
2024-07-12 20:35:21 +0800

    fix(type): webpack type should works with moduleResolution node16+ (#2900)

--- packages/compat/webpack/modern.config.ts
+++ packages/compat/webpack/modern.config.ts
@@ -1,3 +1,22 @@
-import { configForDualPackage } from '@rsbuild/config/modern.config.ts';
+import { defineConfig, moduleTools } from '@modern-js/module-tools';
+import {
+  cjsBuildConfig,
+  emitTypePkgJsonPlugin,
+  esmBuildConfig,
+} from '@rsbuild/config/modern.config.ts';
 
-export default configForDualPackage;
+export default defineConfig({
+  plugins: [moduleTools(), emitTypePkgJsonPlugin],
+  buildConfig: [
+    cjsBuildConfig,
+    esmBuildConfig,
+    // Types
+    {
+      buildType: 'bundleless',
+      dts: {
+        distPath: '../dist-types',
+        only: true,
+      },
+    },
+  ],
+});
