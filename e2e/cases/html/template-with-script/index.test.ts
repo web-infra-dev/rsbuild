3df7b8a4f3f04ab8374380e1304b965703a2bd91
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-13 15:47:28 +0800

    fix: es template string breaks template compiling

---
+++ e2e/cases/html/template-with-script/index.test.ts
@@ -0,0 +1,14 @@
+import { build } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+
+// see: https://github.com/rspack-contrib/html-rspack-plugin/issues/14
+test('should compile template with es template correctly', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+  });
+  const files = await rsbuild.unwrapOutputJSON();
+
+  const indexHtml =
+    files[Object.keys(files).find((file) => file.endsWith('index.html'))!];
+  expect(indexHtml).toContain("const baseUrl = match ? `${match[0]}/` : '/'");
+});
