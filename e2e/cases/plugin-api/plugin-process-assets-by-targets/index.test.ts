ea1ac7a78115a7b0ed757a6e3d8d72b3c13e582f
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-20 20:32:14 +0800

    feat: allow to process assets for specific target

---
+++ e2e/cases/plugin-api/plugin-process-assets-by-targets/index.test.ts
@@ -0,0 +1,36 @@
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest('should process assets when target is web', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    rsbuildConfig: {
+      output: {
+        target: 'web',
+      },
+    },
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+  const indexJs = Object.keys(files).find(
+    (file) => file.includes('index') && file.endsWith('.js'),
+  );
+  expect(indexJs).toBeFalsy();
+});
+
+rspackOnlyTest('should not process assets when target is not web', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    rsbuildConfig: {
+      output: {
+        target: 'web-worker',
+      },
+    },
+  });
+
+  const files = await rsbuild.unwrapOutputJSON();
+  const indexJs = Object.keys(files).find(
+    (file) => file.includes('index') && file.endsWith('.js'),
+  );
+  expect(indexJs).toBeTruthy();
+});
