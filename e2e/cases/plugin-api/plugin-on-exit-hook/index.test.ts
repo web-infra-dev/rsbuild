a4bd183426051c1a1a14d7aca3ca8d79c9c0f969
neverland <chenjiahan.jait@bytedance.com>
2024-07-24 07:01:28 +0800

    test: add e2e case for onExit hook (#2999)

---
+++ e2e/cases/plugin-api/plugin-on-exit-hook/index.test.ts
@@ -0,0 +1,18 @@
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+const distFile = path.join(__dirname, 'node_modules/hooksTempFile');
+
+rspackOnlyTest('should run onExit hook before process exit', async () => {
+  fs.rmSync(distFile, { force: true });
+
+  await new Promise<void>((resolve) => {
+    exec('node ./run.mjs', { cwd: __dirname }, () => {
+      expect(fs.readFileSync(distFile, 'utf-8')).toEqual('1');
+      resolve();
+    });
+  });
+});
