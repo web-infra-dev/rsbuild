e6d173a8c8b828f929daa12b6cb9e60c1ce3ac23
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-23 22:51:55 +0800

    test: add e2e case for onExit hook

---
+++ e2e/cases/plugin-api/plugin-on-exit-hook/run.mjs
@@ -0,0 +1,40 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createRsbuild } from '@rsbuild/core';
+import fse from 'fs-extra';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const distFile = path.join(__dirname, 'node_modules/hooksTempFile');
+
+const write = (str) => {
+  let content;
+  if (fs.existsSync(distFile)) {
+    content = `${fs.readFileSync(distFile, 'utf-8')},${str}`;
+  } else {
+    content = str;
+  }
+  fse.outputFileSync(distFile, content);
+};
+
+const plugin = {
+  name: 'test-plugin',
+  setup(api) {
+    api.onExit(() => {
+      write('1');
+    });
+  },
+};
+
+async function main() {
+  const rsbuild = await createRsbuild({
+    cwd: __dirname,
+    rsbuildConfig: {
+      plugins: [plugin],
+    },
+  });
+
+  await rsbuild.build();
+}
+
+await main();
