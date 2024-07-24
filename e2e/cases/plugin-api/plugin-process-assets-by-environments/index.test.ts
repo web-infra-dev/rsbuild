345e0a70378ce41a6ee3a586ebe18134d1625322
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-24 22:27:55 +0800

    fix: lint

--- e2e/cases/plugin-api/plugin-process-assets-by-environments/index.test.ts
+++ e2e/cases/plugin-api/plugin-process-assets-by-environments/index.test.ts
@@ -1,5 +1,5 @@
-import { existsSync } from 'fs';
-import { join } from 'path';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
 import { build, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
 
