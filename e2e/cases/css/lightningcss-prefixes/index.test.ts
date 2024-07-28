5235f7b57850444f3809b4d982592d2daa212e2e
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-28 17:29:01 +0800

    fix: set browserset for lightningcss minimizer

--- e2e/cases/css/lightningcss-prefixes/index.test.ts
+++ e2e/cases/css/lightningcss-prefixes/index.test.ts
@@ -13,7 +13,7 @@ rspackOnlyTest(
       files[Object.keys(files).find((file) => file.endsWith('.css'))!];
 
     expect(content).toEqual(
-      '@media (-webkit-min-device-pixel-ratio:2),(min-resolution:2dppx){.item{-webkit-user-select:none;user-select:none;background:linear-gradient(#fff,#000);transition:all .5s}}',
+      '@media (-webkit-min-device-pixel-ratio:2),(min-resolution:2dppx){.item{-webkit-user-select:none;-ms-user-select:none;user-select:none;background:-webkit-linear-gradient(#fff,#000);background:linear-gradient(#fff,#000);-webkit-transition:all .5s;transition:all .5s}}',
     );
   },
 );
