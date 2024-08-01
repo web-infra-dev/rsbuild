134605a1f96d22e1873ef43106025f8abc60052e
chenjiahan <chenjiahan.jait@bytedance.com>
2024-08-02 00:19:08 +0800

    test: fix slow case

--- e2e/cases/cli/reload-extra-config/index.test.ts
+++ e2e/cases/cli/reload-extra-config/index.test.ts
@@ -4,48 +4,23 @@ import path from 'node:path';
 import { awaitFileExists } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-test('should restart dev server and reload config when extra config file changed', async () => {
-  const distPath = 'dist';
-  const dist = path.join(__dirname, distPath);
+const extraConfigPath = './test-temp-config.ts';
 
-  const extraConfigPath = './server.ts';
+test('should restart dev server and reload config when extra config file changed', async () => {
+  const dist = path.join(__dirname, 'dist');
   const extraConfigFile = path.join(__dirname, extraConfigPath);
 
-  const configFile = path.join(__dirname, 'rsbuild.config.ts');
-
   fs.rmSync(extraConfigFile, { force: true });
-  fs.rmSync(configFile, { force: true });
   fs.rmSync(dist, { recursive: true, force: true });
 
   // configs/server.ts
   fs.writeFileSync(extraConfigFile, 'export default {};');
 
-  // rsbuild.config.ts
-  fs.writeFileSync(
-    configFile,
-    `
-    import { defineConfig } from '@rsbuild/core';
-    import server from '${extraConfigPath}';
-
-    export default defineConfig({
-      dev: {
-        writeToDisk: true,
-        watchFiles: {
-          type: 'reload-server',
-          paths: ['${extraConfigPath}'],
-        }
-      },
-      output: {
-        distPath: {
-          root: '${distPath}',
-        },
-      },
-      server,
-    });`,
-  );
-
   const process = exec('npx rsbuild dev', {
     cwd: __dirname,
+    env: {
+      WATCH_FILES_TYPE: 'reload-server',
+    },
   });
 
   await awaitFileExists(dist);
@@ -54,54 +29,28 @@ test('should restart dev server and reload config when extra config file changed
   // configs/server.ts changed
   fs.writeFileSync(extraConfigFile, 'export default {};');
 
+  // rebuild and generate dist files
   await awaitFileExists(dist);
+  expect(fs.existsSync(path.join(dist, 'temp.txt')));
 
   process.kill();
 });
 
 test('should not restart dev server when extra config file changed but `dev.watchFiles.type` is not set to `reload-server`', async () => {
-  const distPath = 'dist';
-  const dist = path.join(__dirname, distPath);
-
-  const extraConfigPath = './server.ts';
+  const dist = path.join(__dirname, 'dist');
   const extraConfigFile = path.join(__dirname, extraConfigPath);
 
-  const configFile = path.join(__dirname, 'rsbuild.config.ts');
-
   fs.rmSync(extraConfigFile, { force: true });
-  fs.rmSync(configFile, { force: true });
   fs.rmSync(dist, { recursive: true, force: true });
 
   // configs/server.ts
   fs.writeFileSync(extraConfigFile, 'export default {};');
 
-  // rsbuild.config.ts
-  fs.writeFileSync(
-    configFile,
-    `
-    import { defineConfig } from '@rsbuild/core';
-    import server from '${extraConfigPath}';
-
-    export default defineConfig({
-      dev: {
-        writeToDisk: true,
-        watchFiles: {
-          // should not restart dev server
-          type: 'foo',
-          paths: ['${extraConfigPath}'],
-        }
-      },
-      output: {
-        distPath: {
-          root: '${distPath}',
-        },
-      },
-      server,
-    });`,
-  );
-
   const process = exec('npx rsbuild dev', {
     cwd: __dirname,
+    env: {
+      WATCH_FILES_TYPE: 'reload-page',
+    },
   });
 
   await awaitFileExists(dist);
