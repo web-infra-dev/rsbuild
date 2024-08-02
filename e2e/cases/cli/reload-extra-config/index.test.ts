f1b12fc25f6c112581dc6e64c910afd4790a5a52
neverland <chenjiahan.jait@bytedance.com>
2024-08-02 13:05:58 +0800

    test(e2e): fix a slow case

--- e2e/cases/cli/reload-extra-config/index.test.ts
+++ e2e/cases/cli/reload-extra-config/index.test.ts
@@ -1,24 +1,24 @@
 import { exec } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
-import { awaitFileExists } from '@e2e/helper';
+import { awaitFileExists, getRandomPort } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-const extraConfigPath = './test-temp-config.ts';
+const tempConfigPath = './test-temp-config.ts';
 
-test('should restart dev server and reload config when extra config file changed', async () => {
+test('should restart dev server when extra config file changed', async () => {
   const dist = path.join(__dirname, 'dist');
-  const extraConfigFile = path.join(__dirname, extraConfigPath);
+  const extraConfigFile = path.join(__dirname, tempConfigPath);
 
   fs.rmSync(extraConfigFile, { force: true });
   fs.rmSync(dist, { recursive: true, force: true });
+  fs.writeFileSync(extraConfigFile, 'export default { foo: 1 };');
 
-  // configs/server.ts
-  fs.writeFileSync(extraConfigFile, 'export default {};');
-
-  const process = exec('npx rsbuild dev', {
+  const childProcess = exec('npx rsbuild dev', {
     cwd: __dirname,
     env: {
+      ...process.env,
+      PORT: String(await getRandomPort()),
       WATCH_FILES_TYPE: 'reload-server',
     },
   });
@@ -26,29 +26,29 @@ test('should restart dev server and reload config when extra config file changed
   await awaitFileExists(dist);
 
   fs.rmSync(dist, { recursive: true });
-  // configs/server.ts changed
-  fs.writeFileSync(extraConfigFile, 'export default {};');
+  // temp config changed
+  fs.writeFileSync(extraConfigFile, 'export default { foo: 2 };');
 
   // rebuild and generate dist files
   await awaitFileExists(dist);
   expect(fs.existsSync(path.join(dist, 'temp.txt')));
 
-  process.kill();
+  childProcess.kill();
 });
 
-test('should not restart dev server when extra config file changed but `dev.watchFiles.type` is not set to `reload-server`', async () => {
+test('should not restart dev server if `watchFiles.type` is `reload-page`', async () => {
   const dist = path.join(__dirname, 'dist');
-  const extraConfigFile = path.join(__dirname, extraConfigPath);
+  const extraConfigFile = path.join(__dirname, tempConfigPath);
 
   fs.rmSync(extraConfigFile, { force: true });
   fs.rmSync(dist, { recursive: true, force: true });
+  fs.writeFileSync(extraConfigFile, 'export default { foo: 1 };');
 
-  // configs/server.ts
-  fs.writeFileSync(extraConfigFile, 'export default {};');
-
-  const process = exec('npx rsbuild dev', {
+  const childProcess = exec('npx rsbuild dev', {
     cwd: __dirname,
     env: {
+      ...process.env,
+      PORT: String(await getRandomPort()),
       WATCH_FILES_TYPE: 'reload-page',
     },
   });
@@ -56,12 +56,11 @@ test('should not restart dev server when extra config file changed but `dev.watc
   await awaitFileExists(dist);
 
   fs.rmSync(dist, { recursive: true });
-  // configs/server.ts changed
-  fs.writeFileSync(extraConfigFile, 'export default {};');
+  // temp config changed
+  fs.writeFileSync(extraConfigFile, 'export default { foo: 2 };');
 
-  await expect(awaitFileExists(dist)).rejects.toThrow(
-    `awaitFileExists failed: ${dist}`,
-  );
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  expect(fs.existsSync(path.join(dist, 'temp.txt'))).toBeFalsy();
 
-  process.kill();
+  childProcess.kill();
 });
--- e2e/cases/cli/reload-extra-config/rsbuild.config.ts
+++ e2e/cases/cli/reload-extra-config/rsbuild.config.ts
@@ -1,13 +1,16 @@
 import path from 'node:path';
 import { type RsbuildPlugin, defineConfig } from '@rsbuild/core';
 import fse from 'fs-extra';
-import server from './test-temp-config';
+import content from './test-temp-config';
 
 const testPlugin: RsbuildPlugin = {
   name: 'test-plugin',
   setup(api) {
     api.onBeforeBuild(() => {
-      fse.outputFileSync(path.join(api.context.distPath, 'temp.txt'), 'test');
+      fse.outputFileSync(
+        path.join(api.context.distPath, 'temp.txt'),
+        JSON.stringify(content),
+      );
     });
   },
 };
@@ -17,7 +20,7 @@ export default defineConfig({
     writeToDisk: true,
     watchFiles: {
       type: process.env.WATCH_FILES_TYPE as 'reload-page' | 'reload-server',
-      paths: ['./test-temp-config'],
+      paths: ['./test-temp-config.ts'],
     },
   },
   output: {
@@ -26,5 +29,7 @@ export default defineConfig({
     },
   },
   plugins: [testPlugin],
-  server,
+  server: {
+    port: Number(process.env.PORT),
+  },
 });
