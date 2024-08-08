498bb2080a04ea58adeedcb4f717ee3a1bf13b10
sunyiteng <sunyiteng@bytedance.com>
2024-08-08 19:36:24 +0800

    chore: update

--- scripts/test-helper/src/path.ts
+++ scripts/test-helper/src/path.ts
@@ -1,7 +1,4 @@
-import fs from 'node:fs';
-import os from 'node:os';
 import path from 'node:path';
-import _ from 'lodash';
 import upath from 'upath';
 
 export const isPathString = (test: string): boolean =>
@@ -14,28 +11,3 @@ export const normalizeToPosixPath = (p: string | undefined) =>
   upath
     .normalizeSafe(path.normalize(p || ''))
     .replace(/^([a-zA-Z]+):/, (_, m: string) => `/${m.toLowerCase()}`);
-
-/**
- * Compile path string to RegExp.
- * @note Only support posix path.
- */
-export function compilePathMatcherRegExp(match: string | RegExp) {
-  if (typeof match !== 'string') {
-    return match;
-  }
-  const escaped = _.escapeRegExp(match);
-  return new RegExp(`(?<=\\W|^)${escaped}(?=\\W|$)`);
-}
-
-export function getRealTemporaryDirectory() {
-  let ret: string | null = null;
-  try {
-    ret = os.tmpdir();
-    ret = fs.realpathSync(ret);
-  } catch {}
-  return ret;
-}
-
-export function splitPathString(str: string) {
-  return str.split(/[\\/]/);
-}
--- scripts/test-helper/vitest.setup.ts
+++ scripts/test-helper/vitest.setup.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
+import { createSnapshotSerializer } from 'path-serializer';
 import { beforeAll, expect } from 'vitest';
-import { createSnapshotSerializer } from './dist';
 
 beforeAll((suite) => {
   process.env.REBUILD_TEST_SUITE_CWD = suite.filepath
