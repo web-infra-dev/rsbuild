545d4bcfbc50bd599182eb37fcfe147ec37a9eca
chenjiahan <chenjiahan.jait@bytedance.com>
2024-08-09 21:52:03 +0800

    chore: remove unused ts comments

--- packages/core/src/plugins/fileSize.ts
+++ packages/core/src/plugins/fileSize.ts
@@ -123,8 +123,6 @@ async function printFileSizes(
     const origin = stats.toJson({
       all: false,
       assets: true,
-      // TODO: need supported in rspack
-      // @ts-expect-error
       cachedAssets: true,
       groupAssetsByInfo: false,
       groupAssetsByPath: false,
