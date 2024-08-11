e7132f573bcfa5190958a8e9b12ba709fde7905c
neverland <chenjiahan.jait@bytedance.com>
2024-08-11 09:40:17 +0800

    test(e2e): print line for each test being run in CI (#3187)

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,4 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   // Retry on CI
   retries: process.env.CI ? 3 : 0,
+  // Print line for each test being run in CI
+  reporter: 'list',
 });
