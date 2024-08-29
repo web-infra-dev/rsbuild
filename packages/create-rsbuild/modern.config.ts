60eb8d18168d29216527020d557531ff59ff57f5
neverland <chenjiahan.jait@bytedance.com>
2024-08-29 17:57:02 +0800

    refacor(create-rsbuild): use Rslib to bundle package

--- packages/create-rsbuild/modern.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, moduleTools } from '@modern-js/module-tools';
-import { esmBuildConfig } from '@rsbuild/config/modern.config.ts';
-
-export default defineConfig({
-  plugins: [moduleTools()],
-  buildConfig: [esmBuildConfig],
-});
