8686ecfe7d9d6e1f542142bcf098d22f455f1eb5
sunyiteng <sunyiteng@bytedance.com>
2024-09-03 14:56:51 +0800

    chore: top padding

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -14,7 +14,7 @@ const CopyRight = () => {
   }
 
   return (
-    <footer className="bottom-0 mt-12 py-8 px-6 sm:p-8 w-full border-t border-solid border-divider-light">
+    <footer className="bottom-0 mt-24 py-8 px-6 sm:p-8 w-full border-t border-solid border-divider-light">
       <div className="m-auto w-full text-center">
         <div className="font-medium text-sm text-text-2">{message}</div>
       </div>
