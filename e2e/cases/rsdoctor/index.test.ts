9443c2b9521705e200ea932d42df7ebdf2b0fbb6
neverland <chenjiahan.jait@bytedance.com>
2024-09-04 13:45:41 +0800

    test: fix

--- e2e/cases/rsdoctor/index.test.ts
+++ e2e/cases/rsdoctor/index.test.ts
@@ -2,12 +2,14 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { build, proxyConsole, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
+// @ts-expect-error
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 
 const packagePath = path.join(
   __dirname,
   'node_modules/@rsdoctor/rspack-plugin',
 );
-const testFile = path.join(packagePath, 'test.txt');
+const testFile = path.join(packagePath, 'test-temp.txt');
 
 rspackOnlyTest(
   'should register Rsdoctor plugin when process.env.RSDOCTOR is true',
@@ -46,5 +48,34 @@ rspackOnlyTest(
 
     expect(fs.existsSync(testFile)).toBe(false);
     process.env.RSDOCTOR = '';
+  },
+);
+
+rspackOnlyTest(
+  'should not register Rsdoctor plugin when process.env.RSDOCTOR is true and the plugin has been registered',
+  async () => {
+    fs.rmSync(packagePath, { recursive: true, force: true });
+    fs.cpSync(path.join(__dirname, 'mock'), packagePath, { recursive: true });
+
+    const { logs, restore } = proxyConsole();
+    process.env.RSDOCTOR = 'true';
+
+    await build({
+      cwd: __dirname,
+      rsbuildConfig: {
+        tools: {
+          rspack: {
+            plugins: [new RsdoctorRspackPlugin()],
+          },
+        },
+      },
+    });
+
+    expect(
+      logs.some((log) => log.includes('@rsdoctor') && log.includes('enabled')),
+    ).toBe(false);
+
+    process.env.RSDOCTOR = '';
+    restore();
   },
 );
