4b31457d269c977697fd0780beb5f5032bc50b4e
neverland <chenjiahan.jait@bytedance.com>
2024-09-06 13:44:51 +0800

    fix: lightningcss-loader not work when using PostCSS

---
+++ e2e/cases/css/tailwindcss-vendor-prefix/index.test.ts
@@ -0,0 +1,20 @@
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest(
+  'should generate tailwindcss utilities with vendor prefixes correctly',
+  async () => {
+    const rsbuild = await build({
+      cwd: __dirname,
+    });
+
+    const files = await rsbuild.unwrapOutputJSON();
+    const indexCssFile = Object.keys(files).find(
+      (file) => file.includes('index.') && file.endsWith('.css'),
+    )!;
+
+    expect(files[indexCssFile]).toContain('-webkit-user-select: none;');
+    expect(files[indexCssFile]).toContain('-ms-user-select: none;');
+    expect(files[indexCssFile]).toContain('user-select: none;');
+  },
+);
