3315202a7f106c2f4373d811f06aec25eeb180b8
chenjiahan <chenjiahan.jait@bytedance.com>
2024-09-09 23:02:24 +0800

    docs: add announcement banner

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,9 +1,38 @@
+import { Announcement } from '@rstack-dev/doc-ui/announcement';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 import Theme from 'rspress/theme';
 import { HomeLayout } from './pages';
 import './index.scss';
+import { NoSSR, useLang, usePageData } from 'rspress/runtime';
 
-const Layout = () => <Theme.Layout beforeNavTitle={<NavIcon />} />;
+const ANNOUNCEMENT_URL = '/community/releases/v1-0';
+
+const Layout = () => {
+  const { page } = usePageData();
+  const lang = useLang();
+
+  return (
+    <Theme.Layout
+      beforeNavTitle={<NavIcon />}
+      beforeNav={
+        <NoSSR>
+          <Announcement
+            href={
+              lang === 'en' ? ANNOUNCEMENT_URL : `/${lang}${ANNOUNCEMENT_URL}`
+            }
+            message={
+              lang === 'en'
+                ? 'Rsbuild 1.0 has been released!'
+                : 'Rsbuild 1.0 正式发布！'
+            }
+            localStorageKey="rsbuild-announcement-closed"
+            display={page.pageType === 'home'}
+          />
+        </NoSSR>
+      }
+    />
+  );
+};
 
 export default {
   ...Theme,
