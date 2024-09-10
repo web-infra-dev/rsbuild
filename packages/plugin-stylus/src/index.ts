2a29e6664c17b5c32eebe415f894354d936c9cd0
shulaoda <laipichan@qq.com>
2024-09-10 15:17:58 +0800

    save

--- packages/plugin-stylus/src/index.ts
+++ packages/plugin-stylus/src/index.ts
@@ -6,6 +6,7 @@ export const PLUGIN_STYLUS_NAME = 'rsbuild:stylus';
 
 type StylusOptions = {
   use?: string[];
+  define: Array<any>;
   include?: string[];
   import?: string;
   resolveURL?: boolean;
