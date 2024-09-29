0aed44430840488ed30a02cdb9eb15d94c0b4c78
chenjiahan <chenjiahan.jait@bytedance.com>
2024-09-29 22:39:29 +0800

    chore

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="content" @click="color = !color">
+  <div class="content">
     <h1>Rsbuild with Vue</h1>
     <p>Start building amazing things with Rsbuild.</p>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const color = ref(true)
-</script>
-
 <style scoped>
-content h1 {
+.content {
+  display: flex;
+  min-height: 100vh;
+  line-height: 1.1;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.content h1 {
   font-size: 3.6rem;
   font-weight: 700;
-  /* ok */
-  color: v-bind("color ? 'red': 'green'");
-  /* no ok */
-  color: v-bind('color ? "red": "green"');
+}
+.content p {
+  font-size: 1.2rem;
+  font-weight: 400;
+  opacity: 0.5;
 }
 </style>
