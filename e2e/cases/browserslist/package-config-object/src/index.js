f8e6c5445ec1eb31bd6e95c1fd37622fad54f4b2
chenjiahan <chenjiahan.jait@bytedance.com>
2024-09-29 22:38:23 +0800

    fix: failed load browserslist string value

---
+++ e2e/cases/browserslist/package-config-object/src/index.js
@@ -0,0 +1,5 @@
+async function foo() {
+  console.log('hello');
+}
+
+console.log(foo);
--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div class="content">
+  <div class="content" @click="color = !color">
     <h1>Rsbuild with Vue</h1>
     <p>Start building amazing things with Rsbuild.</p>
   </div>
 </template>
 
-<style scoped>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
+<script setup lang="ts">
+import { ref } from 'vue'
 
-.content h1 {
+const color = ref(true)
+</script>
+
+<style scoped>
+content h1 {
   font-size: 3.6rem;
   font-weight: 700;
-}
-
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
+  /* ok */
+  color: v-bind("color ? 'red': 'green'");
+  /* no ok */
+  color: v-bind('color ? "red": "green"');
 }
 </style>
