578a6f8279afcaeb2c771aa20f850790b56bd124
neverland <chenjiahan.jait@bytedance.com>
2024-10-05 21:31:47 +0800

    feat: define `import.meta.env.ASSET_PREFIX`

--- packages/core/src/plugins/define.ts
+++ packages/core/src/plugins/define.ts
@@ -9,15 +9,16 @@ export const pluginDefine = (): RsbuildPlugin => ({
       const { config } = environment;
 
       const baseUrl = JSON.stringify(config.server.base);
+      const assetPrefix = JSON.stringify(getPublicPathFromChain(chain, false));
+
       const builtinVars: Define = {
         'import.meta.env.MODE': JSON.stringify(config.mode),
         'import.meta.env.DEV': config.mode === 'development',
         'import.meta.env.PROD': config.mode === 'production',
         'import.meta.env.BASE_URL': baseUrl,
-        'process.env.ASSET_PREFIX': JSON.stringify(
-          getPublicPathFromChain(chain, false),
-        ),
+        'import.meta.env.ASSET_PREFIX': assetPrefix,
         'process.env.BASE_URL': baseUrl,
+        'process.env.ASSET_PREFIX': assetPrefix,
       };
 
       chain
