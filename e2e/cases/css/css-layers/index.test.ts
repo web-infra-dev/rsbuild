1fbaaea221c4ada06a3f96915e4abcbc04b100e9
neverland <chenjiahan.jait@bytedance.com>
2024-10-10 11:25:15 +0800

    test: dev

--- e2e/cases/css/css-layers/index.test.ts
+++ e2e/cases/css/css-layers/index.test.ts
@@ -1,7 +1,7 @@
-import { build, rspackOnlyTest } from '@e2e/helper';
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
 
-rspackOnlyTest('should bundle CSS layers as expected', async () => {
+rspackOnlyTest('should bundle CSS layers as expected in build', async () => {
   const rsbuild = await build({
     cwd: __dirname,
   });
@@ -14,3 +14,53 @@ rspackOnlyTest('should bundle CSS layers as expected', async () => {
     '@layer a{.a{color:red}}@layer b{.b{color:green}}@layer c{@layer{.c-sub{color:#00f}.c-sub2{color:green}}.c{color:#00f}}',
   );
 });
+
+rspackOnlyTest(
+  'should bundle CSS layers as expected in dev',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+      rsbuildConfig: {
+        dev: {
+          writeToDisk: true,
+        },
+      },
+    });
+    const files = await rsbuild.unwrapOutputJSON();
+
+    const content =
+      files[Object.keys(files).find((file) => file.endsWith('.css'))!];
+
+    expect(content.trim()).toEqual(`@layer a {
+.a {
+  color: red;
+}
+
+}
+@layer b {
+.b {
+  color: green;
+}
+
+}
+@layer c {
+@layer {.c-sub {
+  color: #00f;
+}
+
+.c-sub2 {
+  color: green;
+}
+}
+}
+@layer c {
+.c {
+  color: #00f;
+}
+
+}`);
+
+    await rsbuild.close();
+  },
+);
