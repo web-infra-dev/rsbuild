0517f03e768bdf0d3c56d1b556171360ad2c46b0
chenjiahan <chenjiahan.jait@bytedance.com>
2024-10-15 22:54:27 +0800

    chore: cra env

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig, loadEnv } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+
+const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
+
+console.log(publicVars);
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: {
+      'process.env': {
+        REACT_APP_PUBLIC_URL: '"https://example.com"',
+        REACT_APP_API_URL: '"https://api.example.com"',
+      },
+    },
+  },
 });
--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log(process.env);
+
+const { REACT_APP_PUBLIC_URL, API_URL } = process.env;
+console.log(REACT_APP_PUBLIC_URL, API_URL);
+
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
