dbefd7ebfa7dcc746d24ac360cb63f72f54689e9
neverland <chenjiahan.jait@bytedance.com>
2024-10-16 14:13:19 +0800

    upd

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -1,18 +1,6 @@
-import { defineConfig, loadEnv } from '@rsbuild/core';
+import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-
-const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
-
-console.log(publicVars);
 
 export default defineConfig({
   plugins: [pluginReact()],
-  source: {
-    define: {
-      'process.env': {
-        REACT_APP_PUBLIC_URL: '"https://example.com"',
-        REACT_APP_API_URL: '"https://api.example.com"',
-      },
-    },
-  },
 });
