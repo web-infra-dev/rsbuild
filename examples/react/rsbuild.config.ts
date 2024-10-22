94c7092aa61b7e002c0728053ed8838af6792cd2
neverland <chenjiahan.jait@bytedance.com>
2024-10-22 10:44:46 +0800

    fix

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,10 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  server: {
-    base: '/foo',
-  },
-  dev: {
-    assetPrefix: true,
-  },
 });
