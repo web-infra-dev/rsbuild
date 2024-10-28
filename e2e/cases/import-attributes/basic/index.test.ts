cffa3ffcd1ed3c02be3cc79241537acef94ce2b4
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-10-28 15:49:45 +0800

    feat: support import attributes syntax

---
+++ e2e/cases/import-attributes/basic/index.test.ts
@@ -0,0 +1,16 @@
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest('should support import attributes syntax', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    rsbuildConfig: {
+      source: {
+        assetsInclude: [/\.json5$/],
+      },
+    },
+  });
+
+  const indexJs = await rsbuild.getIndexFile();
+  expect(indexJs.content).toContain('with import attributes');
+});
