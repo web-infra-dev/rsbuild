90c8828654d2c5c1ac00d62edd9633b42a3374c0
LingyuCoder <lingyucoder@gmail.com>
2024-10-31 17:45:02 +0800

    feat: support queueMicroTask on the server side of SSR

--- e2e/cases/server/ssr/src/index.server.tsx
+++ e2e/cases/server/ssr/src/index.server.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './App';
+import { assert } from './assert.server';
 
 // test dynamic import
 import('./test');
+
+// assert environment
+assert();
 
 export function render() {
   return ReactDOMServer.renderToString(
