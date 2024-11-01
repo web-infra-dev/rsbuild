842dd1e3fe8b696a70a5a969f49175422bb53e84
neverland <chenjiahan.jait@bytedance.com>
2024-11-01 21:33:23 +0800

    feat: automatically read tsconfig paths by `references` (#3882)

---
+++ e2e/cases/alias/tsconfig-paths-references/index.test.ts
@@ -0,0 +1,24 @@
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest(
+  'tsconfig paths should work with references',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+      rsbuildConfig: {
+        source: {
+          alias: {
+            '@common': './src/common2',
+          },
+        },
+      },
+    });
+
+    const foo = page.locator('#foo');
+    await expect(foo).toHaveText('tsconfig paths worked');
+
+    await rsbuild.close();
+  },
+);
