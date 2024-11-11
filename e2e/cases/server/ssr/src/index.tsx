992e1a28089c01e05e83fa069b4c6a782569095f
9aoy <gaoyuan.1226@bytedance.com>
2024-11-11 12:21:16 +0800

    test: use hydrateRoot API in SSR case (#3935)

--- e2e/cases/server/ssr/src/index.tsx
+++ e2e/cases/server/ssr/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root')!,
   <React.StrictMode>
     <App />
   </React.StrictMode>,
