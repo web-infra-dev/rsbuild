a347b40c7fd6e6ffaffc2d403da21143f383b6ef
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-11-14 18:04:15 +0800

    test: add test case

---
+++ e2e/cases/cli/build-watch-restart/index.test.ts
@@ -0,0 +1,55 @@
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { awaitFileExists } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+import fse from 'fs-extra';
+
+test('should support restart build when config changed', async () => {
+  const indexFile = path.join(__dirname, 'src/index.js');
+  const distIndexFile = path.join(__dirname, 'dist/static/js/index.js');
+  fs.rmSync(indexFile, { force: true });
+  fs.rmSync(distIndexFile, { force: true });
+  const tempConfigFile = path.join(__dirname, 'test-temp-rsbuild.config.mjs');
+
+  fse.outputFileSync(
+    tempConfigFile,
+    `export default {
+  output: {
+    filenameHash: false,
+  },
+};
+`,
+  );
+
+  fse.outputFileSync(indexFile, `console.log('hello!');`);
+
+  const process = exec(`npx rsbuild build --watch -c ${tempConfigFile}`, {
+    cwd: __dirname,
+  });
+
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
+  fs.rmSync(distIndexFile, { force: true });
+
+  fse.outputFileSync(
+    tempConfigFile,
+    `export default {
+  // update
+  output: {
+    filenameHash: false,
+  },
+};
+`,
+  );
+
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
+  fs.rmSync(distIndexFile, { force: true });
+
+  fse.outputFileSync(indexFile, `console.log('hello2!');`);
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello2!');
+
+  process.kill();
+});
