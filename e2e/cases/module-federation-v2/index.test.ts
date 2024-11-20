fb1314f6339448a4c3ead060cc32b8054de3f4dc
neverland <chenjiahan.jait@bytedance.com>
2024-11-20 17:46:32 +0800

    test(e2e): enable MF v2 HMR case (#4024)

--- e2e/cases/module-federation-v2/index.test.ts
+++ e2e/cases/module-federation-v2/index.test.ts
@@ -60,40 +60,44 @@ rspackOnlyTest(
   },
 );
 
-// TODO support for remote HMR
-test.skip('should allow remote module to perform HMR', async ({ page }) => {
-  // HMR cases will fail in Windows
-  if (process.platform === 'win32') {
-    test.skip();
-  }
+rspackOnlyTest(
+  'should allow remote module to perform HMR',
+  async ({ page }) => {
+    // HMR cases will fail in Windows
+    if (process.platform === 'win32') {
+      test.skip();
+    }
 
-  writeButtonCode();
+    writeButtonCode();
 
-  const remotePort = await getRandomPort();
+    const remotePort = await getRandomPort();
 
-  process.env.REMOTE_PORT = remotePort.toString();
+    process.env.REMOTE_PORT = remotePort.toString();
 
-  const remoteApp = await dev({
-    cwd: remote,
-  });
-  const hostApp = await dev({
-    cwd: host,
-  });
+    const remoteApp = await dev({
+      cwd: remote,
+    });
+    const hostApp = await dev({
+      cwd: host,
+    });
 
-  await gotoPage(page, remoteApp);
-  await expect(page.locator('#title')).toHaveText('Remote');
-  await expect(page.locator('#button')).toHaveText('Button from remote');
+    await gotoPage(page, remoteApp);
+    await expect(page.locator('#title')).toHaveText('Remote');
+    await expect(page.locator('#button')).toHaveText('Button from remote');
 
-  await gotoPage(page, hostApp);
-  await expect(page.locator('#title')).toHaveText('Host');
-  await expect(page.locator('#button')).toHaveText('Button from remote');
+    await gotoPage(page, hostApp);
+    await expect(page.locator('#title')).toHaveText('Host');
+    await expect(page.locator('#button')).toHaveText('Button from remote');
 
-  writeButtonCode('Button from remote (HMR)');
-  await expect(page.locator('#button')).toHaveText('Button from remote (HMR)');
+    writeButtonCode('Button from remote (HMR)');
+    await expect(page.locator('#button')).toHaveText(
+      'Button from remote (HMR)',
+    );
 
-  await hostApp.close();
-  await remoteApp.close();
-});
+    await hostApp.close();
+    await remoteApp.close();
+  },
+);
 
 // TODO downgrade syntax
 test.skip('should transform module federation runtime with SWC', async () => {
