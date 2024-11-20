31484c1a537434168ccafa7bf34d92faa8a593c2
neverland <chenjiahan.jait@bytedance.com>
2024-11-20 17:34:31 +0800

    test(e2e): enable MF v2 HMR case

--- e2e/cases/module-federation-v2/remote/rsbuild.config.ts
+++ e2e/cases/module-federation-v2/remote/rsbuild.config.ts
@@ -1,30 +1,22 @@
-import { ModuleFederationPlugin } from '@module-federation/rspack';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'remote',
+      exposes: {
+        './Button': './src/test-temp-Button',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   server: {
     port: Number(process.env.REMOTE_PORT) || 3002,
   },
   dev: {
     writeToDisk: true,
-    assetPrefix: true,
-  },
-  tools: {
-    rspack: {
-      output: {
-        uniqueName: 'remote',
-      },
-      plugins: [
-        new ModuleFederationPlugin({
-          name: 'remote',
-          exposes: {
-            './Button': './src/test-temp-Button',
-          },
-          shared: ['react', 'react-dom'],
-        }),
-      ],
-    },
   },
 });
