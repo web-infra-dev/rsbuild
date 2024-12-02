9d7e1aed3e050da79cefe62809422e6fb04affd3
neverland <chenjiahan.jait@bytedance.com>
2024-12-02 17:08:05 +0800

    fix

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,9 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  resolve: {
-    alias(config) {
-      delete config['@swc/helpers'];
-    },
-  },
 });
