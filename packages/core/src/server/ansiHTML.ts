7767ad3b49bb7d0d615322865d088ecd4245f8c2
neverland <chenjiahan.jait@bytedance.com>
2024-12-27 07:42:30 +0800

    fix: ansi HTML edge cases

--- packages/core/src/server/ansiHTML.ts
+++ packages/core/src/server/ansiHTML.ts
@@ -1,8 +1,8 @@
-const openCodes: Record<string, string> = {
+const styles: Record<string, string> = {
   1: 'font-weight:bold', // bold
   2: 'opacity:0.5', // dim
   3: 'font-style:italic', // italic
-  4: 'text-decoration:underline', // underscore
+  4: 'text-decoration:underline;text-underline-offset:3px', // underscore
   8: 'display:none', // hidden
   9: 'text-decoration:line-through', // delete
   30: 'color:#000', // darkgrey
@@ -13,8 +13,13 @@ const openCodes: Record<string, string> = {
   35: 'color:#f76ebe', // magenta, hsl(300deg 90% 70%)
   36: 'color:#6eecf7', // cyan, hsl(210deg 90% 70%)
   37: 'color:#f0f0f0', // lightgrey, hsl(0deg 0% 94%)
-  90: 'color:#888', // darkgrey
+  90: 'color:#888', // bright black
 };
+
+// use the same color for bright colors
+for (let i = 91; i <= 97; i++) {
+  styles[i] = styles[i - 60];
+}
 
 const closeCode = [0, 21, 22, 23, 24, 27, 28, 29, 39, 49];
 
@@ -28,21 +33,20 @@ export function ansiHTML(text: string): string {
   let ret = text.replace(
     // biome-ignore lint/suspicious/noControlCharactersInRegex: allowed
     /\x1B\[([0-9;]+)m/g,
-    (_match: string, seq: string): string => {
-      const openStyle = openCodes[seq];
-      if (openStyle) {
-        // If current sequence has been opened, close it.
-        if (ansiCodes.indexOf(seq) !== -1) {
-          ansiCodes.pop();
-          return '</span>';
+    (_match: string, sequences: string): string => {
+      let style = '';
+      for (const seq of sequences.split(';')) {
+        if (styles[seq]) {
+          style += `${styles[seq]};`;
         }
-        // Open tag.
-        ansiCodes.push(seq);
-        return `<span style="${openStyle};">`;
       }
 
-      if (closeCode.includes(Number(seq))) {
-        // Pop sequence
+      if (style) {
+        ansiCodes.push(sequences);
+        return `<span style="${style}">`;
+      }
+
+      if (closeCode.includes(Number(sequences)) && ansiCodes.length > 0) {
         ansiCodes.pop();
         return '</span>';
       }
