d5c873bc426b24e49f1c16693df71723739c818c
neverland <chenjiahan.jait@bytedance.com>
2024-12-26 23:02:32 +0800

    refactor: rewrite ansiHTML and add test cases

--- packages/core/src/server/ansiHTML.ts
+++ packages/core/src/server/ansiHTML.ts
@@ -1,89 +1,36 @@
-/**
- * This module is modified based on `ansi-html-community`
- * https://github.com/mahdyar/ansi-html-community
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * https://github.com/mahdyar/ansi-html-community/blob/master/LICENSE
- */
-
-// https://github.com/chalk/ansi-regex
-function ansiRegex() {
-  // Valid string terminator sequences are BEL, ESC\, and 0x9c
-  const ST = '(?:\\u0007|\\u001B\\u005C|\\u009C)';
-  const pattern = [
-    `[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?${ST})`,
-    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
-  ].join('|');
-
-  return new RegExp(pattern, 'g');
-}
-
-const colors: Record<string, string> = {
-  black: '#000',
-  // hsl(0deg 95% 70%)
-  red: '#fb6a6a',
-  // hsl(135deg 90% 70%)
-  green: '#6ef790',
-  // hsl(65deg 90% 75%)
-  yellow: '#eff986',
-  // hsl(185deg 90% 70%)
-  cyan: '#6eecf7',
-  // hsl(210deg 90% 70%)
-  blue: '#6eb2f7',
-  // hsl(325deg 90% 70%)
-  magenta: '#f76ebe',
-  lightgrey: '#f0f0f0',
-  darkgrey: '#888',
+const openCodes: Record<string, string> = {
+  1: 'font-weight:bold', // bold
+  2: 'opacity:0.5', // dim
+  3: 'font-style:italic', // italic
+  4: 'text-decoration:underline', // underscore
+  8: 'display:none', // hidden
+  9: 'text-decoration:line-through', // delete
+  30: 'color:#000', // darkgrey
+  31: 'color:#fb6a6a', // red, hsl(0deg 95% 70%)
+  32: 'color:#6ef790', // green, hsl(65deg 90% 75%)
+  33: 'color:#eff986', // yellow, hsl(185deg 90% 70%)
+  34: 'color:#6eb2f7', // blue, hsl(325deg 90% 70%)
+  35: 'color:#f76ebe', // magenta, hsl(300deg 90% 70%)
+  36: 'color:#6eecf7', // cyan, hsl(210deg 90% 70%)
+  37: 'color:#f0f0f0', // lightgrey, hsl(0deg 0% 94%)
+  90: 'color:#888', // darkgrey
 };
 
-const styles: Record<string, string> = {
-  30: 'black',
-  31: 'red',
-  32: 'green',
-  33: 'yellow',
-  34: 'blue',
-  35: 'magenta',
-  36: 'cyan',
-  37: 'lightgrey',
-};
-
-const openTags: Record<string, string> = {
-  '1': 'font-weight:bold', // bold
-  '2': 'opacity:0.5', // dim
-  '3': '<i>', // italic
-  '4': '<u>', // underscore
-  '8': 'display:none', // hidden
-  '9': '<del>', // delete
-};
-
-const closeTags: Record<string, string> = {
-  '23': '</i>', // reset italic
-  '24': '</u>', // reset underscore
-  '29': '</del>', // reset delete
-};
-
-for (const n of [0, 21, 22, 27, 28, 39, 49]) {
-  closeTags[n.toString()] = '</span>';
-}
+const closeCode = [0, 21, 22, 23, 24, 27, 28, 29, 39, 49];
 
 /**
- * Converts text with ANSI color codes to HTML markup.
+ * Converts text with ANSI color codes to HTML markup
  */
 export function ansiHTML(text: string): string {
-  // Returns the text if the string has no ANSI escape code.
-  if (!ansiRegex().test(text)) {
-    return text;
-  }
-
-  // Cache opened sequence.
+  // Cache opened sequence
   const ansiCodes: string[] = [];
-  // Replace with markup.
+  // Replace with markup
   let ret = text.replace(
     // biome-ignore lint/suspicious/noControlCharactersInRegex: allowed
-    /\x1B\[(\d+)m/g,
+    /\x1B\[([0-9;]+)m/g,
     (_match: string, seq: string): string => {
-      const ot = openTags[seq];
-      if (ot) {
+      const openStyle = openCodes[seq];
+      if (openStyle) {
         // If current sequence has been opened, close it.
         if (ansiCodes.indexOf(seq) !== -1) {
           ansiCodes.pop();
@@ -91,38 +38,24 @@ export function ansiHTML(text: string): string {
         }
         // Open tag.
         ansiCodes.push(seq);
-        return ot[0] === '<' ? ot : `<span style="${ot}">`;
+        return `<span style="${openStyle};">`;
       }
 
-      const ct = closeTags[seq];
-      if (ct) {
+      if (closeCode.includes(Number(seq))) {
         // Pop sequence
         ansiCodes.pop();
-        return ct;
+        return '</span>';
       }
       return '';
     },
   );
 
-  // Make sure tags are closed.
-  const l = ansiCodes.length;
-  if (l > 0) {
-    ret += Array(l + 1).join('</span>');
+  // Make sure tags are closed
+  if (ansiCodes.length > 0) {
+    ret += Array(ansiCodes.length + 1).join('</span>');
   }
 
   return ret;
 }
-
-function setTags(): void {
-  openTags['90'] = `color:${colors.darkgrey}`;
-
-  for (const code in styles) {
-    const color = styles[code];
-    const oriColor = colors[color] || colors.black;
-    openTags[code] = `color:${oriColor}`;
-  }
-}
-
-setTags();
 
 export default ansiHTML;
