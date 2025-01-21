6c03ee2f729a1cd6f9c0644189402348ad7a8986
neverland <chenjiahan.jait@bytedance.com>
2025-01-21 21:13:04 +0800

    fix

--- e2e/cases/cli/build-watch-restart/index.test.ts
+++ e2e/cases/cli/build-watch-restart/index.test.ts
@@ -1,61 +1,58 @@
 import { exec } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
-import { awaitFileExists, rspackOnlyTest, webpackOnlyTest } from '@e2e/helper';
-import { expect } from '@playwright/test';
+import { awaitFileExists, webpackOnlyTest } from '@e2e/helper';
+import test, { expect } from '@playwright/test';
 import fse from 'fs-extra';
 
-rspackOnlyTest.skip(
-  'should support restart build when config changed',
-  async () => {
-    const indexFile = path.join(__dirname, 'src/index.js');
-    const distIndexFile = path.join(__dirname, 'dist/static/js/index.js');
-    fs.rmSync(indexFile, { force: true });
-    fs.rmSync(distIndexFile, { force: true });
-    const tempConfigFile = path.join(__dirname, 'test-temp-rsbuild.config.mjs');
+test.skip('should support restart build when config changed', async () => {
+  const indexFile = path.join(__dirname, 'src/index.js');
+  const distIndexFile = path.join(__dirname, 'dist/static/js/index.js');
+  fs.rmSync(indexFile, { force: true });
+  fs.rmSync(distIndexFile, { force: true });
+  const tempConfigFile = path.join(__dirname, 'test-temp-rsbuild.config.mjs');
 
-    fse.outputFileSync(
-      tempConfigFile,
-      `export default {
+  fse.outputFileSync(
+    tempConfigFile,
+    `export default {
   output: {
     filenameHash: false,
   },
 };
 `,
-    );
+  );
 
-    fse.outputFileSync(indexFile, `console.log('hello!');`);
+  fse.outputFileSync(indexFile, `console.log('hello!');`);
 
-    const process = exec(`npx rsbuild build --watch -c ${tempConfigFile}`, {
-      cwd: __dirname,
-    });
+  const process = exec(`npx rsbuild build --watch -c ${tempConfigFile}`, {
+    cwd: __dirname,
+  });
 
-    await awaitFileExists(distIndexFile);
-    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
-    fs.rmSync(distIndexFile, { force: true });
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
+  fs.rmSync(distIndexFile, { force: true });
 
-    fse.outputFileSync(
-      tempConfigFile,
-      `export default {
+  fse.outputFileSync(
+    tempConfigFile,
+    `export default {
   // update
   output: {
     filenameHash: false,
   },
 };
 `,
-    );
+  );
 
-    await awaitFileExists(distIndexFile);
-    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
-    fs.rmSync(distIndexFile, { force: true });
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello!');
+  fs.rmSync(distIndexFile, { force: true });
 
-    fse.outputFileSync(indexFile, `console.log('hello2!');`);
-    await awaitFileExists(distIndexFile);
-    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello2!');
+  fse.outputFileSync(indexFile, `console.log('hello2!');`);
+  await awaitFileExists(distIndexFile);
+  expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('hello2!');
 
-    process.kill();
-  },
-);
+  process.kill();
+});
 
 webpackOnlyTest(
   'should support restart webpack build when config changed',
