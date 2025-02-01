3aba4e9e836c1ab24383e708bf1fffe75559ff0c
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-01-31 18:05:43 -0800

    fix(plugin-react-router): get examples working

--- examples/react-router/custom-node-server/rsbuild.config.ts
+++ examples/react-router/custom-node-server/rsbuild.config.ts
@@ -4,6 +4,15 @@ import { pluginReactRouter } from '@rsbuild/plugin-react-router';
 
 export default defineConfig(() => {
   return {
+    resolve: {
+      dedupe: [
+        'react',
+        'react-dom',
+        'react-router',
+        'react-router/dom',
+        '@react-router/node',
+      ],
+    },
     plugins: [pluginReactRouter(), pluginReact()],
   };
 });
