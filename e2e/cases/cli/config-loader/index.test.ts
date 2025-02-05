f03806a2b489c762c609d4f60a21e3552c599de0
neverland <chenjiahan.jait@bytedance.com>
2025-02-05 21:54:59 +0800

    feat: native runtime support for loading config

---
+++ e2e/cases/cli/config-loader/index.test.ts
@@ -0,0 +1,18 @@
+import { execSync } from 'node:child_process';
+import path from 'node:path';
+import { globContentJSON, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest('should use Node.js native loader to load config', async () => {
+  execSync(
+    'NODE_OPTIONS="--experimental-strip-types" npx rsbuild build --config-loader native',
+    {
+      cwd: __dirname,
+    },
+  );
+
+  const outputs = await globContentJSON(path.join(__dirname, 'dist-custom'));
+  const outputFiles = Object.keys(outputs);
+
+  expect(outputFiles.length > 1).toBeTruthy();
+});
