fb15cfeec5e5d78f68467fcd65c501659dbbf37e
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-02-11 15:53:15 +0800

    fix: dirname in mjs

--- e2e/cases/css/tailwindcss-mjs/postcss.config.mjs
+++ e2e/cases/css/tailwindcss-mjs/postcss.config.mjs
@@ -2,7 +2,7 @@ export default {
   plugins: {
     '@tailwindcss/postcss': {
       // Ensure tailwindcss only watch files in the current directory
-      base: __dirname,
+      base: import.meta.dirname,
     },
   },
 };
