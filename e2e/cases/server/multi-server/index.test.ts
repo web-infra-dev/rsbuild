caa8a87ef7dec990ef29a1facf66802150fab4ab
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-02-19 18:32:37 +0800

    test: multiple rsbuild dev servers should work correctly

---
+++ e2e/cases/server/multi-server/index.test.ts
@@ -0,0 +1,71 @@
+import { getRandomPort } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+import { createRsbuild } from '@rsbuild/core';
+import polka from 'polka';
+
+test('multiple rsbuild dev servers should work correctly', async ({ page }) => {
+  const rsbuild = await createRsbuild({
+    cwd: import.meta.dirname,
+    rsbuildConfig: {
+      output: {
+        distPath: {
+          root: 'dist',
+        },
+      },
+      dev: {
+        assetPrefix: '/app',
+      },
+      server: {
+        printUrls: false,
+      },
+    },
+  });
+
+  const rsbuild1 = await createRsbuild({
+    cwd: import.meta.dirname,
+    rsbuildConfig: {
+      source: {
+        entry: {
+          index: './src/index1',
+        },
+      },
+      output: {
+        distPath: {
+          root: 'dist-1',
+        },
+      },
+      server: {
+        printUrls: false,
+      },
+      dev: {
+        assetPrefix: '/app1',
+      },
+      environments: {
+        web1: {},
+      },
+    },
+  });
+
+  const rsbuildServer = await rsbuild.createDevServer();
+
+  const rsbuildServer1 = await rsbuild1.createDevServer();
+
+  const app = polka();
+
+  app.use('/app', rsbuildServer.middlewares);
+  app.use('/app1', rsbuildServer1.middlewares);
+
+  const port = await getRandomPort();
+
+  const { server } = app.listen({ port });
+  page.goto(`http://localhost:${port}/app`);
+  await expect(page.innerHTML('#test')).resolves.toBe('Hello Rsbuild!');
+
+  page.goto(`http://localhost:${port}/app1`);
+  await expect(page.innerHTML('#test')).resolves.toBe('Hello Rsbuild1!');
+
+  await rsbuildServer.close();
+  await rsbuildServer1.close();
+
+  server?.close();
+});
---
+++ e2e/cases/server/multi-server/src/index.js
@@ -0,0 +1,5 @@
+const testEl = document.createElement('div');
+testEl.id = 'test';
+testEl.innerHTML = 'Hello Rsbuild!';
+
+document.body.appendChild(testEl);
