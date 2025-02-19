d6e3857c6b757d7ee73a5ce1c8f77c959bac81e8
neverland <chenjiahan.jait@bytedance.com>
2025-02-19 22:47:35 +0800

    feat(cache): add env files to buildDependencies

---
+++ e2e/cases/cache/cache-digest/index.test.ts
@@ -0,0 +1,47 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { build } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+import type { RsbuildConfig } from '@rsbuild/core';
+
+test('`buildCache.cacheDigest` should work as expected', async () => {
+  const cacheDirectory = path.resolve(
+    import.meta.dirname,
+    './node_modules/.cache/test-cache-digest',
+  );
+
+  fs.rmSync(cacheDirectory, { recursive: true, force: true });
+
+  const getBuildConfig = (input: string) => ({
+    cwd: import.meta.dirname,
+    rsbuildConfig: {
+      tools: {
+        bundlerChain: (chain) => {
+          if (input === 'foo') {
+            chain.resolve.extensions.prepend('.test.js');
+          }
+        },
+      },
+      performance: {
+        buildCache: {
+          cacheDirectory,
+          cacheDigest: [input],
+        },
+      },
+    } as RsbuildConfig,
+  });
+
+  // first build without cache
+  let rsbuild = await build(getBuildConfig(''));
+
+  expect(
+    (await rsbuild.getIndexFile()).content.includes('222222'),
+  ).toBeTruthy();
+
+  rsbuild = await build(getBuildConfig('foo'));
+
+  // extension '.test.js' should work
+  expect(
+    (await rsbuild.getIndexFile()).content.includes('111111'),
+  ).toBeTruthy();
+});
