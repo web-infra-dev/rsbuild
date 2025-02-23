86fa6233babf5413f2aeb9a748c8230b6666c527
hangCode <1981616118@qq.com>
2025-02-23 21:39:12 +0800

    fix: 多余修改

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,25 +1,10 @@
-import React, { Suspense, useState } from 'react';
 import './App.css';
-const Test = React.lazy(() => import('./test'));
 
 const App = () => {
-  const [showTest, setShowTest] = useState<boolean>(false);
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
-      <div
-        onClick={() => {
-          setShowTest(true);
-        }}
-      >
-        加载
-      </div>
-      {showTest && (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Test />
-        </Suspense>
-      )}
     </div>
   );
 };
