13688f9a07ef1d98aeba185a02b0bb1b0cee6ba2
pshu <pishu.spf@antfin.com>
2025-02-24 16:49:35 +0800

    test: only dump vue react

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   // Retry on CI
   retries: process.env.CI ? 3 : 0,
   // Print line for each test being run in CI
-  reporter: 'list',
+  reporter: [['list'], ['json', { outputFile: 'report.json' }]],
 });
