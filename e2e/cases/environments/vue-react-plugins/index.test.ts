36ff19b3229866b8b7183ea4745da4b819af80b7
pshu <pishu.spf@antfin.com>
2025-02-24 16:15:46 +0800

    test: more logs

--- e2e/cases/environments/vue-react-plugins/index.test.ts
+++ e2e/cases/environments/vue-react-plugins/index.test.ts
@@ -2,10 +2,15 @@ import { build, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
 
 rspackOnlyTest('should build basic Vue jsx correctly', async ({ page }) => {
+  console.time('rsbuild');
   const rsbuild = await build({
     cwd: __dirname,
     page,
   });
+
+  console.timeEnd('rsbuild');
+
+  console.time('react');
 
   const reactUrl = new URL(`http://localhost:${rsbuild.port}/react`);
 
@@ -13,12 +18,17 @@ rspackOnlyTest('should build basic Vue jsx correctly', async ({ page }) => {
 
   await expect(page.locator('#test')).toHaveText('Hello Rsbuild!');
 
+  console.timeEnd('react');
+
+  console.time('vue');
   const vueUrl = new URL(`http://localhost:${rsbuild.port}/vue`);
 
   await page.goto(vueUrl.href);
 
   const button1 = page.locator('#button1');
   await expect(button1).toHaveText('A: 0');
+
+  console.timeEnd('vue');
 
   await rsbuild.close();
 });
