686edde9d47b6de9d007b7387e62062b2c6edadf
neverland <chenjiahan.jait@bytedance.com>
2025-02-24 21:14:29 +0800

    chore: migrate assets retry plugin to separate repo

--- e2e/cases/assets/assets-retry/src/AsyncCompTest.tsx
+++
@@ -1,5 +0,0 @@
-import './AsyncCompTest.css';
-
-export default function AsyncCompTest() {
-  return <div id="async-comp-test">Hello AsyncCompTest</div>;
-}
--- e2e/cases/assets/assets-retry/src/CompTest.tsx
+++
@@ -1,5 +0,0 @@
-import './CompTest.css';
-
-export default function CompTest() {
-  return <div id="comp-test">Hello CompTest</div>;
-}
