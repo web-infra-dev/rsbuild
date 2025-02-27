56bbdc6bbaf17e6787da0c1500870066ab5ae3d3
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-02-27 17:33:27 +0800

    fix: processAssets hook filter error

--- e2e/cases/plugin-api/plugin-process-assets-by-environments/index.test.ts
+++ e2e/cases/plugin-api/plugin-process-assets-by-environments/index.test.ts
@@ -2,6 +2,7 @@ import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 import { build, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
+import type { RsbuildPluginAPI } from '@rsbuild/core';
 
 rspackOnlyTest(
   'should allow plugin to process assets by environments',
@@ -14,3 +15,29 @@ rspackOnlyTest(
     expect(existsSync(join(rsbuild.distPath, 'server/index.js'))).toBeTruthy();
   },
 );
+
+rspackOnlyTest('should filter environments correctly', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    plugins: [
+      {
+        name: 'my-plugin-node',
+        setup(api: RsbuildPluginAPI) {
+          api.processAssets(
+            { stage: 'summarize', environments: ['node'] },
+            ({ assets, compilation }) => {
+              for (const key of Object.keys(assets)) {
+                if (key.endsWith('.js')) {
+                  compilation.deleteAsset(key);
+                }
+              }
+            },
+          );
+        },
+      },
+    ],
+  });
+
+  expect(existsSync(join(rsbuild.distPath, 'static/index.js'))).toBeFalsy();
+  expect(existsSync(join(rsbuild.distPath, 'server/index.js'))).toBeFalsy();
+});
