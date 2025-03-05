b5e47b67e8b87b01d23fb44c598a3f6846cbefd9
neverland <chenjiahan.jait@bytedance.com>
2025-03-05 17:12:55 +0800

    fix(vue): order problem with pluginReact

--- packages/plugin-vue/src/index.ts
+++ packages/plugin-vue/src/index.ts
@@ -95,7 +95,12 @@ export function pluginVue(options: PluginVueOptions = {}): RsbuildPlugin {
         // Support for lang="postcss" and lang="pcss" in SFC
         chain.module.rule(CHAIN_ID.RULE.CSS).test(/\.(?:css|postcss|pcss)$/);
 
-        chain.plugin(CHAIN_ID.PLUGIN.VUE_LOADER_PLUGIN).use(VueLoaderPlugin);
+        chain
+          .plugin(CHAIN_ID.PLUGIN.VUE_LOADER_PLUGIN)
+          // Ensure that the VueLoaderPlugin is applied before the ReactFastRefreshPlugin
+          // otherwise the VueLoaderPlugin will throw an error for `builtin:react-refresh-loader`
+          .before(CHAIN_ID.PLUGIN.REACT_FAST_REFRESH)
+          .use(VueLoaderPlugin);
       });
 
       applySplitChunksRule(api, options.splitChunks);
