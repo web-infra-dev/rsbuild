e86b0a962871caf5e015ae7c6debdabdd7b50093
neverland <chenjiahan.jait@bytedance.com>
2025-03-14 06:16:58 +0800

    feat: integrate env variable loading into createRsbuild options (#4783)

---
+++ e2e/cases/javascript-api/build-and-load-env/index.test.ts
@@ -0,0 +1,46 @@
+import { expect } from '@playwright/test';
+import { createRsbuild } from '@rsbuild/core';
+import { rspackOnlyTest } from 'scripts';
+
+rspackOnlyTest('should not load env by default', async () => {
+  const rsbuild = await createRsbuild({
+    cwd: __dirname,
+    loadEnv: false,
+    rsbuildConfig: {
+      performance: {
+        printFileSize: false,
+      },
+    },
+  });
+
+  expect(process.env.PUBLIC_FOO).toBe(undefined);
+  expect(process.env.PUBLIC_BAR).toBe(undefined);
+  const { close } = await rsbuild.build();
+  await close();
+});
+
+rspackOnlyTest(
+  'should allow to call `build` with `loadEnv` options',
+  async () => {
+    const rsbuild = await createRsbuild({
+      cwd: __dirname,
+      loadEnv: {
+        mode: 'prod',
+      },
+      rsbuildConfig: {
+        performance: {
+          printFileSize: false,
+        },
+      },
+    });
+
+    expect(process.env.PUBLIC_FOO).toBe('foo');
+    expect(process.env.PUBLIC_BAR).toBe('bar');
+
+    const { close } = await rsbuild.build();
+    await close();
+
+    expect(process.env.PUBLIC_FOO).toBe(undefined);
+    expect(process.env.PUBLIC_BAR).toBe(undefined);
+  },
+);
