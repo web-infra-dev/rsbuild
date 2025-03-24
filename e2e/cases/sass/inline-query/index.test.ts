9560ed42b0d974c226560e4d886895eb7122f66c
neverland <chenjiahan.jait@bytedance.com>
2025-03-24 16:26:35 +0800

    fix

--- e2e/cases/sass/inline-query/index.test.ts
+++ e2e/cases/sass/inline-query/index.test.ts
@@ -1,41 +1,43 @@
-import { build, dev } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
 
-test('should allow to import inline Sass files in development mode', async ({
-  page,
-}) => {
-  const rsbuild = await dev({
-    cwd: __dirname,
-    page,
-  });
+rspackOnlyTest(
+  'should allow to import inline Sass files in development mode',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+    });
 
-  const aInline: string = await page.evaluate('window.aInline');
-  const bInline: string = await page.evaluate('window.bInline');
+    const aInline: string = await page.evaluate('window.aInline');
+    const bInline: string = await page.evaluate('window.bInline');
 
-  expect(
-    aInline.includes('.header-class') && aInline.includes('color: red'),
-  ).toBe(true);
-  expect(
-    bInline.includes('.title-class') && bInline.includes('font-size: 14px'),
-  ).toBe(true);
+    expect(
+      aInline.includes('.header-class') && aInline.includes('color: red'),
+    ).toBe(true);
+    expect(
+      bInline.includes('.title-class') && bInline.includes('font-size: 14px'),
+    ).toBe(true);
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
 
-test('should allow to import inline Sass files in production mode', async ({
-  page,
-}) => {
-  const rsbuild = await build({
-    cwd: __dirname,
-    page,
-  });
+rspackOnlyTest(
+  'should allow to import inline Sass files in production mode',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+    });
 
-  expect(await page.evaluate('window.aInline')).toBe(
-    '.header-class{color:red}',
-  );
-  expect(await page.evaluate('window.bInline')).toBe(
-    '.title-class{font-size:14px}',
-  );
+    expect(await page.evaluate('window.aInline')).toBe(
+      '.header-class{color:red}',
+    );
+    expect(await page.evaluate('window.bInline')).toBe(
+      '.title-class{font-size:14px}',
+    );
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
