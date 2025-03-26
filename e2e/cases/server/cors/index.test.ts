c1d1c31a6f1a990f84f747e70a57d116a46b2156
neverland <chenjiahan.jait@bytedance.com>
2025-03-26 15:18:07 +0800

    test

--- e2e/cases/server/cors/index.test.ts
+++ e2e/cases/server/cors/index.test.ts
@@ -41,7 +41,29 @@ test('should include CORS headers for preview server if `cors` is `true`', async
   await rsbuild.close();
 });
 
-test('should allow to disable CORS', async ({ page, request }) => {
+test('should not include CORS headers for dev server if `cors` is `false`', async ({
+  page,
+  request,
+}) => {
+  const rsbuild = await dev({
+    cwd: __dirname,
+    page,
+    rsbuildConfig: {
+      server: {
+        cors: false,
+      },
+    },
+  });
+
+  const response = await request.get(`http://127.0.0.1:${rsbuild.port}`);
+  expect(response.headers()).not.toHaveProperty('access-control-allow-origin');
+  await rsbuild.close();
+});
+
+test('should not include CORS headers for preview server if `cors` is `false`', async ({
+  page,
+  request,
+}) => {
   const rsbuild = await build({
     cwd: __dirname,
     page,
@@ -54,7 +76,6 @@ test('should allow to disable CORS', async ({ page, request }) => {
 
   const response = await request.get(`http://127.0.0.1:${rsbuild.port}`);
   expect(response.headers()).not.toHaveProperty('access-control-allow-origin');
-
   await rsbuild.close();
 });
 
