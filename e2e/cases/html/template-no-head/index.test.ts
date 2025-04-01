fd20be7fcabaa10cdedf74f8ca94c2e8260c0fa4
neverland <chenjiahan.jait@bytedance.com>
2025-04-01 14:28:43 +0800

    test: fix

--- e2e/cases/html/template-no-head/index.test.ts
+++ e2e/cases/html/template-no-head/index.test.ts
@@ -1,16 +1,19 @@
-import { build } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
 
 // https://github.com/web-infra-dev/rsbuild/issues/4924
-test('should inject tags to HTML template without <head> tag', async () => {
-  const rsbuild = await build({
-    cwd: __dirname,
-  });
-  const files = await rsbuild.unwrapOutputJSON();
+rspackOnlyTest(
+  'should inject tags to HTML template without <head> tag',
+  async () => {
+    const rsbuild = await build({
+      cwd: __dirname,
+    });
+    const files = await rsbuild.unwrapOutputJSON();
 
-  const indexHtml =
-    files[Object.keys(files).find((file) => file.endsWith('index.html'))!];
-  expect(indexHtml).toContain(
-    '<!doctype html><head><title>Rsbuild App</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src=',
-  );
-});
+    const indexHtml =
+      files[Object.keys(files).find((file) => file.endsWith('index.html'))!];
+    expect(indexHtml).toContain(
+      '<!doctype html><head><title>Rsbuild App</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src=',
+    );
+  },
+);
