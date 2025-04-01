158d9d7934d9994ac6bfff979a8591ad0a5531e8
neverland <chenjiahan.jait@bytedance.com>
2025-04-01 14:24:00 +0800

    fix: inject tags to HTML template without `<head>` tag

---
+++ e2e/cases/html/template-no-head/index.test.ts
@@ -0,0 +1,16 @@
+import { build } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+
+// https://github.com/web-infra-dev/rsbuild/issues/4924
+test('should inject tags to HTML template without <head> tag', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+  });
+  const files = await rsbuild.unwrapOutputJSON();
+
+  const indexHtml =
+    files[Object.keys(files).find((file) => file.endsWith('index.html'))!];
+  expect(indexHtml).toContain(
+    '<!doctype html><head><title>Rsbuild App</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><script defer src=',
+  );
+});
