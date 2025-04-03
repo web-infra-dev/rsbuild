4896360f2f65f4886a1bca91b08316cf92bd7cf3
Yiheng <yihengz@outlook.com>
2025-04-04 07:52:59 +0800

    feat: support for BROWSER_ARGS (#4886)

--- packages/core/src/server/open.ts
+++ packages/core/src/server/open.ts
@@ -87,7 +87,14 @@ async function openBrowser(url: string): Promise<boolean> {
   // It will always open new tab
   try {
     const { default: open } = await import('../../compiled/open/index.js');
-    const options = browser ? { app: { name: browser } } : {};
+    const options = browser
+      ? {
+          app: {
+            name: browser,
+            arguments: process.env.BROWSER_ARGS?.split(' '),
+          },
+        }
+      : {};
     await open(url, options);
     return true;
   } catch (err) {
