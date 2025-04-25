064f1f2c022987ac208044c4862067fcf38fd000
neverland <chenjiahan.jait@bytedance.com>
2025-04-25 15:40:49 +0800

    Update website/rspress.config.ts

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -127,7 +127,7 @@ export default defineConfig({
   lang: 'en',
   title: 'Rsbuild',
   description:
-    'Rsbuild is a high-performance build tool powered by Rspack. It provides a set of thoughtfully designed default build configs and an out-of-the-box DX.',
+    'Rsbuild is a high-performance build tool powered by Rspack. It provides out-of-the-box setup for enjoyable development experience.',
   icon: 'https://assets.rspack.dev/rsbuild/favicon-128x128.png',
   logo: {
     light: 'https://assets.rspack.dev/rsbuild/navbar-logo-light.png',
