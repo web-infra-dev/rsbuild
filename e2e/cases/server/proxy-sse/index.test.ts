cb54e0b80d5441793ac32030b55849f2b658ec06
neverland <chenjiahan.jait@bytedance.com>
2025-04-26 17:44:25 +0800

    test(e2e): add case for proxy SSE request

---
+++ e2e/cases/server/proxy-sse/index.test.ts
@@ -0,0 +1,62 @@
+import { dev, getRandomPort } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+import polka from 'polka';
+
+test('should proxy SSE request correctly', async ({ page }) => {
+  const ssePort = await getRandomPort();
+  const rsbuild = await dev({
+    cwd: __dirname,
+    page,
+    rsbuildConfig: {
+      server: {
+        proxy: {
+          '/api': {
+            target: `http://localhost:${ssePort}`,
+            pathRewrite: { '^/api': '' },
+          },
+        },
+      },
+    },
+  });
+
+  const app = polka();
+
+  app.get('/sse', (_req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+    res.write('Hello SSE1,');
+    res.write('Hello SSE2');
+  });
+
+  app.listen(ssePort);
+  const sseUrl = `http://localhost:${rsbuild.port}/api/sse`;
+
+  const result = await page.evaluate(async (url) => {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 2000);
+
+    try {
+      const response = await fetch(url, { signal: controller.signal });
+      const reader = response.body!.getReader();
+      const { value } = await reader.read();
+      const text = new TextDecoder().decode(value);
+
+      return {
+        ok: response.ok,
+        contentType: response.headers.get('content-type'),
+        text,
+      };
+    } finally {
+      clearTimeout(timeoutId);
+    }
+  }, sseUrl);
+
+  expect(result.ok).toBe(true);
+  expect(result.contentType).toBe('text/event-stream');
+  expect(result.text).toEqual('Hello SSE1,Hello SSE2');
+
+  await rsbuild.close();
+  app.server?.close();
+});
