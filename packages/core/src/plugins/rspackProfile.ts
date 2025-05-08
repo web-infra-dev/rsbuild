a5ca44951220841e2b0c053ad147304dfc13aac4
neverland <chenjiahan.jait@bytedance.com>
2025-05-08 16:55:43 +0800

    fix(profile): remove unused jscpuprofile.json (#5199)

--- packages/core/src/plugins/rspackProfile.ts
+++ packages/core/src/plugins/rspackProfile.ts
@@ -1,27 +1,12 @@
 import fs from 'node:fs';
-import inspector from 'node:inspector';
 import path from 'node:path';
 import rspack from '@rspack/core';
 import { color, isFileExists } from '../helpers';
 import { logger } from '../logger';
 import type { RsbuildPlugin } from '../types';
 
-const stopProfiler = (output: string, profileSession?: inspector.Session) => {
-  if (!profileSession) {
-    return;
-  }
-
-  profileSession.post('Profiler.stop', (error, param) => {
-    if (error) {
-      logger.error('Failed to generate JavaScript CPU profile:', error);
-      return;
-    }
-    fs.writeFileSync(output, JSON.stringify(param.profile));
-  });
-};
-
 // Referenced from Rspack CLI
-// https://github.com/web-infra-dev/rspack/blob/v1.3.0/packages/rspack-cli/src/utils/profile.ts
+// https://github.com/web-infra-dev/rspack/blob/v1.3.9/packages/rspack-cli/src/utils/profile.ts
 export const pluginRspackProfile = (): RsbuildPlugin => ({
   name: 'rsbuild:rspack-profile',
 
@@ -32,7 +17,7 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
 
     /**
      * RSPACK_PROFILE=ALL
-     * RSPACK_PROFILE=TRACE|CPU
+     * RSPACK_PROFILE=TRACE
      */
     const RSPACK_PROFILE = process.env.RSPACK_PROFILE?.toUpperCase();
 
@@ -43,18 +28,12 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
     const timestamp = Date.now();
     const profileDirName = `rspack-profile-${timestamp}`;
 
-    let profileSession: inspector.Session | undefined;
-
     const enableProfileTrace =
       RSPACK_PROFILE === 'ALL' || RSPACK_PROFILE.includes('TRACE');
-
-    const enableCPUProfile =
-      RSPACK_PROFILE === 'ALL' || RSPACK_PROFILE.includes('CPU');
 
     const onStart = async () => {
       // Note: Cannot obtain accurate `api.context.distPath` before config initialization
       const profileDir = path.join(api.context.distPath, profileDirName);
-
       const traceFilePath = path.join(profileDir, 'trace.json');
 
       if (!(await isFileExists(profileDir))) {
@@ -67,13 +46,6 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
           'chrome',
           traceFilePath,
         );
-      }
-
-      if (enableCPUProfile) {
-        profileSession = new inspector.Session();
-        profileSession.connect();
-        profileSession.post('Profiler.enable');
-        profileSession.post('Profiler.start');
       }
     };
 
@@ -89,12 +61,7 @@ export const pluginRspackProfile = (): RsbuildPlugin => ({
         rspack.experiments.globalTrace.cleanup();
       }
       const profileDir = path.join(api.context.distPath, profileDirName);
-
-      const cpuProfilePath = path.join(profileDir, 'jscpuprofile.json');
-
-      stopProfiler(cpuProfilePath, profileSession);
-
-      logger.info(`profile files saved to ${color.cyan(profileDir)}`);
+      logger.info(`profile file saved to ${color.cyan(profileDir)}`);
     });
   },
 });
