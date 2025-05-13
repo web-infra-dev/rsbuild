3dc387d7ca27e99b9052f0cce56df644c8aa08be
neverland <chenjiahan.jait@bytedance.com>
2025-05-13 14:23:02 +0800

    test: fix

--- e2e/cases/mode/basic/index.test.ts
+++ e2e/cases/mode/basic/index.test.ts
@@ -1,4 +1,4 @@
-import { build, dev } from '@e2e/helper';
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
 test('should allow to set development mode when building', async () => {
@@ -50,38 +50,39 @@ test('should allow to set none mode when building', async () => {
   expect(files[indexFile]).toContain('// this is a comment');
 });
 
-test('should allow to set production mode when starting dev server', async ({
-  page,
-}) => {
-  const rsbuild = await dev({
-    cwd: __dirname,
-    page,
-    rsbuildConfig: {
-      mode: 'production',
-      dev: {
-        writeToDisk: true,
+rspackOnlyTest(
+  'should allow to set production mode when starting dev server',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+      rsbuildConfig: {
+        mode: 'production',
+        dev: {
+          writeToDisk: true,
+        },
       },
-    },
-  });
+    });
 
-  const files = await rsbuild.getDistFiles(false);
+    const files = await rsbuild.getDistFiles(false);
 
-  // should have filename hash in production mode
-  const indexFile = Object.keys(files).find((key) =>
-    key.match(/static\/js\/index\.\w+\.js/),
-  )!;
+    // should have filename hash in production mode
+    const indexFile = Object.keys(files).find((key) =>
+      key.match(/static\/js\/index\.\w+\.js/),
+    )!;
 
-  // should replace `process.env.NODE_ENV` with `'production'`
-  expect(files[indexFile]).toContain('this is production mode!');
+    // should replace `process.env.NODE_ENV` with `'production'`
+    expect(files[indexFile]).toContain('this is production mode!');
 
-  // should remove comments
-  expect(files[indexFile]).not.toContain('// this is a comment');
+    // should remove comments
+    expect(files[indexFile]).not.toContain('// this is a comment');
 
-  // should not have JavaScript source map
-  const indexSourceMap = Object.keys(files).find((key) =>
-    key.endsWith('.js.map'),
-  );
-  expect(indexSourceMap).toBeFalsy();
+    // should not have JavaScript source map
+    const indexSourceMap = Object.keys(files).find((key) =>
+      key.endsWith('.js.map'),
+    );
+    expect(indexSourceMap).toBeFalsy();
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
