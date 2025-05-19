4b7638437cad5f2a36db0632ea10612c35c7fbcf
neverland <chenjiahan.jait@bytedance.com>
2025-05-19 14:29:24 +0800

    test(e2e): transpile .svelte files

---
+++ e2e/cases/svelte/transpile/src/App.svelte
@@ -0,0 +1,13 @@
+<main>
+  <p>Current value: {value}</p>
+  <button onclick={changeValue}>Change value</button>
+</main>
+
+<script lang="ts">
+  let value = $state('Hello World');
+
+  function changeValue() {
+    value = 'Hello Svelte';
+    console.log('test optional chaining', window?.value);
+  }
+</script>
