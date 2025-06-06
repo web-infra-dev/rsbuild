3374817db75b1b905ed27b5f33ecbfa46f8696c2
neverland <chenjiahan.jait@bytedance.com>
2025-06-06 10:50:11 +0800

    test(e2e): debug timeout

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,4 +5,9 @@ export default defineConfig({
   retries: process.env.CI ? 3 : 0,
   // Print line for each test being run in CI
   reporter: 'list',
+  timeout: 1 * 60 * 1000, // 1 minutes
+  use: {
+    // Record trace on first retry of each test.
+    trace: 'on-first-retry',
+  },
 });
