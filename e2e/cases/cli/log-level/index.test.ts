fdf6af52e8ca0025193c559875b2b11c8d236b67
neverland <chenjiahan.jait@bytedance.com>
2025-06-17 16:30:11 +0800

    fix: ensure log level can work for all logs

--- e2e/cases/cli/log-level/index.test.ts
+++ e2e/cases/cli/log-level/index.test.ts
@@ -1,21 +1,28 @@
 import { execSync } from 'node:child_process';
+import { stripVTControlCharacters as stripAnsi } from 'node:util';
 import { rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
 
 rspackOnlyTest('should run build command with log level: info', async () => {
-  const result = execSync('npx rsbuild build --logLevel info', {
-    cwd: __dirname,
-  }).toString();
+  const result = stripAnsi(
+    execSync('npx rsbuild build --logLevel info', {
+      cwd: __dirname,
+    }).toString(),
+  );
 
+  expect(result).toContain('Rsbuild v');
   expect(result).toContain('build started...');
   expect(result).toContain('built in');
 });
 
 rspackOnlyTest('should run build command with log level: warn', async () => {
-  const result = execSync('npx rsbuild build --logLevel warn', {
-    cwd: __dirname,
-  }).toString();
+  const result = stripAnsi(
+    execSync('npx rsbuild build --logLevel warn', {
+      cwd: __dirname,
+    }).toString(),
+  );
 
+  expect(result).not.toContain('Rsbuild v');
   expect(result).not.toContain('build started...');
   expect(result).not.toContain('built in');
 });
