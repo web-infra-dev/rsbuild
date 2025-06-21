37966130f9f97770432f8ce2e3d5f3823e02951b
neverland <chenjiahan.jait@bytedance.com>
2025-06-21 20:51:25 +0800

    chore(website): remove unused useUrl helper

--- website/theme/components/utils.ts
+++ website/theme/components/utils.ts
@@ -1,14 +1,6 @@
 import { useCallback } from 'react';
 import { useLang, usePageData, withBase } from 'rspress/runtime';
 
-export function useUrl(url: string) {
-  const lang = useLang();
-  const {
-    siteData: { lang: defaultLang },
-  } = usePageData();
-  return withBase(lang === defaultLang ? url : `/${lang}${url}`);
-}
-
 export function useI18nUrl() {
   const lang = useLang();
   const {
