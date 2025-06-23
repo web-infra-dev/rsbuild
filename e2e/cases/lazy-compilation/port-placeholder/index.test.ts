e8a76ff5d5671f8d6bec9f6c93bfd08db8f77ded
neverland <chenjiahan.jait@bytedance.com>
2025-06-23 17:01:26 +0800

    fix: lazy compilation port replacement (#5470)

--- e2e/cases/lazy-compilation/port-placeholder/index.test.ts
+++ e2e/cases/lazy-compilation/port-placeholder/index.test.ts
@@ -1,32 +1,32 @@
-import { dev, expectPoll, gotoPage } from '@e2e/helper';
+import { dev, expectPoll, gotoPage, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-// TODO: fixme
-test.skip('should replace port placeholder with actual port', async ({
-  page,
-}) => {
-  if (process.platform === 'win32') {
-    test.skip();
-  }
+rspackOnlyTest(
+  'should replace port placeholder with actual port',
+  async ({ page }) => {
+    if (process.platform === 'win32') {
+      test.skip();
+    }
 
-  const rsbuild = await dev({
-    cwd: __dirname,
-  });
+    const rsbuild = await dev({
+      cwd: __dirname,
+    });
 
-  await gotoPage(page, rsbuild, 'page1');
-  await expect(page.locator('#test')).toHaveText('Page 1');
-  await expectPoll(() =>
-    rsbuild.logs.some((log) => log.includes('building src/page1/index.js')),
-  ).toBeTruthy();
-  expect(
-    rsbuild.logs.some((log) => log.includes('building src/page2/index.js')),
-  ).toBeFalsy();
+    await gotoPage(page, rsbuild, 'page1');
+    await expect(page.locator('#test')).toHaveText('Page 1');
+    await expectPoll(() =>
+      rsbuild.logs.some((log) => log.includes('building src/page1/index.js')),
+    ).toBeTruthy();
+    expect(
+      rsbuild.logs.some((log) => log.includes('building src/page2/index.js')),
+    ).toBeFalsy();
 
-  await gotoPage(page, rsbuild, 'page2');
-  await expect(page.locator('#test')).toHaveText('Page 2');
-  await expectPoll(() =>
-    rsbuild.logs.some((log) => log.includes('building src/page2/index.js')),
-  ).toBeTruthy();
+    await gotoPage(page, rsbuild, 'page2');
+    await expect(page.locator('#test')).toHaveText('Page 2');
+    await expectPoll(() =>
+      rsbuild.logs.some((log) => log.includes('building src/page2/index.js')),
+    ).toBeTruthy();
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
