e56d70544daec14e5ff121f8f4620163217aa61b
neverland <chenjiahan.jait@bytedance.com>
2025-06-23 15:30:42 +0800

    fix

--- e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
+++ e2e/cases/lazy-compilation/add-initial-chunk/index.test.ts
@@ -1,20 +1,21 @@
-import { dev } from '@e2e/helper';
+import { dev, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
 // https://github.com/web-infra-dev/rspack/issues/6633
-test('should render pages correctly when using lazy compilation and add new initial chunk', async ({
-  page,
-}) => {
-  // TODO fix this case on Windows
-  if (process.platform === 'win32') {
-    test.skip();
-  }
+rspackOnlyTest(
+  'should render pages correctly when using lazy compilation and add new initial chunk',
+  async ({ page }) => {
+    // TODO fix this case on Windows
+    if (process.platform === 'win32') {
+      test.skip();
+    }
 
-  const rsbuild = await dev({
-    cwd: __dirname,
-    page,
-  });
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+    });
 
-  await expect(page.locator('#test')).toHaveText('Hello World!');
-  await rsbuild.close();
-});
+    await expect(page.locator('#test')).toHaveText('Hello World!');
+    await rsbuild.close();
+  },
+);
--- e2e/cases/lazy-compilation/dynamic-import/index.test.ts
+++ e2e/cases/lazy-compilation/dynamic-import/index.test.ts
@@ -1,27 +1,30 @@
-import { dev, expectPoll, gotoPage } from '@e2e/helper';
+import { dev, expectPoll, gotoPage, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-test('should lazy compile dynamic imported modules', async ({ page }) => {
-  // TODO fix this case on Windows
-  if (process.platform === 'win32') {
-    test.skip();
-  }
+rspackOnlyTest(
+  'should lazy compile dynamic imported modules',
+  async ({ page }) => {
+    // TODO fix this case on Windows
+    if (process.platform === 'win32') {
+      test.skip();
+    }
 
-  const rsbuild = await dev({
-    cwd: __dirname,
-  });
+    const rsbuild = await dev({
+      cwd: __dirname,
+    });
 
-  await expectPoll(() =>
-    rsbuild.logs.some((log) => log.includes('built in ')),
-  ).toBeTruthy();
-  expect(
-    rsbuild.logs.some((log) => log.includes('building src/foo.js')),
-  ).toBeFalsy();
+    await expectPoll(() =>
+      rsbuild.logs.some((log) => log.includes('built in ')),
+    ).toBeTruthy();
+    expect(
+      rsbuild.logs.some((log) => log.includes('building src/foo.js')),
+    ).toBeFalsy();
 
-  await gotoPage(page, rsbuild, 'index');
-  await expectPoll(() =>
-    rsbuild.logs.some((log) => log.includes('building src/foo.js')),
-  ).toBeTruthy();
+    await gotoPage(page, rsbuild, 'index');
+    await expectPoll(() =>
+      rsbuild.logs.some((log) => log.includes('building src/foo.js')),
+    ).toBeTruthy();
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
