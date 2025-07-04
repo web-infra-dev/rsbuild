ac39b814e0f2c7ff1efac3aaac3fbaa8e09047d7
neverland <chenjiahan.jait@bytedance.com>
2025-07-04 17:36:58 +0800

    feat(deps): update Rspack to v1.4.3 (#5532)

--- e2e/cases/source/source-include/index.test.ts
+++ e2e/cases/source/source-include/index.test.ts
@@ -1,17 +1,10 @@
 import { build } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
-import { pluginCheckSyntax } from '@rsbuild/plugin-check-syntax';
 
 test('should compile modules outside of project by default', async () => {
   const rsbuild = await build({
     cwd: __dirname,
-    plugins: [pluginCheckSyntax()],
     catchBuildError: true,
-    rsbuildConfig: {
-      output: {
-        overrideBrowserslist: ['ie 11'],
-      },
-    },
   });
 
   expect(rsbuild.buildError).toBeFalsy();
