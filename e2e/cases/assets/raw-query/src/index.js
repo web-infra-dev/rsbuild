f705cbbc32a94138ffb75c19cb64fe33bce41e61
neverland <chenjiahan.jait@bytedance.com>
2025-07-04 22:32:25 +0800

    fix: ensure that raw query of scripts can be accurately matched (#5538)

--- e2e/cases/assets/raw-query/src/index.js
+++ e2e/cases/assets/raw-query/src/index.js
@@ -1,9 +1,11 @@
 import img from '@assets/circle.svg?raw';
 import rawTs from './bar.ts?raw';
 import rawTsx from './baz.tsx?raw';
+import normalJs from './foo.js?other_raw';
 import rawJs from './foo.js?raw';
 
 window.rawImg = img;
 window.rawJs = rawJs;
 window.rawTs = rawTs;
 window.rawTsx = rawTsx;
+window.normalJs = normalJs;
