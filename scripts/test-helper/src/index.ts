de2728944d10d031a45aa7104015cc9fcc5da0db
neverland <chenjiahan.jait@bytedance.com>
2025-07-07 18:46:52 +0800

    fix

--- scripts/test-helper/src/index.ts
+++ scripts/test-helper/src/index.ts
@@ -11,12 +11,6 @@ import type {
 } from '@rsbuild/core';
 import upath from 'upath';
 
-export const isPathString = (test: string): boolean =>
-  path.posix.basename(test) !== test || path.win32.basename(test) !== test;
-
-export const isRelativePath = (test: string): boolean =>
-  /^\.\.?($|[\\/])/.test(test);
-
 export const normalizeToPosixPath = (p: string | undefined) =>
   upath
     .normalizeSafe(path.normalize(p || ''))
