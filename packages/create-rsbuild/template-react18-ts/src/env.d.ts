2aab95955c91cb96d750135424ed86b908e07f1c
GRAMMAC <GRAMMAC@163.com>
2025-07-12 16:40:26 +0800

    fix(types): import svg file with react query get type error

--- packages/create-rsbuild/template-react18-ts/src/env.d.ts
+++ packages/create-rsbuild/template-react18-ts/src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="@rsbuild/core/types" />
+
+declare module "*.svg?react" {
+  import React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+  export { ReactComponent };
+}
