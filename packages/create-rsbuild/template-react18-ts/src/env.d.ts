a9483cf6c9ebf614e5376facfb331e525e00eba3
neverland <jait.chen@foxmail.com>
2025-07-12 21:19:22 +0800

    Apply suggestion from @chenjiahan

--- packages/create-rsbuild/template-react18-ts/src/env.d.ts
+++ packages/create-rsbuild/template-react18-ts/src/env.d.ts
@@ -4,5 +4,4 @@ declare module "*.svg?react" {
   import React from "react";
   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
-  export { ReactComponent };
 }
