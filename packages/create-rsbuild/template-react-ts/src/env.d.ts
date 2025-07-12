e0379e94e9454000e34d500e6308297faf36a275
neverland <chenjiahan.jait@bytedance.com>
2025-07-13 07:51:48 +0800

    chore(types): add JSDoc for SVGR

--- packages/create-rsbuild/template-react-ts/src/env.d.ts
+++ packages/create-rsbuild/template-react-ts/src/env.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="@rsbuild/core/types" />
 
-declare module "*.svg?react" {
-  import React from "react";
+/**
+ * Imports the SVG file as a React component.
+ * @requires [@rsbuild/plugin-svgr](https://npmjs.com/package/@rsbuild/plugin-svgr)
+ */
+declare module '*.svg?react' {
+  import type React from 'react';
   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
