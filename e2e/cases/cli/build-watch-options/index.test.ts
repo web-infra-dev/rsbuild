5484f11985b6c2c067586a60d9a99ba15cac1e7c
neverland <chenjiahan.jait@bytedance.com>
2025-07-25 16:58:53 +0800

    fix: watch options not work in build watch mode (#5674)

---
+++ e2e/cases/cli/build-watch-options/index.test.ts
@@ -0,0 +1,42 @@
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { expectFile, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+import fse, { remove } from 'fs-extra';
+
+rspackOnlyTest(
+  'should allow to custom watch options for build watch',
+  async () => {
+    const srcDir = path.join(__dirname, 'src');
+    const tempDir = path.join(__dirname, 'test-temp-src');
+    const distIndexFile = path.join(__dirname, 'dist/static/js/index.js');
+    const fooFile = path.join(tempDir, 'foo.js');
+    const barFile = path.join(tempDir, 'bar.js');
+
+    // clean up
+    await remove(tempDir);
+    await remove(distIndexFile);
+    await fse.copy(srcDir, tempDir);
+
+    const childProcess = exec('npx rsbuild build --watch', {
+      cwd: __dirname,
+    });
+
+    await expectFile(distIndexFile);
+    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('foo1bar1');
+    await remove(distIndexFile);
+
+    // should watch foo.js
+    fse.outputFileSync(fooFile, `export const foo = 'foo2';`);
+    await expectFile(distIndexFile);
+    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('foo2bar1');
+
+    // should not watch bar.js
+    fse.outputFileSync(barFile, `export const bar = 'bar2';`);
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    expect(fs.readFileSync(distIndexFile, 'utf-8')).toContain('foo2bar1');
+
+    childProcess.kill();
+  },
+);
