14b518c94cd6eaa52ee02256e70deeb2bb20b267
neverland <chenjiahan.jait@bytedance.com>
2025-07-25 10:07:35 +0800

    fix

--- e2e/cases/typescript/type-re-exports/index.test.ts
+++ e2e/cases/typescript/type-re-exports/index.test.ts
@@ -1,14 +1,15 @@
-import { build } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
 
-test('should allow to re-export type without the type modifier', async ({
-  page,
-}) => {
-  const rsbuild = await build({
-    cwd: __dirname,
-    page,
-    catchBuildError: true,
-  });
-  expect(rsbuild.buildError).toBeFalsy();
-  await rsbuild.close();
-});
+rspackOnlyTest(
+  'should allow to re-export type without the type modifier',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+      catchBuildError: true,
+    });
+    expect(rsbuild.buildError).toBeFalsy();
+    await rsbuild.close();
+  },
+);
