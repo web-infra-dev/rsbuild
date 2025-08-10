3a4c2de438514a58fe4498d7a4f6c7bdd4a6e333
neverland <chenjiahan.jait@bytedance.com>
2025-08-10 16:24:35 +0800

    fix

--- e2e/cases/wasm/wasm-filename/index.test.ts
+++ e2e/cases/wasm/wasm-filename/index.test.ts
@@ -1,17 +1,20 @@
-import { build } from '@e2e/helper';
-import { expect, test } from '@playwright/test';
+import { build, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
 
-test('should allow to custom the filename of Wasm files', async ({ page }) => {
-  const rsbuild = await build({
-    cwd: __dirname,
-    page,
-  });
-  const files = await rsbuild.getDistFiles();
+rspackOnlyTest(
+  'should allow to custom the filename of Wasm files',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+    });
+    const files = await rsbuild.getDistFiles();
 
-  const wasmFile = Object.keys(files).find((file) =>
-    file.endsWith('factorial.wasm'),
-  );
+    const wasmFile = Object.keys(files).find((file) =>
+      file.endsWith('factorial.wasm'),
+    );
 
-  expect(wasmFile).toBeTruthy();
-  await rsbuild.close();
-});
+    expect(wasmFile).toBeTruthy();
+    await rsbuild.close();
+  },
+);
