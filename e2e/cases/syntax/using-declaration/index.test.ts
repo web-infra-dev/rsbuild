6ad63e6d8f67c7068b848a7243c460ba8a8fec00
neverland <chenjiahan.jait@bytedance.com>
2025-08-18 17:03:24 +0800

    fix

--- e2e/cases/syntax/using-declaration/index.test.ts
+++ e2e/cases/syntax/using-declaration/index.test.ts
@@ -1,8 +1,21 @@
-import { build, rspackOnlyTest } from '@e2e/helper';
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
 
 rspackOnlyTest(
-  'should allow to use the `using` declaration for explicit resource management',
+  'should allow to use the `using` declaration for explicit resource management in development',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+    });
+
+    expect(await page.evaluate('window.disposeCounter')).toEqual(4);
+    await rsbuild.close();
+  },
+);
+
+rspackOnlyTest(
+  'should allow to use the `using` declaration for explicit resource management in production',
   async ({ page }) => {
     const rsbuild = await build({
       cwd: __dirname,
