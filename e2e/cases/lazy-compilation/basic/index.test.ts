2295119cb5c268367a6f034be6380971e23e7576
neverland <chenjiahan.jait@bytedance.com>
2025-08-18 22:14:07 +0800

    fix

--- e2e/cases/lazy-compilation/basic/index.test.ts
+++ e2e/cases/lazy-compilation/basic/index.test.ts
@@ -23,17 +23,17 @@ rspackOnlyTest(
 
     // build page1
     await gotoPage(page, rsbuild, 'page1');
-    await expect(page.locator('#test')).toHaveText('Page 1');
     await rsbuild.expectLog('building src/page1/index.js');
     await rsbuild.expectBuildEnd();
+    await expect(page.locator('#test')).toHaveText('Page 1');
     rsbuild.expectNoLog('building src/page2/index.js');
     rsbuild.clearLogs();
 
     // build page2
     await gotoPage(page, rsbuild, 'page2');
-    await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.expectLog('building src/page2/index.js');
     await rsbuild.expectBuildEnd();
+    await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.close();
   },
 );
@@ -62,17 +62,17 @@ rspackOnlyTest(
 
     // build page1
     await gotoPage(page, rsbuild, 'page1');
-    await expect(page.locator('#test')).toHaveText('Page 1');
     await rsbuild.expectLog('building src/page1/index.js');
     await rsbuild.expectBuildEnd();
+    await expect(page.locator('#test')).toHaveText('Page 1');
     rsbuild.expectNoLog('building src/page2/index.js');
     rsbuild.clearLogs();
 
     // build page2
     await gotoPage(page, rsbuild, 'page2');
-    await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.expectLog('building src/page2/index.js');
     await rsbuild.expectBuildEnd();
+    await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.close();
   },
 );
