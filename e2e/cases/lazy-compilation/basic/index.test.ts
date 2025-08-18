58f06fe0e606822b1a41d0e73896ac166f4becb4
neverland <chenjiahan.jait@bytedance.com>
2025-08-18 22:23:21 +0800

    fix

--- e2e/cases/lazy-compilation/basic/index.test.ts
+++ e2e/cases/lazy-compilation/basic/index.test.ts
@@ -1,6 +1,9 @@
 import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
+const BUILD_PAGE1 = 'building src/page1/index.js';
+const BUILD_PAGE2 = 'building src/page2/index.js';
+
 rspackOnlyTest(
   'should render pages correctly when using lazy compilation',
   async ({ page }) => {
@@ -23,15 +26,15 @@ rspackOnlyTest(
 
     // build page1
     await gotoPage(page, rsbuild, 'page1');
-    await rsbuild.expectLog('building src/page1/index.js');
+    await rsbuild.expectLog(BUILD_PAGE1);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 1');
-    rsbuild.expectNoLog('building src/page2/index.js');
+    rsbuild.expectNoLog(BUILD_PAGE2);
     rsbuild.clearLogs();
 
     // build page2
     await gotoPage(page, rsbuild, 'page2');
-    await rsbuild.expectLog('building src/page2/index.js');
+    await rsbuild.expectLog(BUILD_PAGE2);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.close();
@@ -62,15 +65,15 @@ rspackOnlyTest(
 
     // build page1
     await gotoPage(page, rsbuild, 'page1');
-    await rsbuild.expectLog('building src/page1/index.js');
+    await rsbuild.expectLog(BUILD_PAGE1);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 1');
-    rsbuild.expectNoLog('building src/page2/index.js');
+    rsbuild.expectNoLog(BUILD_PAGE2);
     rsbuild.clearLogs();
 
     // build page2
     await gotoPage(page, rsbuild, 'page2');
-    await rsbuild.expectLog('building src/page2/index.js');
+    await rsbuild.expectLog(BUILD_PAGE2);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.close();
