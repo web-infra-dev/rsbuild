ff9a3fbeb2e40a54c9dc70d57bdec95ba3983237
neverland <chenjiahan.jait@bytedance.com>
2025-08-19 09:23:09 +0800

    test(e2e): improve lazy compilation cases (#5878)

--- e2e/cases/lazy-compilation/basic/index.test.ts
+++ e2e/cases/lazy-compilation/basic/index.test.ts
@@ -1,6 +1,9 @@
 import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
+const BUILD_PAGE1 = 'building src/page1/index.js';
+const BUILD_PAGE2 = 'building src/page2/index.js';
+
 rspackOnlyTest(
   'should render pages correctly when using lazy compilation',
   async ({ page }) => {
@@ -17,15 +20,23 @@ rspackOnlyTest(
       },
     });
 
+    // the first build
+    await rsbuild.expectBuildEnd();
+    rsbuild.clearLogs();
+
+    // build page1
     await gotoPage(page, rsbuild, 'page1');
+    await rsbuild.expectLog(BUILD_PAGE1);
+    await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 1');
-    await rsbuild.expectLog('building src/page1/index.js');
-    rsbuild.expectNoLog('building src/page2/index.js');
+    rsbuild.expectNoLog(BUILD_PAGE2);
+    rsbuild.clearLogs();
 
+    // build page2
     await gotoPage(page, rsbuild, 'page2');
+    await rsbuild.expectLog(BUILD_PAGE2);
+    await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 2');
-    await rsbuild.expectLog('building src/page2/index.js');
-
     await rsbuild.close();
   },
 );
@@ -48,15 +59,23 @@ rspackOnlyTest(
       },
     });
 
+    // the first build
+    await rsbuild.expectBuildEnd();
+    rsbuild.clearLogs();
+
+    // build page1
     await gotoPage(page, rsbuild, 'page1');
+    await rsbuild.expectLog(BUILD_PAGE1);
+    await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 1');
-    await rsbuild.expectLog('building src/page1/index.js');
-    rsbuild.expectNoLog('building src/page2/index.js');
+    rsbuild.expectNoLog(BUILD_PAGE2);
+    rsbuild.clearLogs();
 
+    // build page2
     await gotoPage(page, rsbuild, 'page2');
+    await rsbuild.expectLog(BUILD_PAGE2);
+    await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 2');
-    await rsbuild.expectLog('building src/page2/index.js');
-
     await rsbuild.close();
   },
 );
