5f3cca57b39c335534c9750ca3de3546f482f96b
neverland <chenjiahan.jait@bytedance.com>
2025-08-21 16:32:00 +0800

    feat(core): add `expose` API to Rsbuild instance (#5910)

--- e2e/cases/javascript-api/instance-api/index.test.ts
+++ e2e/cases/javascript-api/instance-api/index.test.ts
@@ -1,6 +1,6 @@
 import { rspackOnlyTest } from '@e2e/helper';
 import { expect } from '@playwright/test';
-import { createRsbuild } from '@rsbuild/core';
+import { createRsbuild, type RsbuildPlugin } from '@rsbuild/core';
 
 rspackOnlyTest(
   'should allow to call `modifyRsbuildConfig` via Rsbuild instance',
@@ -33,5 +33,35 @@ rspackOnlyTest(
     const result = await rsbuild.inspectConfig();
     expect(result.origin.rsbuildConfig.mode).toBe('development');
     expect(result.origin.environmentConfigs.web.mode).toBe('none');
+  },
+);
+
+rspackOnlyTest(
+  'should allow to call `expose` via Rsbuild instance',
+  async () => {
+    const rsbuild = await createRsbuild({
+      cwd: __dirname,
+    });
+
+    rsbuild.expose('test', {
+      sayHello() {
+        return 'hello';
+      },
+    });
+
+    let result = '';
+
+    const plugin: RsbuildPlugin = {
+      name: 'test-plugin',
+      setup(api) {
+        const exposed = api.useExposed('test');
+        result = exposed.sayHello();
+      },
+    };
+
+    rsbuild.addPlugins([plugin]);
+    await rsbuild.initConfigs();
+
+    expect(result).toEqual('hello');
   },
 );
