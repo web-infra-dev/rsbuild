f5d5ee1de0cd223ffa135951f956211eba734ce7
neverland <chenjiahan.jait@bytedance.com>
2025-08-23 20:30:44 +0800

    test(plugin-vue): add e2e test case for custom css modules inject name

---
+++ e2e/cases/plugin-vue/sfc-css-modules-custom/index.test.ts
@@ -0,0 +1,42 @@
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
+import { expect } from '@playwright/test';
+
+rspackOnlyTest(
+  'should allow to custom CSS Modules inject name in dev build',
+  async ({ page }) => {
+    const rsbuild = await dev({
+      cwd: __dirname,
+      page,
+    });
+
+    const test1 = page.locator('#test1');
+    const test2 = page.locator('#test2');
+    const test3 = page.locator('#test3');
+
+    await expect(test1).toHaveCSS('color', 'rgb(255, 0, 0)');
+    await expect(test2).toHaveCSS('color', 'rgb(0, 0, 255)');
+    await expect(test3).toHaveCSS('color', 'rgb(0, 128, 0)');
+
+    await rsbuild.close();
+  },
+);
+
+rspackOnlyTest(
+  'should allow to custom CSS Modules inject name in prod build',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+    });
+
+    const test1 = page.locator('#test1');
+    const test2 = page.locator('#test2');
+    const test3 = page.locator('#test3');
+
+    await expect(test1).toHaveCSS('color', 'rgb(255, 0, 0)');
+    await expect(test2).toHaveCSS('color', 'rgb(0, 0, 255)');
+    await expect(test3).toHaveCSS('color', 'rgb(0, 128, 0)');
+
+    await rsbuild.close();
+  },
+);
