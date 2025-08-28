151c71d7660ebd41d224cacea6e489785b7f3581
Fy <1114550440@qq.com>
2025-08-28 16:52:42 +0800

    test(e2e): do not rely on building URL to verify lazy compilation (#5966)

--- e2e/cases/lazy-compilation/port-placeholder/index.test.ts
+++ e2e/cases/lazy-compilation/port-placeholder/index.test.ts
@@ -1,9 +1,6 @@
 import { dev, gotoPage, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-const BUILD_PAGE1 = 'building src/page1/index.js';
-const BUILD_PAGE2 = 'building src/page2/index.js';
-
 rspackOnlyTest(
   'should replace port placeholder with actual port',
   async ({ page }) => {
@@ -21,15 +18,12 @@ rspackOnlyTest(
 
     // build page1
     await gotoPage(page, rsbuild, 'page1');
-    await rsbuild.expectLog(BUILD_PAGE1);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 1');
-    rsbuild.expectNoLog(BUILD_PAGE2);
     rsbuild.clearLogs();
 
     // build page2
     await gotoPage(page, rsbuild, 'page2');
-    await rsbuild.expectLog(BUILD_PAGE2);
     await rsbuild.expectBuildEnd();
     await expect(page.locator('#test')).toHaveText('Page 2');
     await rsbuild.close();
