ea4dc0b6ea3b64ca81886ddff877a761701bb20a
neverland <chenjiahan.jait@bytedance.com>
2025-08-29 13:02:46 +0800

    test(e2e): add new cases to verify enum with property access

--- e2e/cases/typescript/const-enum/index.test.ts
+++ e2e/cases/typescript/const-enum/index.test.ts
@@ -6,6 +6,10 @@ test('should compile const enum correctly', async ({ page }) => {
     cwd: __dirname,
     page,
   });
-  expect(await page.evaluate(() => window.test)).toBe('Fish 0, Cat 1');
+  expect(await page.evaluate(() => window.test1)).toBe('Fish fish, Cat cat');
   expect(await page.evaluate(() => window.test2)).toBe('Fish 0, Cat 1');
+
+  // https://github.com/web-infra-dev/rsbuild/issues/5959
+  expect(await page.evaluate(() => window.test3)).toBe('Fish FISH, Cat CAT');
+  expect(await page.evaluate(() => window.test4)).toBe('Fish 0, Cat 1');
 });
