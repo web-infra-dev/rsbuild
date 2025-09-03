7db6f974682e1394d28131cc51a67981a0a17634
neverland <chenjiahan.jait@bytedance.com>
2025-09-03 11:59:47 +0800

    fix

--- e2e/cases/optimization/inline-const/index.test.ts
+++ e2e/cases/optimization/inline-const/index.test.ts
@@ -1,20 +1,23 @@
-import { build, dev } from '@e2e/helper';
+import { build, dev, rspackOnlyTest } from '@e2e/helper';
 import { expect, test } from '@playwright/test';
 
-test('should inline the constants in production mode', async ({ page }) => {
-  const rsbuild = await build({
-    cwd: __dirname,
-    page,
-  });
-  expect(await page.evaluate(() => window.test1)).toBe('Fish fish, Cat cat');
-  expect(await page.evaluate(() => window.test2)).toBe('Fish FISH, Cat CAT');
+rspackOnlyTest(
+  'should inline the constants in production mode',
+  async ({ page }) => {
+    const rsbuild = await build({
+      cwd: __dirname,
+      page,
+    });
+    expect(await page.evaluate(() => window.test1)).toBe('Fish fish, Cat cat');
+    expect(await page.evaluate(() => window.test2)).toBe('Fish FISH, Cat CAT');
 
-  const indexJs = await rsbuild.getIndexBundle();
-  expect(indexJs).toContain('window.test1="Fish fish, Cat cat"');
-  expect(indexJs).toContain('window.test2="Fish FISH, Cat CAT"');
+    const indexJs = await rsbuild.getIndexBundle();
+    expect(indexJs).toContain('window.test1="Fish fish, Cat cat"');
+    expect(indexJs).toContain('window.test2="Fish FISH, Cat CAT"');
 
-  await rsbuild.close();
-});
+    await rsbuild.close();
+  },
+);
 
 test('should import the constants as expected in development mode', async ({
   page,
