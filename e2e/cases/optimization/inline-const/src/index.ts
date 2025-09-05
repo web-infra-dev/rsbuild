604ef6d15b44131e149e3414f3a4b0d3c67f2c87
neverland <chenjiahan.jait@bytedance.com>
2025-09-05 13:47:16 +0800

    test(e2e): improve inline const test case

--- e2e/cases/optimization/inline-const/src/index.ts
+++ e2e/cases/optimization/inline-const/src/index.ts
@@ -4,8 +4,13 @@ declare global {
   interface Window {
     test1: string;
     test2: string;
+    test3: string;
   }
 }
 
-window.test1 = `Fish ${FISH}, Cat ${CAT}`;
-window.test2 = `Fish ${FISH.toUpperCase()}, Cat ${CAT.toUpperCase()}`;
+window.test1 = `${FISH},${CAT}`;
+window.test2 = `${FISH.toUpperCase()},${CAT.toUpperCase()}`;
+
+import('./constants2').then(({ DOG }) => {
+  window.test3 = `${DOG},${DOG.toUpperCase()}`;
+});
