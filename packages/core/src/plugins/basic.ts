61c16ef33813c56bc13005a46999cd0bf7aaf6c1
neverland <chenjiahan.jait@bytedance.com>
2025-09-05 14:09:55 +0800

    fix(core): only enable inline enum in production mode (#6064)

--- packages/core/src/plugins/basic.ts
+++ packages/core/src/plugins/basic.ts
@@ -97,7 +97,7 @@ export const pluginBasic = (): RsbuildPlugin => ({
           chain.experiments({
             ...chain.get('experiments'),
             lazyBarrel: true,
-            inlineEnum: true,
+            inlineEnum: isProd,
             inlineConst: isProd,
             typeReexportsPresence: true,
             rspackFuture: {
