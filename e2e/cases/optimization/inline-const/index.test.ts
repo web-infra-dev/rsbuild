c15a2b7d4445b5bfe3e6962494688c22f6d0002b
neverland <chenjiahan.jait@bytedance.com>
2025-09-05 13:55:05 +0800

    test(e2e): improve inline const test case (#6063)

--- e2e/cases/optimization/inline-const/index.test.ts
+++ e2e/cases/optimization/inline-const/index.test.ts
@@ -8,12 +8,14 @@ rspackOnlyTest(
       cwd: __dirname,
       page,
     });
-    expect(await page.evaluate(() => window.test1)).toBe('Fish fish, Cat cat');
-    expect(await page.evaluate(() => window.test2)).toBe('Fish FISH, Cat CAT');
+    await page.waitForFunction(() => window.test3);
+    expect(await page.evaluate(() => window.test1)).toBe('fish,FISH');
+    expect(await page.evaluate(() => window.test2)).toBe('cat,CAT');
+    expect(await page.evaluate(() => window.test3)).toBe('dog,DOG');
 
     const indexJs = await rsbuild.getIndexBundle();
-    expect(indexJs).toContain('window.test1="Fish fish, Cat cat"');
-    expect(indexJs).toContain('window.test2="Fish FISH, Cat CAT"');
+    expect(indexJs).toContain('window.test1="fish,FISH"');
+    expect(indexJs).toContain('window.test2="cat,CAT"');
 
     await rsbuild.close();
   },
@@ -26,7 +28,9 @@ test('should import the constants as expected in development mode', async ({
     cwd: __dirname,
     page,
   });
-  expect(await page.evaluate(() => window.test1)).toBe('Fish fish, Cat cat');
-  expect(await page.evaluate(() => window.test2)).toBe('Fish FISH, Cat CAT');
+  await page.waitForFunction(() => window.test3);
+  expect(await page.evaluate(() => window.test1)).toBe('fish,FISH');
+  expect(await page.evaluate(() => window.test2)).toBe('cat,CAT');
+  expect(await page.evaluate(() => window.test3)).toBe('dog,DOG');
   await rsbuild.close();
 });
--- e2e/cases/optimization/inline-const/src/index.ts
+++ e2e/cases/optimization/inline-const/src/index.ts
@@ -4,8 +4,13 @@ declare global {
   interface Window {
     test1: string;
     test2: string;
+    test3: string;
   }
 }
 
-window.test1 = `Fish ${FISH}, Cat ${CAT}`;
-window.test2 = `Fish ${FISH.toUpperCase()}, Cat ${CAT.toUpperCase()}`;
+window.test1 = `${FISH},${FISH.toUpperCase()}`;
+window.test2 = `${CAT},${CAT.toUpperCase()}`;
+
+import('./constants2').then(({ DOG }) => {
+  window.test3 = `${DOG},${DOG.toUpperCase()}`;
+});
