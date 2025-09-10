f6f0df50a8729d82d1c8e42ac93c6a124a71fb44
neverland <chenjiahan.jait@bytedance.com>
2025-09-10 16:46:30 +0800

    test(e2e): improve the directory structure of e2e cases

---
+++ e2e/cases/resolve/exports-presence/index.test.ts
@@ -0,0 +1,29 @@
+import { build, dev } from '@e2e/helper';
+import { expect, test } from '@playwright/test';
+
+test('should detect and report missing named export errors during build', async () => {
+  const rsbuild = await build({
+    cwd: __dirname,
+    catchBuildError: true,
+  });
+
+  expect(rsbuild.buildError).toBeTruthy();
+  await rsbuild.expectLog(
+    `export 'aa' (imported as 'aa') was not found in './test'`,
+  );
+  await rsbuild.close();
+});
+
+test('should detect and report missing named export errors in development mode', async ({
+  page,
+}) => {
+  const rsbuild = await dev({
+    cwd: __dirname,
+    page,
+  });
+
+  await rsbuild.expectLog(
+    `export 'aa' (imported as 'aa') was not found in './test'`,
+  );
+  await rsbuild.close();
+});
