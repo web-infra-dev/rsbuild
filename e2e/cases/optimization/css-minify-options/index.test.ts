013774ed97514bef5ce5f1566cfba30e9c99a0a6
neverland <chenjiahan.jait@bytedance.com>
2025-09-13 11:29:56 +0800

    fix

--- e2e/cases/optimization/css-minify-options/index.test.ts
+++ e2e/cases/optimization/css-minify-options/index.test.ts
@@ -3,11 +3,8 @@ import { expect, rspackOnlyTest } from '@e2e/helper';
 rspackOnlyTest(
   'should allow to custom CSS minify options',
   async ({ buildOnly }) => {
-    const rsbuild = await buildOnly({
-      rsbuildConfig: {},
-    });
+    const rsbuild = await buildOnly();
     const files = rsbuild.getDistFiles();
-
     const content =
       files[Object.keys(files).find((file) => file.endsWith('.css'))!];
 
