52f1a4ce4314eb6e88ef75c320a6ccd9478d147c
neverland <chenjiahan.jait@bytedance.com>
2025-09-17 11:45:23 +0800

    test(e2e): add test case for proxy mixed rules

---
+++ e2e/cases/server/proxy-error/index.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@e2e/helper';
+
+test('should handle proxy error', async ({ dev, page }) => {
+  const rsbuild = await dev({
+    rsbuildConfig: {
+      server: {
+        cors: true,
+        proxy: [
+          {
+            context: ['/api'],
+            target: 'http://somepagewhichdoesnotexits.com:9000',
+            changeOrigin: true,
+            secure: false,
+          },
+        ],
+      },
+    },
+  });
+
+  const res = await page.goto(`http://localhost:${rsbuild.port}/api`);
+  expect(res?.status()).toBe(504);
+  await rsbuild.expectLog('Error occurred while proxying request');
+});
--- e2e/cases/server/proxy-mixed-rules/index.test.ts
+++ e2e/cases/server/proxy-mixed-rules/index.test.ts
@@ -4,7 +4,7 @@ import { expect, test } from '@e2e/helper';
 const cwd1 = join(__dirname, 'project1');
 const cwd2 = join(__dirname, 'project2');
 
-test('should apply basic proxy rules', async ({ dev, page }) => {
+test('should apply mixed proxy rules', async ({ dev, page }) => {
   const rsbuild1 = await dev({
     cwd: cwd1,
     rsbuildConfig: {
@@ -19,13 +19,17 @@ test('should apply basic proxy rules', async ({ dev, page }) => {
     rsbuildConfig: {
       server: {
         proxy: {
-          // https://stackoverflow.com/a/76727711/6219457
-          '/': `http://127.0.0.1:${rsbuild1.port}/`,
+          '/foo': `http://127.0.0.1:${rsbuild1.port}/`,
+          '/bar': {
+            target: `http://127.0.0.1:${rsbuild1.port}/`,
+          },
         },
       },
     },
   });
 
-  await page.goto(`http://localhost:${rsbuild2.port}/`);
+  await page.goto(`http://localhost:${rsbuild2.port}/foo`);
+  expect(await page.innerHTML('body')).toContain('<div id="root">1</div>');
+  await page.goto(`http://localhost:${rsbuild2.port}/bar`);
   expect(await page.innerHTML('body')).toContain('<div id="root">1</div>');
 });
