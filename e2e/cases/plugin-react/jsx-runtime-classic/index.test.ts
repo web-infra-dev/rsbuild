f4483be6a696fa84348b8ee84034b21a0d17d9f7
neverland <chenjiahan.jait@bytedance.com>
2025-09-24 21:36:37 +0800

    fix

--- e2e/cases/plugin-react/jsx-runtime-classic/index.test.ts
+++ e2e/cases/plugin-react/jsx-runtime-classic/index.test.ts
@@ -1,19 +1,19 @@
-import { expect, test } from '@e2e/helper';
+import { expect, rspackTest } from '@e2e/helper';
 
-test('should render element with classic JSX runtime in build', async ({
-  page,
-  buildPreview,
-}) => {
-  await buildPreview();
-  const testEl = page.locator('#test');
-  await expect(testEl).toHaveText('Hello Rsbuild!');
-});
+rspackTest(
+  'should render element with classic JSX runtime in build',
+  async ({ page, buildPreview }) => {
+    await buildPreview();
+    const testEl = page.locator('#test');
+    await expect(testEl).toHaveText('Hello Rsbuild!');
+  },
+);
 
-test('should render element with classic JSX runtime in dev', async ({
-  page,
-  dev,
-}) => {
-  await dev();
-  const testEl = page.locator('#test');
-  await expect(testEl).toHaveText('Hello Rsbuild!');
-});
+rspackTest(
+  'should render element with classic JSX runtime in dev',
+  async ({ page, dev }) => {
+    await dev();
+    const testEl = page.locator('#test');
+    await expect(testEl).toHaveText('Hello Rsbuild!');
+  },
+);
