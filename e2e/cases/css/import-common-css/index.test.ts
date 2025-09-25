b0c3c62faa6b5f76dee7ffc9e9a63e808e0b1681
neverland <chenjiahan.jait@bytedance.com>
2025-09-25 12:38:11 +0800

    fix

--- e2e/cases/css/import-common-css/index.test.ts
+++ e2e/cases/css/import-common-css/index.test.ts
@@ -2,11 +2,7 @@ import { expect, getFileContent, rspackTest } from '@e2e/helper';
 
 rspackTest('should compile common CSS import correctly', async ({ build }) => {
   const rsbuild = await build();
-
   const files = rsbuild.getDistFiles();
-  const cssContent = getFileContent(files, 'index.css');
-
-  expect(cssContent).toEqual(
-    'html{min-height:100%}#a{color:red}#b{color:#00f}',
-  );
+  const indexCss = getFileContent(files, 'index.css');
+  expect(indexCss).toEqual('html{min-height:100%}#a{color:red}#b{color:#00f}');
 });
