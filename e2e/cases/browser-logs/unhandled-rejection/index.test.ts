2fd76071c27cd02c6f26d807bcfc307dcf95cd23
neverland <chenjiahan.jait@bytedance.com>
2025-09-27 10:38:07 +0800

    fix: skip browser error reporting for webpack (#6264)

--- e2e/cases/browser-logs/unhandled-rejection/index.test.ts
+++ e2e/cases/browser-logs/unhandled-rejection/index.test.ts
@@ -1,30 +1,33 @@
-import { test } from '@e2e/helper';
+import { rspackTest } from '@e2e/helper';
 
-test('should forward browser unhandled rejection logs to terminal', async ({
-  dev,
-}) => {
-  const rsbuild = await dev();
-  await rsbuild.expectLog('error   [browser] Uncaught (in promise) 404');
-  await rsbuild.expectLog('error   [browser] Uncaught (in promise) false');
-  await rsbuild.expectLog('error   [browser] Uncaught (in promise) null');
-  await rsbuild.expectLog('error   [browser] Uncaught (in promise) undefined');
-  await rsbuild.expectLog('error   [browser] Uncaught (in promise) string');
-  await rsbuild.expectLog(
-    'error   [browser] Uncaught (in promise) {"name":"Custom","message":"custom message"}',
-  );
-  await rsbuild.expectLog(
-    'error   [browser] Uncaught (in promise) Error: reason (src/index.js:7:0)',
-    { posix: true },
-  );
-  await rsbuild.expectLog(
-    'error   [browser] Uncaught (in promise) AbortError: Aborted',
-  );
-  await rsbuild.expectLog(
-    'error   [browser] Uncaught (in promise) Error: Thrown in async (src/index.js:11:0)',
-    { posix: true },
-  );
-  await rsbuild.expectLog(
-    'error   [browser] Uncaught (in promise) AbortError: signal is aborted without reason (src/index.js:16:0)',
-    { posix: true },
-  );
-});
+rspackTest(
+  'should forward browser unhandled rejection logs to terminal',
+  async ({ dev }) => {
+    const rsbuild = await dev();
+    await rsbuild.expectLog('error   [browser] Uncaught (in promise) 404');
+    await rsbuild.expectLog('error   [browser] Uncaught (in promise) false');
+    await rsbuild.expectLog('error   [browser] Uncaught (in promise) null');
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) undefined',
+    );
+    await rsbuild.expectLog('error   [browser] Uncaught (in promise) string');
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) {"name":"Custom","message":"custom message"}',
+    );
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) Error: reason (src/index.js:7:0)',
+      { posix: true },
+    );
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) AbortError: Aborted',
+    );
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) Error: Thrown in async (src/index.js:11:0)',
+      { posix: true },
+    );
+    await rsbuild.expectLog(
+      'error   [browser] Uncaught (in promise) AbortError: signal is aborted without reason (src/index.js:16:0)',
+      { posix: true },
+    );
+  },
+);
