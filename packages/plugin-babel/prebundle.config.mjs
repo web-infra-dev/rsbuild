34b389328267d4db8fe1663a8595d65d75c5e45b
neverland <chenjiahan.jait@bytedance.com>
2025-09-28 13:02:49 +0800

    perf: optimize module resolution of compiled packages

--- packages/plugin-babel/prebundle.config.mjs
+++ packages/plugin-babel/prebundle.config.mjs
@@ -1,14 +1,4 @@
 // @ts-check
-import { writeFileSync } from 'node:fs';
-import { join } from 'node:path';
-
-// The package size of `schema-utils` is large, and validate has a performance overhead of tens of ms.
-// So we skip the validation and let TypeScript to ensure type safety.
-const writeEmptySchemaUtils = (task) => {
-  const schemaUtilsPath = join(task.distPath, 'schema-utils.js');
-  writeFileSync(schemaUtilsPath, 'module.exports.validate = () => {};');
-};
-
 /** @type {import('prebundle').Config} */
 export default {
   prettier: true,
@@ -18,9 +8,7 @@ export default {
       ignoreDts: true,
       externals: {
         '@babel/core': '@babel/core',
-        'schema-utils': './schema-utils',
       },
-      afterBundle: writeEmptySchemaUtils,
     },
   ],
 };
