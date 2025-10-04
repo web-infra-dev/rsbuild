9dd9b76477cb5709b25cfca019a57096b203c7bb
neverland <chenjiahan.jait@bytedance.com>
2025-10-04 13:59:34 +0800

    fix

--- e2e/cases/diagnostic/disable-stats-errors/index.test.ts
+++ e2e/cases/diagnostic/disable-stats-errors/index.test.ts
@@ -1,19 +1,21 @@
-import { test } from '@e2e/helper';
+import { rspackTest } from '@e2e/helper';
 
 const EXPECTED_LOG = `Build failed. No errors reported since Rspack's "stats.errors" is disabled.`;
 
-test('should print a hint if stats.errors is disabled after a dev failure', async ({
-  dev,
-}) => {
-  const rsbuild = await dev();
-  await rsbuild.expectLog(EXPECTED_LOG);
-});
+rspackTest(
+  'should print a hint if stats.errors is disabled after a dev failure',
+  async ({ dev }) => {
+    const rsbuild = await dev();
+    await rsbuild.expectLog(EXPECTED_LOG);
+  },
+);
 
-test('should print a hint if stats.errors is disabled after a build failure', async ({
-  build,
-}) => {
-  const rsbuild = await build({
-    catchBuildError: true,
-  });
-  await rsbuild.expectLog(EXPECTED_LOG);
-});
+rspackTest(
+  'should print a hint if stats.errors is disabled after a build failure',
+  async ({ build }) => {
+    const rsbuild = await build({
+      catchBuildError: true,
+    });
+    await rsbuild.expectLog(EXPECTED_LOG);
+  },
+);
