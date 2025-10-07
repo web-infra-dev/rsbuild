db1392237c0c1cb5bdae5d36de556f5b22901e1b
neverland <chenjiahan.jait@bytedance.com>
2025-10-07 23:50:05 +0900

    fix

--- e2e/cases/server/serve-assets/index.test.ts
+++ e2e/cases/server/serve-assets/index.test.ts
@@ -3,10 +3,10 @@ import { expect, test } from '@e2e/helper';
 test('should serve static files', async ({ request, dev }) => {
   const rsbuild = await dev();
   const baseUrl = `http://127.0.0.1:${rsbuild.port}`;
-  const reqText = await request.get(`${baseUrl}/foo.txt`);
-  expect(await reqText.text()).toContain('bar');
-  const reqIndexJs = await request.get(`${baseUrl}/static/js/index.js`);
-  expect(await reqIndexJs.text()).toContain('Hello Rsbuild!');
+  const resText = await request.get(`${baseUrl}/foo.txt`);
+  expect(await resText.text()).toContain('bar');
+  const resIndexJs = await request.get(`${baseUrl}/static/js/index.js`);
+  expect(await resIndexJs.text()).toContain('Hello Rsbuild!');
 });
 
 test('should return 403 for path traversal', async ({ request, dev }) => {
