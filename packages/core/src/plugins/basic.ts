325a29fde3a5801f0cfdc11ec4f651cd6bc6f9cb
neverland <chenjiahan.jait@bytedance.com>
2025-10-22 15:50:44 +0800

    chore: remove Rspack config schema env variable

--- packages/core/src/plugins/basic.ts
+++ packages/core/src/plugins/basic.ts
@@ -111,12 +111,6 @@ export const pluginBasic = (): RsbuildPlugin => ({
             },
           });
         }
-
-        // Disable Rspack's config schema validation to improve performance.
-        // Rsbuild has ensured that the built-in Rspack configuration is correct
-        // through TypeScript, so we no longer need to perform schema validation
-        // at runtime. This can be manually enabled via `RSPACK_CONFIG_VALIDATE=strict'
-        process.env.RSPACK_CONFIG_VALIDATE ||= 'loose-silent';
       },
     );
   },
