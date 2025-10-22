c4c76ec42909e57937dbc460c8767fa7cdacf24d
neverland <chenjiahan.jait@bytedance.com>
2025-10-22 15:33:29 +0800

    fix

--- e2e/cases/diagnostic/process-undefined/index.test.ts
+++ e2e/cases/diagnostic/process-undefined/index.test.ts
@@ -1,10 +1,11 @@
-import { test } from '@e2e/helper';
+import { rspackTest } from '@e2e/helper';
 
-test('should print help message if undefined process.env.* is accessed in dev', async ({
-  dev,
-}) => {
-  const rsbuild = await dev();
-  await rsbuild.expectLog(
-    'To access `process.env.*`, define them in a `.env` file with the `PUBLIC_` prefix.',
-  );
-});
+rspackTest(
+  'should print help message if undefined process.env.* is accessed in dev',
+  async ({ dev }) => {
+    const rsbuild = await dev();
+    await rsbuild.expectLog(
+      'To access `process.env.*`, define them in a `.env` file with the `PUBLIC_` prefix.',
+    );
+  },
+);
