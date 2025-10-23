31a6d14465c8a8e5a5a28f45fe6c7a04741034d6
neverland <chenjiahan.jait@bytedance.com>
2025-10-23 15:32:26 +0800

    docs: deprecate bundleAnalyze in favor of Rsdoctor

--- packages/core/src/types/config.ts
+++ packages/core/src/types/config.ts
@@ -733,6 +733,8 @@ export interface PerformanceConfig {
 
   /**
    * Analyze the size of output files.
+   * @default undefined
+   * @deprecated Use Rsdoctor instead.
    */
   bundleAnalyze?: BundleAnalyzerPlugin.Options;
 
