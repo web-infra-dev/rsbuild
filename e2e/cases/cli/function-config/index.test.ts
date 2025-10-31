2847816170418401d752f4f0f37d04a183f69643
neverland <chenjiahan.jait@bytedance.com>
2025-11-01 07:35:25 +0800

    test(e2e): add tests to verify env mode command handling (#6477)

--- e2e/cases/cli/function-config/index.test.ts
+++ e2e/cases/cli/function-config/index.test.ts
@@ -1,19 +1,42 @@
 import path from 'node:path';
-import { expect, readDirContents, rspackTest } from '@e2e/helper';
+import {
+  expect,
+  getFileContent,
+  readDirContents,
+  rspackTest,
+} from '@e2e/helper';
 import { remove } from 'fs-extra';
+
+const distDir = path.join(__dirname, 'dist');
 
 rspackTest(
   'should support exporting a function from the config file',
   async ({ execCliSync }) => {
-    const targetDir = path.join(__dirname, 'dist-production-build');
-
-    await remove(targetDir);
-
+    await remove(distDir);
     execCliSync('build');
-
-    const outputs = await readDirContents(targetDir);
-    const outputFiles = Object.keys(outputs);
-
-    expect(outputFiles.length > 1).toBeTruthy();
+    const files = await readDirContents(distDir);
+    const content = getFileContent(files, 'index.js');
+    expect(content.includes('production-production-build')).toBeTruthy();
   },
 );
+
+rspackTest('should specify env as expected', async ({ execCliSync }) => {
+  await remove(distDir);
+  execCliSync('build', {
+    env: {
+      ...process.env,
+      NODE_ENV: 'development',
+    },
+  });
+  const files = await readDirContents(distDir);
+  const content = getFileContent(files, 'index.js');
+  expect(content.includes('development-development-build')).toBeTruthy();
+});
+
+rspackTest('should specify env mode as expected', async ({ execCliSync }) => {
+  await remove(distDir);
+  execCliSync('build --env-mode staging');
+  const files = await readDirContents(distDir);
+  const content = getFileContent(files, 'index.js');
+  expect(content.includes('production-staging-build')).toBeTruthy();
+});
