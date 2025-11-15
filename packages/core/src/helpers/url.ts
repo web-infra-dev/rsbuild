4d9533fd31488c9c6a2b6a44aad96853d08b80ea
neverland <chenjiahan.jait@bytedance.com>
2025-11-15 11:17:55 +0800

    fix: treat protocol-relative paths as URLs (#6582)

--- packages/core/src/helpers/url.ts
+++ packages/core/src/helpers/url.ts
@@ -11,7 +11,7 @@ export const addTrailingSlash = (s: string): string =>
 
 // Determine if the string is a URL
 export const isURL = (str: string): boolean =>
-  str.startsWith('http') || str.startsWith('//:');
+  str.startsWith('http') || str.startsWith('//');
 
 export const urlJoin = (base: string, path: string) => {
   const [urlProtocol, baseUrl] = base.split('://');
