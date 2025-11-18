e05396bfbeb7ee0becdd13cda1a5c46e561ee583
SoonIter <sooniter@gmail.com>
2025-11-18 12:35:29 +0800

    chore: update

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -1,4 +1,3 @@
-import { HomeBackground as BasicHomeBackground } from '@rspress/core/theme';
 import { BackgroundImage } from '@rstack-dev/doc-ui/background-image';
 import { Benchmark } from '../components/Benchmark';
 import { CopyRight } from '../components/Copyright';
@@ -7,20 +6,10 @@ import { Hero } from '../components/Hero';
 import { HomeFooter } from '../components/HomeFooter';
 import { ToolStack } from '../components/ToolStack';
 
-function HomeBackground() {
-  return (
-    <>
-      {/* For transparent nav at top */}
-      <BasicHomeBackground style={{ background: 'none' }} />
-      <BackgroundImage />
-    </>
-  );
-}
-
 export function HomeLayout() {
   return (
     <div style={{ position: 'relative' }}>
-      <HomeBackground />
+      <BackgroundImage />
       <Hero />
       <Features />
       <Benchmark />
