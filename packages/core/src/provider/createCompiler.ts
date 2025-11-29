0c91194ac63e7e23055aa9616695626a37de0ee3
neverland <chenjiahan.jait@bytedance.com>
2025-11-29 20:01:08 +0800

    fix: add workaround for virtual modules build log

--- packages/core/src/provider/createCompiler.ts
+++ packages/core/src/provider/createCompiler.ts
@@ -69,6 +69,12 @@ function printBuildLog(
     : null;
 
   if (removedFiles?.length) {
+    // workaround for https://github.com/web-infra-dev/rspack/issues/11694
+    if (removedFiles.every((item) => item.includes('virtual'))) {
+      logger.start(`building ${color.dim('virtual modules')}`);
+      return;
+    }
+
     const fileInfo = formatFileList(removedFiles, context.rootPath);
     logger.start(`building ${color.dim(`removed ${fileInfo}`)}`);
     return;
