fa458af6a4f369f60cfb9cdaa9e44cb6b3790dcd
neverland <chenjiahan.jait@bytedance.com>
2025-12-04 21:58:21 +0800

    fix

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,9 +3,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  performance: {
-    printFileSize: {
-      diff: true,
-    },
-  },
 });
