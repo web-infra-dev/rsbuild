02bc42ee9cbc87b9cff9a2c4ea7782dc9492db37
neverland <chenjiahan.jait@bytedance.com>
2025-12-10 11:17:07 +0800

    test: remove skipped test case

--- packages/compat/webpack/tests/initConfigs.test.ts
+++ packages/compat/webpack/tests/initConfigs.test.ts
@@ -1,35 +1,7 @@
-import type { RsbuildConfig, RsbuildPluginAPI } from '@rsbuild/core';
+import type { RsbuildPluginAPI } from '@rsbuild/core';
 import { createStubRsbuild } from './helper';
 
 describe('modifyRsbuildConfig', () => {
-  it.skip('should not allow to modify Rsbuild config', async () => {
-    const rsbuild = await createStubRsbuild({});
-    let config: RsbuildConfig;
-
-    rsbuild.addPlugins([
-      {
-        name: 'foo',
-        setup(api: RsbuildPluginAPI) {
-          api.modifyRsbuildConfig((_config) => {
-            config = _config;
-            config.server = { port: 8080 };
-          });
-
-          api.modifyWebpackChain(() => {
-            config.server ||= {};
-            config.server.port = 8899;
-          });
-        },
-      },
-    ]);
-
-    const {
-      origin: { rsbuildConfig },
-    } = await rsbuild.inspectConfig();
-
-    expect(rsbuildConfig.server?.port).toBe(8080);
-  });
-
   it('should modify config by utils', async () => {
     const rsbuild = await createStubRsbuild({
       config: {
