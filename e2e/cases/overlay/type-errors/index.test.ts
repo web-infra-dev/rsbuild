21e3b7ae034004d07498f9450ceb6d5d9f0610bc
neverland <chenjiahan.jait@bytedance.com>
2025-12-21 18:05:26 +0800

    test(e2e): add runtime error overlay test

--- e2e/cases/overlay/type-errors/index.test.ts
+++ e2e/cases/overlay/type-errors/index.test.ts
@@ -8,12 +8,10 @@ const expectErrorOverlay = async (page: Page) => {
 
   // Get "<span style="color:#888">TS2322: </span>"
   const tsSpan = errorOverlay.locator('span').getByText('TS2322: ');
-  expect(await tsSpan.textContent()).toEqual('TS2322: ');
   expect(await tsSpan.getAttribute('style')).toEqual('color:#888;');
 
   // The first link is "<a class="file-link">./path/to/src/index.ts:3:1</a>"
   const firstLink = errorOverlay.locator('.file-link').first();
-  expect(await firstLink.getAttribute('class')).toEqual('file-link');
   const linkText = await firstLink.textContent();
   expect(
     linkText?.endsWith('/src/index.ts:3:1') && linkText.startsWith('.'),
