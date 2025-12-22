59449ed7f3c89d83acaac00144ba3ffe2b04c9b7
neverland <chenjiahan.jait@bytedance.com>
2025-12-22 22:09:00 +0800

    fix

--- e2e/cases/overlay/error-recovery/index.test.ts
+++ e2e/cases/overlay/error-recovery/index.test.ts
@@ -1,34 +1,36 @@
 import { join } from 'node:path';
-import { expect, MODULE_BUILD_FAILED_LOG, OVERLAY_ID, test } from '@e2e/helper';
+import {
+  expect,
+  MODULE_BUILD_FAILED_LOG,
+  OVERLAY_ID,
+  rspackTest,
+} from '@e2e/helper';
 
-test('should hide overlay after resolving error', async ({
-  page,
-  dev,
-  editFile,
-  logHelper,
-  copySrcDir,
-}) => {
-  const tempSrc = await copySrcDir();
-  await dev({
-    config: {
-      source: {
-        entry: {
-          index: join(tempSrc, 'index.jsx'),
+rspackTest(
+  'should hide overlay after resolving error',
+  async ({ page, dev, editFile, logHelper, copySrcDir }) => {
+    const tempSrc = await copySrcDir();
+    await dev({
+      config: {
+        source: {
+          entry: {
+            index: join(tempSrc, 'index.jsx'),
+          },
         },
       },
-    },
-  });
+    });
 
-  await editFile(join(tempSrc, 'App.jsx'), (code) =>
-    code.replace('</div>', '</a>'),
-  );
-  await logHelper.expectLog(MODULE_BUILD_FAILED_LOG);
-  await expect(page.locator(OVERLAY_ID)).toBeAttached();
+    await editFile(join(tempSrc, 'App.jsx'), (code) =>
+      code.replace('</div>', '</a>'),
+    );
+    await logHelper.expectLog(MODULE_BUILD_FAILED_LOG);
+    await expect(page.locator(OVERLAY_ID)).toBeAttached();
 
-  logHelper.clearLogs();
-  await editFile(join(tempSrc, 'App.jsx'), (code) =>
-    code.replace('</a>', '</div>'),
-  );
-  await logHelper.expectBuildEnd();
-  await expect(page.locator(OVERLAY_ID)).not.toBeAttached();
-});
+    logHelper.clearLogs();
+    await editFile(join(tempSrc, 'App.jsx'), (code) =>
+      code.replace('</a>', '</div>'),
+    );
+    await logHelper.expectBuildEnd();
+    await expect(page.locator(OVERLAY_ID)).not.toBeAttached();
+  },
+);
