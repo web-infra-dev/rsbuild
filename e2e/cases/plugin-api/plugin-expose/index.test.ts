e6b09303069d21b082c0fa85def8ac5224ebf0c3
neverland <chenjiahan.jait@bytedance.com>
2025-12-29 17:49:21 +0800

    fix(plugin-api): allow to override exposed API

--- e2e/cases/plugin-api/plugin-expose/index.test.ts
+++ e2e/cases/plugin-api/plugin-expose/index.test.ts
@@ -6,19 +6,29 @@ type ParentAPI = {
   double: (val: number) => number;
 };
 
+const parentSymbol = Symbol('parent-api');
+
+const pluginParent: RsbuildPlugin = {
+  name: 'plugin-parent',
+  setup(api) {
+    api.expose<ParentAPI>(parentSymbol, {
+      initial: 1,
+      double: (val: number) => val * 2,
+    });
+  },
+};
+
+const pluginParent2: RsbuildPlugin = {
+  name: 'plugin-parent2',
+  setup(api) {
+    api.expose<ParentAPI>(parentSymbol, {
+      initial: 2,
+      double: (val: number) => val * 2,
+    });
+  },
+};
+
 rspackTest('should allow plugin to expose and consume API', async () => {
-  const parentSymbol = Symbol('parent-api');
-
-  const pluginParent: RsbuildPlugin = {
-    name: 'plugin-parent',
-    setup(api) {
-      api.expose<ParentAPI>(parentSymbol, {
-        initial: 1,
-        double: (val: number) => val * 2,
-      });
-    },
-  };
-
   const pluginChild: RsbuildPlugin = {
     name: 'plugin-child',
     setup(api) {
@@ -33,6 +43,23 @@ rspackTest('should allow plugin to expose and consume API', async () => {
       plugins: [pluginParent, pluginChild],
     },
   });
+  await rsbuild.build();
+});
 
+rspackTest('should override the previous exposed API', async () => {
+  const pluginChild: RsbuildPlugin = {
+    name: 'plugin-child',
+    setup(api) {
+      const parentAPI = api.useExposed<ParentAPI>(parentSymbol);
+      expect(parentAPI?.double(parentAPI.initial)).toEqual(4);
+    },
+  };
+
+  const rsbuild = await createRsbuild({
+    cwd: import.meta.dirname,
+    config: {
+      plugins: [pluginParent, pluginParent2, pluginChild],
+    },
+  });
   await rsbuild.build();
 });
