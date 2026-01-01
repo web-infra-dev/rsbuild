6fb90bcd26b73e572e9aeaa01a9c9d246edb1ba9
neverland <chenjiahan.jait@bytedance.com>
2026-01-01 21:29:13 +0800

    chore(create-rsbuild): move setup files to test dir

--- packages/create-rsbuild/template-rstest/vanilla-js/rstest.config.js
+++ packages/create-rsbuild/template-rstest/vanilla-js/rstest.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from '@rstest/core';
 // Docs: https://rstest.rs/config/
 export default defineConfig({
   testEnvironment: 'happy-dom',
-  setupFiles: ['./rstest.setup.js'],
+  setupFiles: ['./tests/rstest.setup.js'],
 });
