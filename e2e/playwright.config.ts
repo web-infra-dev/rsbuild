0a680251fac061ca56e4852b903a3b03839fdfb4
neverland <chenjiahan.jait@bytedance.com>
2026-01-01 17:08:26 +0800

    chore(CI): speed up playwright browser setup (#6877)

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,9 +4,15 @@ process.env.PROVIDE_TYPE ||= 'rspack';
 // https://playwright.dev/docs/service-workers-experimental
 process.env.PW_EXPERIMENTAL_SERVICE_WORKER_NETWORK_EVENTS = '1';
 
+const isCI = Boolean(process.env.CI);
+
 export default defineConfig({
   // Retry on CI
-  retries: process.env.CI ? 3 : 0,
+  retries: isCI ? 3 : 0,
   // Print line for each test being run in CI
   reporter: 'list',
+  use: {
+    // Use the built-in Chrome browser to speed up CI tests
+    channel: isCI ? 'chrome' : undefined,
+  },
 });
