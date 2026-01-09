8e120034f2a8f3c6995d5c0d34ebe08a80bf71ee
neverland <chenjiahan.jait@bytedance.com>
2026-01-09 13:36:07 +0800

    fix

--- e2e/cases/cli/host/index.test.ts
+++ e2e/cases/cli/host/index.test.ts
@@ -1,12 +1,13 @@
-import { NETWORK_LOG_REGEX, test } from '@e2e/helper';
+import { getRandomPort, NETWORK_LOG_REGEX, test } from '@e2e/helper';
 
 test('should listen on localhost by default', async ({
   execCli,
   logHelper,
 }) => {
-  execCli('dev');
+  const port = await getRandomPort();
+  execCli(`dev --port ${port}`);
   await logHelper.expectBuildEnd();
-  await logHelper.expectLog('➜  Local:    http://localhost:');
+  await logHelper.expectLog(`➜  Local:    http://localhost:${port}`);
   logHelper.expectNoLog(NETWORK_LOG_REGEX);
 });
 
@@ -14,9 +15,10 @@ test('should listen on all interfaces when host is true', async ({
   execCli,
   logHelper,
 }) => {
-  execCli('dev --host');
+  const port = await getRandomPort();
+  execCli(`dev --host --port ${port}`);
   await logHelper.expectBuildEnd();
-  await logHelper.expectLog('➜  Local:    http://localhost:');
+  await logHelper.expectLog(`➜  Local:    http://localhost:${port}`);
   await logHelper.expectLog(NETWORK_LOG_REGEX);
 });
 
@@ -24,8 +26,9 @@ test('should listen on all interfaces when host is 0.0.0.0', async ({
   execCli,
   logHelper,
 }) => {
-  execCli('dev --host 0.0.0.0');
+  const port = await getRandomPort();
+  execCli(`dev --host 0.0.0.0 --port ${port}`);
   await logHelper.expectBuildEnd();
-  await logHelper.expectLog('➜  Local:    http://localhost:');
+  await logHelper.expectLog(`➜  Local:    http://localhost:${port}`);
   await logHelper.expectLog(NETWORK_LOG_REGEX);
 });
