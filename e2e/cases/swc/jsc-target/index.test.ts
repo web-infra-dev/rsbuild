20d4ed99647e6bac731c8bde778089896ed733b8
neverland <chenjiahan.jait@bytedance.com>
2026-01-12 18:43:12 +0800

    fix

--- e2e/cases/swc/jsc-target/index.test.ts
+++ e2e/cases/swc/jsc-target/index.test.ts
@@ -4,12 +4,12 @@ test('should configure jsc.target correctly in dev', async ({ devOnly }) => {
   const rsbuild = await devOnly();
   const files = rsbuild.getDistFiles();
   const content = getFileContent(files, 'index.js');
-  expect(content).not.toContain('...a');
+  expect(content).not.toContain('...baz');
 });
 
 test('should configure jsc.target correctly in build', async ({ build }) => {
   const rsbuild = await build();
   const files = rsbuild.getDistFiles();
   const content = getFileContent(files, 'index.js');
-  expect(content).not.toContain('...a');
+  expect(content).not.toContain('...baz');
 });
