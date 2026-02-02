5253966430868d58e2cb9695adbf34cfb29b622a
neverland <chenjiahan.jait@bytedance.com>
2026-02-02 13:45:53 +0800

    docs: enable Rspress's new llms generation (#7114)

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,7 +1,4 @@
-import {
-  Layout as BaseLayout,
-  getCustomMDXComponent as basicGetCustomMDXComponent,
-} from '@rspress/core/theme-original';
+import { Layout as BaseLayout } from '@rspress/core/theme-original';
 import { Announcement } from '@rstack-dev/doc-ui/announcement';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 import { HomeLayout } from './pages';
@@ -11,34 +8,9 @@ import {
   Search as PluginAlgoliaSearch,
   ZH_LOCALES,
 } from '@rspress/plugin-algolia/runtime';
-import {
-  LlmsContainer,
-  LlmsCopyButton,
-  LlmsViewOptions,
-} from '@rspress/plugin-llms/runtime';
 
 // Enable announcement when we have something to announce
 const ANNOUNCEMENT_URL = '';
-
-export function getCustomMDXComponent() {
-  const { h1: H1, ...mdxComponents } = basicGetCustomMDXComponent();
-
-  const MyH1 = ({ ...props }) => {
-    return (
-      <>
-        <H1 {...props} />
-        <LlmsContainer>
-          <LlmsCopyButton />
-          <LlmsViewOptions />
-        </LlmsContainer>
-      </>
-    );
-  };
-  return {
-    ...mdxComponents,
-    h1: MyH1,
-  };
-}
 
 const Layout = () => {
   const { page } = usePageData();
