67c8e834663b733bfd3d5067dd8e90629b23c3a0
neverland <chenjiahan.jait@bytedance.com>
2026-02-03 17:41:11 +0800

    fix: should generate manifest with integrity when html plugin is disabled

--- e2e/cases/manifest/integrity/index.test.ts
+++ e2e/cases/manifest/integrity/index.test.ts
@@ -15,8 +15,48 @@ test('should generate manifest file with integrity in build', async ({
   });
 });
 
+test('should generate manifest file with integrity in build when html plugin is disabled', async ({
+  build,
+}) => {
+  const rsbuild = await build({
+    config: {
+      tools: {
+        htmlPlugin: false,
+      },
+    },
+  });
+  const files = rsbuild.getDistFiles();
+  const manifestContent = getFileContent(files, 'manifest.json');
+  const manifest = JSON.parse(manifestContent) as ManifestData;
+  manifest.allFiles.forEach((item) => {
+    if (item.endsWith('.js')) {
+      expect(manifest.integrity[item]).toBeTruthy();
+    }
+  });
+});
+
 test('should generate manifest file with integrity in dev', async ({ dev }) => {
   const rsbuild = await dev();
+  const files = rsbuild.getDistFiles();
+  const manifestContent = getFileContent(files, 'manifest.json');
+  const manifest = JSON.parse(manifestContent) as ManifestData;
+  manifest.allFiles.forEach((item) => {
+    if (item.endsWith('.js')) {
+      expect(manifest.integrity[item]).toBeTruthy();
+    }
+  });
+});
+
+test('should generate manifest file with integrity in dev when html plugin is disabled', async ({
+  dev,
+}) => {
+  const rsbuild = await dev({
+    config: {
+      tools: {
+        htmlPlugin: false,
+      },
+    },
+  });
   const files = rsbuild.getDistFiles();
   const manifestContent = getFileContent(files, 'manifest.json');
   const manifest = JSON.parse(manifestContent) as ManifestData;
