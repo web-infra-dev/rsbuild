8f3e992b2af16be095b37b4a0110ae066bcfdbe0
neverland <chenjiahan.jait@bytedance.com>
2026-02-03 16:58:31 +0800

    feat(deps): update @rspack/core to ^1.7.5 (#7129)

--- e2e/cases/output/charset-emoji-filename/index.test.ts
+++ e2e/cases/output/charset-emoji-filename/index.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@e2e/helper';
+import { expect, rspackTest, test } from '@e2e/helper';
 
 const utf8Str = `你好 world! I'm 🦀`;
 
@@ -7,12 +7,12 @@ test('should resolve emoji filename in dev', async ({ page, dev }) => {
   expect(await page.evaluate('window.test')).toBe(utf8Str);
 });
 
-test('should resolve emoji filename in build', async ({
-  page,
-  buildPreview,
-}) => {
-  const rsbuild = await buildPreview();
-  const content = await rsbuild.getIndexBundle();
-  expect(await page.evaluate('window.test')).toBe(utf8Str);
-  expect(content.includes(utf8Str)).toBeTruthy();
-});
+rspackTest(
+  'should resolve emoji filename in build',
+  async ({ page, buildPreview }) => {
+    const rsbuild = await buildPreview();
+    const content = await rsbuild.getIndexBundle();
+    expect(await page.evaluate('window.test')).toBe(utf8Str);
+    expect(content.includes("你好 world! I'm \\uD83E\\uDD80")).toBeTruthy();
+  },
+);
