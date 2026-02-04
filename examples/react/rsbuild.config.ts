4416f007a7d2ec01d5a28472cadcd81df2fd2db0
neverland <chenjiahan.jait@bytedance.com>
2026-02-04 15:04:54 +0800

    docs: update proxy examples to remove unnecessary path segments

--- examples/react/rsbuild.config.ts
+++ examples/react/rsbuild.config.ts
@@ -3,4 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  server: {
+    proxy: {
+      // http://localhost:3000/api -> http://localhost:3001/api
+      // http://localhost:3000/api/foo -> http://localhost:3001/api/foo
+      '/api': 'http://example.com:3001',
+    },
+  },
 });
