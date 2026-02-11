db94b46deb51d34e980e01641f6fbed108307d83
neverland <chenjiahan.jait@bytedance.com>
2026-02-11 13:21:27 +0800

    chore: no need to set exportsPresence error

--- packages/core/src/plugins/basic.ts
+++ packages/core/src/plugins/basic.ts
@@ -33,7 +33,6 @@ export const pluginBasic = (): RsbuildPlugin => ({
 
         chain.module.parser.merge({
           javascript: {
-            exportsPresence: 'error',
             typeReexportsPresence: 'tolerant',
           },
         });
