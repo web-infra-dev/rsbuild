9b299774a24d3ca7b94323ad44514bb737f557f6
neverland <chenjiahan.jait@bytedance.com>
2026-02-13 10:52:03 +0800

    fix

--- e2e/cases/swc/swc-plugin/index.test.ts
+++ e2e/cases/swc/swc-plugin/index.test.ts
@@ -1,14 +1,11 @@
 import { expect, gotoPage, test } from '@e2e/helper';
 
-test('should run SWC Wasm plugin correctly', async ({ page, runBoth }) => {
-  await runBoth(
-    async ({ result }) => {
-      const msgPromise = page.waitForEvent('console');
-      await gotoPage(page, result);
+test('should run SWC Wasm plugin correctly', async ({ page, runBothServe }) => {
+  await runBothServe(async ({ result }) => {
+    const msgPromise = page.waitForEvent('console');
+    await gotoPage(page, result);
 
-      const msg = await msgPromise;
-      expect(await msg.args()[0].jsonValue()).toEqual('this is error');
-    },
-    { runServer: true },
-  );
+    const msg = await msgPromise;
+    expect(await msg.args()[0].jsonValue()).toEqual('this is error');
+  });
 });
