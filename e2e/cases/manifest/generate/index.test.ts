f856db2ba1c9729f14ff6a895a8cdf130ba66121
neverland <chenjiahan.jait@bytedance.com>
2026-02-14 19:01:48 +0800

    test: refactor e2e tests to use runBoth helper

--- e2e/cases/manifest/generate/index.test.ts
+++ e2e/cases/manifest/generate/index.test.ts
@@ -18,40 +18,24 @@ const config: RsbuildConfig = {
   splitChunks: false,
 };
 
-test('should generate custom manifest data in build', async ({ build }) => {
-  const rsbuild = await build({
-    config,
-  });
+test('should generate custom manifest data', async ({ runBoth }) => {
+  await runBoth(
+    async ({ result }) => {
+      const files = result.getDistFiles();
+      const manifestContent = getFileContent(files, 'my-manifest.json');
+      const manifest = JSON.parse(manifestContent);
 
-  const files = rsbuild.getDistFiles();
-  const manifestContent = getFileContent(files, 'my-manifest.json');
-  const manifest = JSON.parse(manifestContent);
-
-  expect(manifest.filesCount).toBe(2);
-  expect(manifest.data.allFiles.length).toBe(2);
-  expect(manifest.data.entries.index).toMatchObject({
-    initial: {
-      js: ['/static/js/index.js'],
+      expect(manifest.filesCount).toBe(2);
+      expect(manifest.data.allFiles.length).toBe(2);
+      expect(manifest.data.entries.index).toMatchObject({
+        initial: {
+          js: ['/static/js/index.js'],
+        },
+        html: ['/index.html'],
+      });
     },
-    html: ['/index.html'],
-  });
-});
-
-test('should generate custom manifest data in dev', async ({ dev }) => {
-  const rsbuild = await dev({
-    config,
-  });
-
-  const files = rsbuild.getDistFiles();
-  const manifestContent = getFileContent(files, 'my-manifest.json');
-  const manifest = JSON.parse(manifestContent);
-
-  expect(manifest.filesCount).toBe(2);
-  expect(manifest.data.allFiles.length).toBe(2);
-  expect(manifest.data.entries.index).toMatchObject({
-    initial: {
-      js: ['/static/js/index.js'],
+    {
+      config,
     },
-    html: ['/index.html'],
-  });
+  );
 });
