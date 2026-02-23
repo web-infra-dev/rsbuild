2f134a12bdad800d0df3799306e02e1d6ef03166
neverland <chenjiahan.jait@bytedance.com>
2026-02-23 18:20:23 +0800

    test(e2e): add test case for tailwindcss loader

---
+++ e2e/cases/tailwindcss/loader/index.test.ts
@@ -0,0 +1,17 @@
+import { expect, getFileContent, test } from '@e2e/helper';
+
+test('should generate tailwindcss utilities correctly', async ({ build }) => {
+  const rsbuild = await build();
+
+  const files = rsbuild.getDistFiles();
+  const indexCssContent = getFileContent(files, 'index.css');
+  expect(indexCssContent).toContain(
+    '.text-3xl{font-size:var(--text-3xl);line-height:var(--tw-leading,var(--text-3xl--line-height))}',
+  );
+  expect(indexCssContent).toContain(
+    '.font-bold{--tw-font-weight:var(--font-weight-bold);font-weight:var(--font-weight-bold)}',
+  );
+  expect(indexCssContent).toContain(
+    '.underline{text-decoration-line:underline}',
+  );
+});
