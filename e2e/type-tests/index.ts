ee3b859addaeacc01cee5f5e5a7d3c982f74e272
neverland <chenjiahan.jait@bytedance.com>
2026-02-26 18:39:38 +0800

    test: more

--- e2e/type-tests/index.ts
+++ e2e/type-tests/index.ts
@@ -1,4 +1,5 @@
 // This folder disabled `skipLibCheck` to check the public types of @rsbuild/core
+/// <reference types="@rsbuild/core/types" />
 import { createRsbuild, defineConfig } from '@rsbuild/core';
 
 createRsbuild({});
