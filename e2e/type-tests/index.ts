ea07709dd213cf548e5c357c30751b9f5fd05b27
neverland <chenjiahan.jait@bytedance.com>
2026-02-26 17:04:07 +0800

    test(e2e): add type tests for @rsbuild/core public types

---
+++ e2e/type-tests/index.ts
@@ -0,0 +1,6 @@
+// This folder disabled `skipLibCheck` to check the public types of @rsbuild/core
+import { createRsbuild, defineConfig } from '@rsbuild/core';
+
+createRsbuild({});
+
+defineConfig({});
