de21f5d0393618e5f2f02d3a5f825e00c3736d86
neverland <chenjiahan.jait@bytedance.com>
2026-02-28 15:12:53 +0800

    fix

--- e2e/helper/server.ts
+++ e2e/helper/server.ts
@@ -3,13 +3,13 @@ import type { RequestHandler } from '@rsbuild/core';
 import type { Context } from 'hono';
 
 export const createConnectHandler = (middlewares: RequestHandler) => {
-  return (c: Context) => {
-    middlewares(c.env.incoming, c.env.outgoing, (error) => {
-      if (c.env.outgoing.writableEnded) {
+  return ({ env }: Context) => {
+    middlewares(env.incoming, env.outgoing, (error) => {
+      if (env.outgoing.writableEnded) {
         return;
       }
-      c.env.outgoing.statusCode = error ? 500 : 404;
-      c.env.outgoing.end(error ? String(error) : 'Not Found');
+      env.outgoing.statusCode = error ? 500 : 404;
+      env.outgoing.end(error ? String(error) : 'Not Found');
     });
     return RESPONSE_ALREADY_SENT;
   };
