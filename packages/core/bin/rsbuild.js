5a70fe51a8aea2a93a81d6232bbe440336e33a23
neverland <chenjiahan.jait@bytedance.com>
2026-03-07 17:20:16 +0800

    fix(core): add node version check (#7296)

--- packages/core/bin/rsbuild.js
+++ packages/core/bin/rsbuild.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import nodeModule from 'node:module';
+import process from 'node:process';
 
 // enable on-disk code caching of all modules loaded by Node.js
 // requires Nodejs >= 22.8.0
@@ -12,7 +13,20 @@ if (enableCompileCache) {
   }
 }
 
+function checkNodeVersion() {
+  const { node, bun, deno } = process.versions;
+  if (!node || bun || deno) {
+    return;
+  }
+  if (Number(node.split('.')[0]) < 20) {
+    throw new Error(
+      `Unsupported Node.js version "${node}", Rsbuild requires Node.js 20+.`,
+    );
+  }
+}
+
 async function main() {
+  checkNodeVersion();
   const { runCLI } = await import('../dist/index.js');
   runCLI();
 }
