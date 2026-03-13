94e369daaab98ea5d6aa091b100f47589c0202cd
neverland <chenjiahan.jait@bytedance.com>
2026-03-13 14:53:17 +0800

    feat(create-rsbuild): add React Compiler support for React templates (#7329)

---
+++ packages/create-rsbuild/template-react-compiler/react-ts/rsbuild.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift('babel-plugin-react-compiler');
+      },
+    }),
+  ],
+});
