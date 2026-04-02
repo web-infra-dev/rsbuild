e50201982477df4f4d3f2355833fa87ef592f68d
neverland <chenjiahan.jait@bytedance.com>
2026-04-02 15:52:32 +0800

    fix

--- e2e/cases/source/transform-import-package/helper.ts
+++ e2e/cases/source/transform-import-package/helper.ts
@@ -1,13 +1,13 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import fse from 'fs-extra';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export function copyPkgToNodeModules() {
-  const nodeModules = path.resolve(__dirname, 'node_modules');
+  const nodeModules = path.resolve(import.meta.dirname, 'node_modules');
   const fixturePackage = path.resolve(nodeModules, 'fixture-package');
 
-  fse.copySync(path.resolve(__dirname, 'fixture-package'), fixturePackage);
+  fse.copySync(
+    path.resolve(import.meta.dirname, 'fixture-package'),
+    fixturePackage,
+  );
 }
