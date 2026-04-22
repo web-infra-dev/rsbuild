f106de91ce8accab0d363e2382bf721099dc27cb
neverland <chenjiahan.jait@bytedance.com>
2026-04-22 22:27:07 +0800

    test(type-tests): add plugin package registration coverage

--- e2e/type-tests/resolution-nodenext/index.ts
+++ e2e/type-tests/resolution-nodenext/index.ts
@@ -1,7 +1,34 @@
 // This folder disables `skipLibCheck` to check the public types of @rsbuild/core.
 import '@rsbuild/core/types';
 import { createRsbuild, defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginLess } from '@rsbuild/plugin-less';
+import { pluginPreact } from '@rsbuild/plugin-preact';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginSolid } from '@rsbuild/plugin-solid';
+import { pluginStylus } from '@rsbuild/plugin-stylus';
+import { pluginSvelte } from '@rsbuild/plugin-svelte';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginVue } from '@rsbuild/plugin-vue';
 
-createRsbuild({});
+const plugins = [
+  pluginBabel(),
+  pluginLess(),
+  pluginPreact(),
+  pluginReact(),
+  pluginSass(),
+  pluginSolid(),
+  pluginStylus(),
+  pluginSvelte(),
+  pluginSvgr(),
+  pluginVue(),
+];
 
-defineConfig({});
+createRsbuild({
+  config: {
+    plugins,
+  },
+});
+
+defineConfig({ plugins });
