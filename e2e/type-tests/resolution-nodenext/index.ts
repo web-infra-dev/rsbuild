c7bd836104314d1895e43f7aacdffb9765492cda
neverland <chenjiahan.jait@bytedance.com>
2026-04-30 10:07:02 +0800

    fix(types): remove problematic plugin checks

--- e2e/type-tests/resolution-nodenext/index.ts
+++ e2e/type-tests/resolution-nodenext/index.ts
@@ -5,24 +5,16 @@ import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginPreact } from '@rsbuild/plugin-preact';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSass } from '@rsbuild/plugin-sass';
 import { pluginSolid } from '@rsbuild/plugin-solid';
 import { pluginStylus } from '@rsbuild/plugin-stylus';
-import { pluginSvelte } from '@rsbuild/plugin-svelte';
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
-import { pluginVue } from '@rsbuild/plugin-vue';
 
 const plugins = [
   pluginBabel(),
   pluginLess(),
   pluginPreact(),
   pluginReact(),
-  pluginSass(),
   pluginSolid(),
   pluginStylus(),
-  pluginSvelte(),
-  pluginSvgr(),
-  pluginVue(),
 ];
 
 createRsbuild({
