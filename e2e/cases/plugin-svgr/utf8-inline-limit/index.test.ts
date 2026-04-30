4d06aa902cc07ed8d5cf23376f9fe14644d1b915
easy1090 <congzhe@bytedance.com>
2026-04-30 14:59:08 +0800

    test: add e2e case for svgr utf8 dataUriLimit

---
+++ e2e/cases/plugin-svgr/utf8-inline-limit/index.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@e2e/helper';
+
+test('should not inline utf8 svg when byte length exceeds limit', async ({
+  page,
+  buildPreview,
+}) => {
+  const rsbuild = await buildPreview();
+
+  await expect(
+    page.evaluate(`document.getElementById('utf8-svg').src`),
+  ).resolves.not.toContain('data:image/svg+xml;base64');
+
+  const files = rsbuild.getDistFiles();
+
+  expect(Object.keys(files).some((key) => key.endsWith('.svg'))).toBeTruthy();
+});
---
+++ e2e/cases/plugin-svgr/utf8-inline-limit/src/App.jsx
@@ -0,0 +1,7 @@
+import iconUrl from './icon.svg';
+
+function App() {
+  return <img id="utf8-svg" src={iconUrl} alt="utf8-svg" />;
+}
+
+export default App;
