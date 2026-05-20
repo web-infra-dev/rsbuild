e4614b65703a742e5e0b6190818fa2435bde883b
neverland <chenjiahan.jait@bytedance.com>
2026-05-20 17:10:08 +0800

    test: add public manifest html case

---
+++ e2e/cases/html/app-icon-public-manifest/index.test.ts
@@ -0,0 +1,23 @@
+import { expect, getDistFiles, getFileContent, test } from '@e2e/helper';
+
+test('should use public manifest for additional manifest fields', async ({
+  build,
+}) => {
+  const rsbuild = await build();
+  const files = await getDistFiles(rsbuild.distPath);
+  const html = getFileContent(files, 'index.html');
+  const manifest = getFileContent(files, 'manifest.webmanifest');
+
+  expect(html).toContain('<link rel="manifest" href="/manifest.webmanifest">');
+  expect(JSON.parse(manifest)).toEqual({
+    name: 'My Website',
+    short_name: 'Website',
+    icons: [
+      {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  });
+});
