9191d810e7d3b57daa977a50428c88453ab2a4a6
Jiahan Chen <jait.chenjiahan@gmail.com>
2026-05-31 11:27:33 +0800

    test(e2e): debug template cache on Windows

--- e2e/cases/html/template-cache/index.test.ts
+++ e2e/cases/html/template-cache/index.test.ts
@@ -9,14 +9,10 @@ test('should not re-compile templates when the template is not changed', async (
   editFile,
   copySrcDir,
 }) => {
-  // Failed to run this case on Windows
-  if (process.platform === 'win32') {
-    test.skip();
-  }
-
   let count = 0;
 
   const tempSrc = await copySrcDir();
+  console.log('[template-cache] tempSrc', tempSrc);
 
   await dev({
     config: {
@@ -34,8 +30,9 @@ test('should not re-compile templates when the template is not changed', async (
         {
           name: 'test-plugin',
           setup(api) {
-            api.transform({ test: /\.html$/ }, ({ code }) => {
+            api.transform({ test: /\.html$/ }, ({ code, resource }) => {
               count++;
+              console.log('[template-cache] transform', count, resource);
               return `export default \`${code}\`;`;
             });
           },
@@ -45,6 +42,7 @@ test('should not re-compile templates when the template is not changed', async (
   });
 
   await expect(page.locator('#root')).toHaveText('foo');
+  console.log('[template-cache] after first compile', count);
   expect(count).toEqual(2);
 
   // Re-compile the template when the template is changed
@@ -52,6 +50,7 @@ test('should not re-compile templates when the template is not changed', async (
     code.replace('foo', 'foo2'),
   );
   await expect(page.locator('#root')).toHaveText('foo2');
+  console.log('[template-cache] after html edit', count);
   // The count will be 4 as the childCompiler in html-rspack-plugin
   // will compile all the templates
   expect(count).toEqual(4);
@@ -60,6 +59,7 @@ test('should not re-compile templates when the template is not changed', async (
     code.replace('foo', 'foo3'),
   );
   await expect(page.locator('#content')).toHaveText('foo3');
+  console.log('[template-cache] after js edit', count);
   // The count should not change if the templates are not changed
   expect(count).toEqual(4);
 });
