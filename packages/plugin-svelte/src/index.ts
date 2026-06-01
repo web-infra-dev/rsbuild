6bb87731c2c9f6ccd74b089dcbfd5e10842efadb
neverland <chenjiahan.jait@bytedance.com>
2026-06-01 17:37:21 +0800

    fix

--- packages/plugin-svelte/src/index.ts
+++ packages/plugin-svelte/src/index.ts
@@ -60,7 +60,7 @@ const validateSvelteVersion = (rootPath: string) => {
   const majorVersion = version ? Number(version.split('.')[0]) : 0;
   if (!(majorVersion >= 5)) {
     throw new Error(
-      `[rsbuild:svelte] @rsbuild/plugin-svelte requires svelte >= 5.0.0, but found ${version || 'unknown'}.`,
+      `"@rsbuild/plugin-svelte" requires svelte >= 5.0.0, but found ${version || 'unknown'}.`,
     );
   }
 };
