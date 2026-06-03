aa8c29f96454a022a326c297a7150d82954cc54a
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-06-03 18:53:00 +0800

    feat(core): enable Rspack pureFunctions optimization (#7824)

--- packages/core/src/plugins/basic.ts
+++ packages/core/src/plugins/basic.ts
@@ -31,6 +31,11 @@ export const pluginBasic = (): RsbuildPlugin => ({
         // Disable performance hints, these logs are too complex
         chain.performance.hints(false);
 
+        chain.experiments({
+          ...chain.get('experiments'),
+          pureFunctions: true,
+        });
+
         chain.module.parser.merge({
           javascript: {
             typeReexportsPresence: 'tolerant',
