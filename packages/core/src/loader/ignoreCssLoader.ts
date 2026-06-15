956e874416abcf3be6d01faa81ff20b98e29052c
neverland <chenjiahan.jait@bytedance.com>
2026-06-15 17:18:33 +0800

    fix(core): skip css-loader for non-emitting global CSS

--- packages/core/src/loader/ignoreCssLoader.ts
+++ packages/core/src/loader/ignoreCssLoader.ts
@@ -1,8 +1,12 @@
-import type { LoaderDefinition } from '@rspack/core';
+import type { LoaderDefinition, PitchLoaderDefinitionFunction } from '@rspack/core';
+import { isCSSModules } from '../helpers/css';
+import type { CSSLoaderOptions } from '../types';
 
-const ignoreCssLoader: LoaderDefinition = function (source) {
-  this?.cacheable(true);
+type IgnoreCssLoaderOptions = {
+  modules?: CSSLoaderOptions['modules'];
+};
 
+const ignoreCssLoader: LoaderDefinition<IgnoreCssLoaderOptions> = function (source) {
   // if the source code include '___CSS_LOADER_EXPORT___'
   // It is not a CSS Modules file because exportOnlyLocals is enabled,
   // so we don't need to preserve it.
@@ -12,6 +16,18 @@ const ignoreCssLoader: LoaderDefinition = function (source) {
 
   // Preserve CSS Modules export for SSR.
   return source;
+};
+
+// In non-emitting builds, skip css-loader and following CSS transforms for global CSS.
+// CSS Modules must still pass through css-loader so SSR builds can export locals.
+export const pitch: PitchLoaderDefinitionFunction<IgnoreCssLoaderOptions> = function () {
+  const { modules } = this.getOptions();
+
+  if (isCSSModules(modules, this.resourcePath, this.resourceQuery, this.resourceFragment)) {
+    return;
+  }
+
+  return '';
 };
 
 export default ignoreCssLoader;
