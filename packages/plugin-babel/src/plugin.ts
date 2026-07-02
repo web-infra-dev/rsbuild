11c87e8570c9ec5f96e3561f1ce5a25efe7dca61
neverland <chenjiahan.jait@bytedance.com>
2026-07-02 14:34:02 +0800

    fix(plugin-babel): skip raw query in standalone rule

--- packages/plugin-babel/src/plugin.ts
+++ packages/plugin-babel/src/plugin.ts
@@ -141,6 +141,7 @@ export const pluginBabel = (options: PluginBabelOptions = {}): RsbuildPlugin => 
 
           const loader = rule
             .test(SCRIPT_REGEX)
+            .resourceQuery({ not: /[?&]raw(?:&|=|$)/ })
             .with({ type: { not: 'text' } })
             .use(CHAIN_ID.USE.BABEL)
             .loader(BABEL_LOADER_PATH)
