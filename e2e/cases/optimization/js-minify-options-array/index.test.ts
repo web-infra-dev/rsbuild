a77fa372f0802e284770747ede86c950c25f24aa
neverland <chenjiahan.jait@bytedance.com>
2026-07-21 21:40:41 +0800

    test(e2e): cover multiple JS minimizer options

---
+++ e2e/cases/optimization/js-minify-options-array/index.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@e2e/helper';
+import { getFileContent } from '@rstackjs/test-utils';
+
+test('should apply different JS minify options to different assets', async ({ build }) => {
+  const rsbuild = await build();
+  const files = rsbuild.getDistFiles();
+  const foo = getFileContent(files, 'static/js/foo.js');
+  const bar = getFileContent(files, 'static/js/bar.js');
+
+  expect(foo).not.toContain('foo-console');
+  expect(foo).toContain('foo-result');
+  expect(bar).toContain('bar-console');
+});
---
+++ e2e/cases/optimization/js-minify-options-array/rsbuild.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from '@rsbuild/core';
+
+export default defineConfig({
+  source: {
+    entry: {
+      foo: './src/foo.js',
+      bar: './src/bar.js',
+    },
+  },
+  output: {
+    filenameHash: false,
+    minify: {
+      css: false,
+      jsOptions: [
+        {
+          include: /foo\.js$/,
+          minimizerOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        },
+        {
+          include: /bar\.js$/,
+          minimizerOptions: {
+            compress: false,
+            mangle: false,
+          },
+        },
+      ],
+    },
+  },
+});
