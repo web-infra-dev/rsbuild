7ed2a1aed18c7597571088721ddb2cbbe09b8cda
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-22 22:00:35 +0800

    chore(build): simplify Rslib configs (#8168)

--- scripts/config/rslib.config.ts
+++ scripts/config/rslib.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig, type LibConfig, type Rsbuild } from '@rslib/core';
-import { pluginAreTheTypesWrong } from 'rsbuild-plugin-arethetypeswrong';
-
-export const commonExternals: Array<string | RegExp> = ['webpack', /[\\/]compiled[\\/]/];
 
 export const nodeMinifyConfig = {
-  js: true,
-  css: false,
   jsOptions: {
     minimizerOptions: {
       // preserve variable name and disable minify for easier debugging
@@ -28,54 +23,6 @@ export const esmConfig: LibConfig = {
   },
 };
 
-export const cjsConfig: LibConfig = {
-  format: 'cjs',
-  syntax: 'es2023',
-  output: {
-    minify: nodeMinifyConfig,
-  },
-};
-
 export const pureEsmPackage = defineConfig({
   lib: [esmConfig],
-  tools: {
-    rspack: {
-      experiments: {
-        nativeWatcher: true,
-      },
-      externals: commonExternals,
-    },
-  },
-  plugins: [
-    pluginAreTheTypesWrong({
-      enable: Boolean(process.env.CI),
-      areTheTypesWrongOptions: {
-        // Pure ESM packages are expected to be import-only in Node.
-        ignoreRules: ['cjs-resolves-to-esm'],
-      },
-    }),
-  ],
-});
-
-export const dualPackage = defineConfig({
-  lib: [esmConfig, cjsConfig],
-  tools: {
-    rspack: {
-      experiments: {
-        nativeWatcher: true,
-      },
-      externals: commonExternals,
-    },
-  },
-  plugins: [
-    pluginAreTheTypesWrong({
-      enable: Boolean(process.env.CI),
-      areTheTypesWrongOptions: {
-        ignoreRules: [
-          // The dual package always provide the ESM types.
-          'false-esm',
-        ],
-      },
-    }),
-  ],
 });
