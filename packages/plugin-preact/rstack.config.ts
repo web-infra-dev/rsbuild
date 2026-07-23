79a894c40749ed0c2e9ada2ad4f4eebff88c94d9
neverland <chenjiahan.jait@bytedance.com>
2026-07-23 11:20:50 +0800

    chore: migrate Rslib to Rstack CLI

--- packages/plugin-preact/rstack.config.ts
+++ packages/plugin-preact/rstack.config.ts
@@ -1,7 +1,15 @@
 import { define } from 'rstack';
 
+define.lib(async () => {
+  const { esmConfig } = await import('@scripts/config/lib');
+
+  return {
+    lib: [esmConfig],
+  };
+});
+
 define.test(async () => {
-  const { baseConfig } = await import('@scripts/config/rstest.config.ts');
+  const { baseConfig } = await import('@scripts/config/test');
 
   return baseConfig;
 });
