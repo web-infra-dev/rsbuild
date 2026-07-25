871a13b9cc7524be351c4af1883d3c2d8b355067
neverland <chenjiahan.jait@bytedance.com>
2026-07-25 08:40:41 +0800

    chore(deps): update Rstest to 0.11.4

--- e2e/helper/fixture.ts
+++ e2e/helper/fixture.ts
@@ -8,7 +8,7 @@ import {
 import { constants as fsConstants, promises } from 'node:fs';
 import path from 'node:path';
 import { expect, test as base } from '@rstest/playwright';
-import type { PlaywrightFixture, PlaywrightOptions } from '@rstest/playwright';
+import type { PlaywrightOptions } from '@rstest/playwright';
 import {
   copyNodeModules as baseCopyNodeModules,
   editFile as baseEditFile,
@@ -16,7 +16,6 @@ import {
   prepareDist as basePrepareDist,
 } from '@rstackjs/test-utils';
 import fse from 'fs-extra';
-import type { TestContext, TestOptions } from 'rstack/test';
 import { RSBUILD_BIN_PATH } from './constants.ts';
 import {
   type Build,
@@ -400,19 +399,6 @@ const rsbuildTest = rsbuildBase.extend<RsbuildFixture>({
   },
 });
 
-// TODO: Remove these hook type overrides after https://github.com/web-infra-dev/rstest/pull/1604 is released.
-type MaybePromise<T> = T | Promise<T>;
-type RsbuildTestContext = TestContext & PlaywrightFixture & RsbuildFixture;
-type TestCallback = (context: RsbuildTestContext) => MaybePromise<void>;
-type AfterEachCallback = (context: RsbuildTestContext) => MaybePromise<void>;
-type BeforeEachCallback = (context: RsbuildTestContext) => MaybePromise<void | AfterEachCallback>;
-type RsbuildTest = Omit<typeof rsbuildTest, 'afterEach' | 'beforeEach'> & {
-  (description: string, callback?: TestCallback, timeout?: number): void;
-  (description: string, options: TestOptions, callback?: TestCallback): void;
-  afterEach: (callback: AfterEachCallback, timeout?: number) => void;
-  beforeEach: (callback: BeforeEachCallback, timeout?: number) => void;
-};
-
-export const test = rsbuildTest as unknown as RsbuildTest;
+export const test = rsbuildTest;
 
 export { expect };
