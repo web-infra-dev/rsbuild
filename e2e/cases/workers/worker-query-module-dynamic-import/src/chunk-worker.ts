bab78cf9ba1b99be4777e2d8c5a7e04b515abfe1
neverland <chenjiahan.jait@bytedance.com>
2026-08-16 10:24:21 +0800

    chore: format worker e2e files

--- e2e/cases/workers/worker-query-module-dynamic-import/src/chunk-worker.ts
+++ e2e/cases/workers/worker-query-module-dynamic-import/src/chunk-worker.ts
@@ -1,5 +1,7 @@
 self.onmessage = async ({ data }) => {
-  const { getMessage } = await import(/* rspackChunkName: "worker-async" */ './async-message');
+  const { getMessage } = await import(
+    /* rspackChunkName: "worker-async" */ './async-message'
+  );
 
   self.postMessage({
     text: getMessage(self.name, data),
