b8138ad47d33df4c4bf500ea23206a9561f252d9
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-16 09:34:23 +0800

    test(core): add module worker async chunk e2e test (#8272)

---
+++ e2e/cases/workers/worker-query-module-dynamic-import/src/chunk-worker.ts
@@ -0,0 +1,7 @@
+self.onmessage = async ({ data }) => {
+  const { getMessage } = await import(/* rspackChunkName: "worker-async" */ './async-message');
+
+  self.postMessage({
+    text: getMessage(self.name, data),
+  });
+};
