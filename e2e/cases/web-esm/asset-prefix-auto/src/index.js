03ff9ab67874986693a105e04225d62c76d790fd
neverland <chenjiahan.jait@bytedance.com>
2026-08-16 16:59:15 +0800

    test(core): simplify web ESM asset prefix case

--- e2e/cases/web-esm/asset-prefix-auto/src/index.js
+++ e2e/cases/web-esm/asset-prefix-auto/src/index.js
@@ -1,8 +1,1 @@
-const loadButton = document.createElement('button');
-loadButton.id = 'load';
-loadButton.textContent = 'Load async chunk';
-loadButton.addEventListener('click', async () => {
-  const { renderAsyncContent } = await import(/* rspackChunkName: "asset-prefix-auto" */ './async');
-  renderAsyncContent();
-});
-document.body.appendChild(loadButton);
+void import(/* rspackChunkName: "asset-prefix-auto" */ './async');
