4e69ae0f634df3d956b356f24b077956b8157ecd
neverland <chenjiahan.jait@bytedance.com>
2026-08-25 13:57:09 +0800

    chore(examples): remove Babel from Solid example

--- examples/solid/rsbuild.config.ts
+++ examples/solid/rsbuild.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
-import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginSolid } from '@rsbuild/plugin-solid';
 
 export default defineConfig({
-  plugins: [
-    pluginBabel({
-      include: /\.(?:jsx|tsx)$/,
-    }),
-    pluginSolid({}),
-  ],
+  plugins: [pluginSolid()],
 });
