a8f297d99b81c904c11bba82714df40613474ccf
neverland <chenjiahan.jait@bytedance.com>
2026-08-30 10:33:32 +0800

    test: normalize global virtual store paths

--- scripts/config/rstest.setup.ts
+++ scripts/config/rstest.setup.ts
@@ -4,6 +4,9 @@ import { createSnapshotSerializer } from 'path-serializer';
 
 const repoRoot = path.join(__dirname, '../..');
 
+const GLOBAL_VIRTUAL_STORE_PATH =
+  /(?:file:\/{2})?(?:[a-zA-Z]:)?\/(?:[^/"'\r\n]+\/)*store\/v\d+\/links\/.+?\/node_modules(?=\/)/g;
+
 process.chdir(repoRoot);
 
 beforeAll((suite) => {
@@ -15,5 +18,11 @@ expect.addSnapshotSerializer(
   createSnapshotSerializer({
     root: repoRoot,
     workspace: path.join(__dirname, '..'),
+    replace: [
+      {
+        match: GLOBAL_VIRTUAL_STORE_PATH,
+        mark: 'pnpm-inner',
+      },
+    ],
   }),
 );
