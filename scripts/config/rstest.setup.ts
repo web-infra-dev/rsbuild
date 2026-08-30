5ce2e219da587109b0d8311469d5a36f702d8a54
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-30 12:54:05 +0800

    chore: enable pnpm global virtual store (#8395)

--- scripts/config/rstest.setup.ts
+++ scripts/config/rstest.setup.ts
@@ -4,6 +4,9 @@ import { createSnapshotSerializer } from 'path-serializer';
 
 const repoRoot = path.join(__dirname, '../..');
 
+const GLOBAL_VIRTUAL_STORE_PATH =
+  /(?:file:\/{2})?(?:[a-zA-Z]:)?\/(?:[^/"'\r\n]+\/)*v\d+\/links\/.+?\/node_modules(?=\/)/g;
+
 process.chdir(repoRoot);
 
 beforeAll((suite) => {
@@ -15,5 +18,13 @@ expect.addSnapshotSerializer(
   createSnapshotSerializer({
     root: repoRoot,
     workspace: path.join(__dirname, '..'),
+    replace: [
+      {
+        // TODO: Remove after path-serializer supports custom store directories.
+        // https://github.com/rstackjs/path-serializer/pull/27
+        match: GLOBAL_VIRTUAL_STORE_PATH,
+        mark: 'pnpm-inner',
+      },
+    ],
   }),
 );
