574561f3ac96a869bd650283d3ffe5069e2fad9f
SoonIter <sooniter@gmail.com>
2026-08-31 16:48:01 +0800

    chore(deps): upgrade path-serializer to 0.7.0

--- scripts/config/rstest.setup.ts
+++ scripts/config/rstest.setup.ts
@@ -4,9 +4,6 @@ import { createSnapshotSerializer } from 'path-serializer';
 
 const repoRoot = path.join(__dirname, '../..');
 
-const GLOBAL_VIRTUAL_STORE_PATH =
-  /(?:file:\/{2})?(?:[a-zA-Z]:)?\/(?:[^/"'\r\n]+\/)*v\d+\/links\/.+?\/node_modules(?=\/)/g;
-
 process.chdir(repoRoot);
 
 beforeAll((suite) => {
@@ -18,13 +15,5 @@ expect.addSnapshotSerializer(
   createSnapshotSerializer({
     root: repoRoot,
     workspace: path.join(__dirname, '..'),
-    replace: [
-      {
-        // TODO: Remove after path-serializer supports custom store directories.
-        // https://github.com/rstackjs/path-serializer/pull/27
-        match: GLOBAL_VIRTUAL_STORE_PATH,
-        mark: 'pnpm-inner',
-      },
-    ],
   }),
 );
