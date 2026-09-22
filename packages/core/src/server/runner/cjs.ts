bc7860441f972d12161cbeb473c9956e535d4cf8
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-09-22 21:09:20 +0800

    perf(core): reuse dynamic import function in CJS runner (#8525)

--- packages/core/src/server/runner/cjs.ts
+++ packages/core/src/server/runner/cjs.ts
@@ -88,6 +88,8 @@ export class CommonJsRunner extends BasicRunner {
     const requireCache: Record<string, ModuleObject> = Object.create(null);
     // rslint-disable-next-line @typescript-eslint/no-require-imports
     const vm = require('node:vm') as typeof import('node:vm');
+    let cachedDynamicImport:
+      ((specifier: string) => Promise<Module>) | undefined;
 
     return (currentDirectory, modulePath, context = {}) => {
       const file = context.file || this.getFile(modulePath, currentDirectory);
@@ -113,10 +115,10 @@ export class CommonJsRunner extends BasicRunner {
       const argValues = args.map((arg) => currentModuleScope[arg]);
       this.preExecute(file.content, file);
       // rslint-disable-next-line @typescript-eslint/no-implied-eval -- Evaluate import() in the main context instead of the VM context.
-      const dynamicImport = new Function(
+      const dynamicImport = (cachedDynamicImport ??= new Function(
         'specifier',
         'return import(specifier)',
-      ) as (specifier: string) => Promise<Module>;
+      ) as (specifier: string) => Promise<Module>);
       const fn = vm.compileFunction(file.content, args, {
         filename: file.path,
         // Specify how the modules should be loaded during the evaluation of this script when `import()` is called.
